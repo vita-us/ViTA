e8b24903b4d33da7a0c112b4d6b0e29805bcf0b9
Marc <weisemc@studi.informatik.uni-stuttgart.de>
2014-07-30 13:04:34 +0200

    Implement model class 'EntityRelation'

--- src/main/java/de/unistuttgart/vis/vita/model/EntityRelation.java
+++ src/main/java/de/unistuttgart/vis/vita/model/EntityRelation.java
@@ -1,5 +1,58 @@
 package de.unistuttgart.vis.vita.model;
 
-public class EntityRelation<T> {
+/**
+ * Represents a Relation between two Entities.
+ * 
+ * @author Marc Weise
+ * @version 0.1 30.07.2014
+ *
+ * @param <TEntity> - the type of the other entity
+ */
+public class EntityRelation<TEntity extends Entity> {
+
+  // constants
+  private static final int WEIGHT_MIN = 0;
+  private static final int WEIGHT_MAX = 1;
+
+  // attributes
+  private double weight;
+  private TEntity relatedEntity;
+
+  /**
+   * Returns how strong this relation is, returning a value between 0 (very weak) and 1 (very
+   * strong).
+   * 
+   * @return the weight of this relation
+   */
+  public double getWeight() {
+    return weight;
+  }
+
+  /**
+   * Sets the weight of this relation, indicating how strong it is.
+   * @param weight - value from 0.0 (very weak relation) to 1.0 (very strong relation)
+   */
+  public void setWeight(double weight) {
+    if (weight < WEIGHT_MIN || weight > WEIGHT_MAX) {
+      throw new IllegalArgumentException("weight of relation must be between 0 and 1!");
+    }
+    this.weight = weight;
+  }
+
+  /**
+   * @return entity which is target of this relation
+   */
+  public TEntity getRelatedEntity() {
+    return relatedEntity;
+  }
+ 
+  /**
+   * Sets the entity which is target of this relation.
+   * 
+   * @param relatedEntity - the related entity
+   */
+  public void setRelatedEntity(TEntity relatedEntity) {
+    this.relatedEntity = relatedEntity;
+  }
 
 }
