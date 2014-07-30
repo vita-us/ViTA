045118a88d2f9a4a2e5ccc4522a280e8ad85df18
Marc <weisemc@studi.informatik.uni-stuttgart.de>
2014-07-30 17:39:04 +0200

    Add model classes for WordCloud
    - Add and Implement class 'WordCloud'
    
    - Add and Implement class 'WordCloudItem'

---
+++ src/main/java/de/unistuttgart/vis/vita/model/WordCloud.java
@@ -0,0 +1,50 @@
+package de.unistuttgart.vis.vita.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Represents the model for a WordCloud. A WordCloud shows a set of words in different font sizes,
+ * depending on how often they are used in a document.
+ * 
+ * @author Marc Weise
+ * @version 0.1 30.07.2014
+ *
+ */
+public class WordCloud {
+
+  private Set<WordCloudItem> items;
+
+  /**
+   * Creates a new empty WordCloud.
+   */
+  public WordCloud() {
+    this.items = new HashSet<>();
+  }
+
+  /**
+   * Creates a new WordCloud with the given items.
+   * 
+   * @param pItems - the items for the new WordCloud
+   */
+  public WordCloud(Set<WordCloudItem> pItems) {
+    this.setItems(pItems);
+  }
+
+  /**
+   * @return a Set of all items of this WordCloud
+   */
+  public Set<WordCloudItem> getItems() {
+    return items;
+  }
+
+  /**
+   * Sets the items for this WordCloud
+   * 
+   * @param newItems
+   */
+  public void setItems(Set<WordCloudItem> newItems) {
+    this.items = newItems;
+  }
+
+}
