750c409fdffd7bce0183f5c802e58954a68f3b12
Marc <weisemc@studi.informatik.uni-stuttgart.de>
2014-07-30 17:03:47 +0200

    Implement 'Attribute' class and add 'AttributeType' enum to model package

--- src/main/java/de/unistuttgart/vis/vita/model/Attribute.java
+++ src/main/java/de/unistuttgart/vis/vita/model/Attribute.java
@@ -1,5 +1,101 @@
 package de.unistuttgart.vis.vita.model;
 
+import java.util.Set;
+
+/**
+ * Represents one attribute of an entity found in the document. This contains an id, type, content
+ * and a set of occurrences in the document.
+ * 
+ * @author Marc Weise
+ * @version 0.1 30.07.2014
+ */
 public class Attribute {
 
+  private int id;
+  private AttributeType type;
+  private String content;
+  private Set<TextSpan> occurrences;
+  
+  /**
+   * Creates a new attribute, setting all fields to default values.
+   */
+  public Attribute() {
+    
+  }
+  
+  /**
+   * Creates a new Attribute with given id, type and content.
+   * 
+   * @param pId - the id for the new attribute
+   * @param pType - the type of the new attribute, for example 'Name'
+   * @param pContent - the content of the new attribute, for example 'Bilbo Baggins'
+   */
+  public Attribute(int pId, AttributeType pType, String pContent) {
+    this.id = pId;
+    this.type = pType;
+    this.content = pContent;
+  }
+
+  /**
+   * @return the id of this Attribute
+   */
+  public int getId() {
+    return id;
+  }
+
+  /**
+   * Sets the id of this Attribute to the given value
+   * 
+   * @param newId - the new id for this Attribute
+   */
+  public void setId(int newId) {
+    this.id = newId;
+  }
+
+  /**
+   * @return the type of this Attribute
+   */
+  public AttributeType getType() {
+    return type;
+  }
+
+  /**
+   * @param newType the new type for the Attribute to set
+   */
+  public void setType(AttributeType newType) {
+    this.type = newType;
+  }
+
+  /**
+   * @return the content of this Attribute
+   */
+  public String getContent() {
+    return content;
+  }
+
+  /**
+   * Sets the content for this Attribute.
+   * 
+   * @param newContent - the new content for this Attribute
+   */
+  public void setContent(String newContent) {
+    this.content = newContent;
+  }
+
+  /**
+   * @return a Set of all occurrences in the text which are related to this Attribute
+   */
+  public Set<TextSpan> getOccurrences() {
+    return occurrences;
+  }
+
+  /**
+   * Sets the occurrences related to this Attribute.
+   * 
+   * @param occurrences - a set of all occurrences related to this Attribute
+   */
+  public void setOccurrences(Set<TextSpan> occurrences) {
+    this.occurrences = occurrences;
+  }
+  
 }
---
+++ src/main/java/de/unistuttgart/vis/vita/model/AttributeType.java
@@ -0,0 +1,14 @@
+package de.unistuttgart.vis.vita.model;
+
+/**
+ * Represents the type of an Attribute.
+ * 
+ * @author Marc Weise
+ * @version 0.1 30.07.2014
+ */
+public enum AttributeType {
+  AGE,
+  NAME, 
+  SIZE,
+  UNKNOWN
+}
