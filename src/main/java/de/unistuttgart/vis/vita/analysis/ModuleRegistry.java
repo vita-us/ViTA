01d99ae991a5bb90c62ed7d8761035e694d88c60
Felix <felixhuan.do@googlemail.com>
2014-07-31 18:24:20 +0200

    Add method.

--- src/main/java/de/unistuttgart/vis/vita/analysis/ModuleRegistry.java
+++ src/main/java/de/unistuttgart/vis/vita/analysis/ModuleRegistry.java
@@ -1,12 +1,16 @@
 package de.unistuttgart.vis.vita.analysis;
 
 public class ModuleRegistry {
-  private ModuleRegistry getDefaultRegistry() {
-
+  
+  public static ModuleRegistry getDefaultRegistry() {
+    ModuleRegistry reg1 = new ModuleRegistry();
     return null;
 
   }
 
+  public <TResult> void registerModule(Class<TResult> resultClass,
+      Class<IModule<TResult>> moduleClass) {
 
+  }
 
 }
