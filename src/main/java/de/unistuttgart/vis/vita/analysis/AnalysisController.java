9029de7dd5d179571b08ea1964292175a8a2be0e
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2014-07-31 17:49:36 +0200

    Add class comment.

--- src/main/java/de/unistuttgart/vis/vita/analysis/AnalysisController.java
+++ src/main/java/de/unistuttgart/vis/vita/analysis/AnalysisController.java
@@ -2,8 +2,12 @@ package de.unistuttgart.vis.vita.analysis;
 
 import java.nio.file.Path;
 
-
-
+/**
+ *  The AnalysisController resolves the dependencies of every module. It also provides a optimized
+ *  order corresponding to the computers cores.
+ *
+ *  The controller starts and cancels the analysis.
+ */
 public class AnalysisController {
 
   public AnalysisController(Model model) {
