df5434217b27a6279bab80f5233a2045eaa4abbe
Marc <weisemc@studi.informatik.uni-stuttgart.de>
2014-07-31 20:23:51 +0200

    Fix JUnit Test 'TextSpanTest' for issue #44
    - Rewrite tests for working with absolute offsets in TextPositions

--- src/main/java/de/unistuttgart/vis/vita/test/model/TextSpanTest.java
+++ src/main/java/de/unistuttgart/vis/vita/test/model/TextSpanTest.java
@@ -17,131 +17,77 @@ import de.unistuttgart.vis.vita.model.text.TextSpan;
  */
 public class TextSpanTest {
 
-  private static final int CHAPTER_3_2_TO_4_DIFF = 40000;
-  private static final int CHAPTER_3_2_TO_5_DIFF = 50000;
-  private static final int CHAPTER_1_LENGTH = 50000;
-  private static final int CHAPTER_1_OFFSET_1 = 3000;
+  private static final int OFFSET_1 = 10000;
+  private static final int OFFSET_2 = 70000;
+  private static final int OFFSET_3 = 95000;
+  private static final int DIFF = 25000;
 
-  private static final int CHAPTER_2_LENGTH = 80000;
-  private static final int CHAPTER_2_OFFSET_1 = 43000;
+  private Chapter chapter;
 
-  private static final int CHAPTER_3_LENGTH = 10000;
-  private static final int CHAPTER_3_OFFSET_1 = 1000;
-  private static final int CHAPTER_3_OFFSET_2 = 5000;
-  private static final int CHAPTER_3_OFFSET_DIFF = 4000;
+  private TextPosition pos1;
+  private TextPosition pos2;
+  private TextPosition pos3;
 
-  private static final int CHAPTER_4_LENGTH = 100000;
-  private static final int CHAPTER_4_OFFSET_1 = 35000;
-
-  private static final int CHAPTER_5_LENGTH = 70000;
-  private static final int CHAPTER_5_OFFSET_1 = 10000;
-
-  private Chapter chapter1;
-  private TextPosition chapter1Pos1;
-
-  private Chapter chapter2;
-  private TextPosition chapter2Pos1;
-
-  private Chapter chapter3;
-  private TextPosition chapter3Pos1;
-  private TextPosition chapter3Pos2;
-  private TextPosition chapter3PosStart;
-  private TextPosition chapter3PosEnd;
-
-  private Chapter chapter4;
-  private TextPosition chapter4Pos1;
-
-  private TextPosition chapter4PosEnd;
-  private Chapter chapter5;
-  private TextPosition chapter5Pos1;
-
+  /**
+   * Create positions and chapter.
+   */
   @Before
   public void setUp() {
+    chapter = new Chapter();
 
-    // chapter 1 and position
-    chapter1 = new Chapter();
-    chapter1.setNumber(1);
-    chapter1.setLength(CHAPTER_1_LENGTH);
-    chapter1Pos1 = new TextPosition(chapter1, CHAPTER_1_OFFSET_1);
-
-    // chapter 2 and position
-    chapter2 = new Chapter();
-    chapter2.setNumber(2);
-    chapter2.setLength(CHAPTER_2_LENGTH);
-    chapter2Pos1 = new TextPosition(chapter2, CHAPTER_2_OFFSET_1);
-
-    // chapter 3 and positions
-    chapter3 = new Chapter();
-    chapter3.setNumber(3);
-    chapter3.setLength(CHAPTER_3_LENGTH);
-    chapter3Pos1 = new TextPosition(chapter3, CHAPTER_3_OFFSET_1);
-    chapter3Pos2 = new TextPosition(chapter3, CHAPTER_3_OFFSET_2);
-    chapter3PosStart = new TextPosition(chapter3, 1);
-    chapter3PosEnd = new TextPosition(chapter3, CHAPTER_3_LENGTH);
-
-    // chapter 4 and position
-    chapter4 = new Chapter();
-    chapter4.setNumber(4);
-    chapter4.setLength(CHAPTER_4_LENGTH);
-    chapter4Pos1 = new TextPosition(chapter4, CHAPTER_4_OFFSET_1);
-    chapter4PosEnd = new TextPosition(chapter4, CHAPTER_4_LENGTH);
-
-    // chapter 5 and position
-    chapter5 = new Chapter();
-    chapter5.setNumber(5);
-    chapter5.setLength(CHAPTER_5_LENGTH);
-    chapter5Pos1 = new TextPosition(chapter5, CHAPTER_5_OFFSET_1);
+    pos1 = new TextPosition(chapter, OFFSET_1);
+    pos2 = new TextPosition(chapter, OFFSET_2);
+    pos3 = new TextPosition(chapter, OFFSET_3);
   }
 
+  /**
+   * Checks whether an exception is thrown if start position > end position
+   */
   @Test(expected = IllegalArgumentException.class)
-  public void testEndMultipleChaptersBeforeStart() {
-    new TextSpan(chapter3Pos1, chapter1Pos1);
+  public void testIllegalTextSpan() {
+    new TextSpan(pos2, pos1);
   }
 
+  /**
+   * Checks whether an IllegalArgumentException is thrown if fist parameter is null.
+   */
   @Test(expected = IllegalArgumentException.class)
-  public void testEndOneChapterBeforeStart() {
-    new TextSpan(chapter3Pos1, chapter2Pos1);
+  public void testFirstNullTextSpan() {
+    new TextSpan(null, pos1);
   }
 
+  /**
+   * Checks whether an IllegalArgumentException is thrown if the second parameter is null.
+   */
   @Test(expected = IllegalArgumentException.class)
-  public void testEndSameChapterBeforeStart() {
-    new TextSpan(chapter3Pos2, chapter3Pos1);
+  public void testSecondNullTextSpan() {
+    new TextSpan(pos1, null);
   }
 
+  /**
+   * Checks whether an IllegalArgumentException is thrown if both parameters are null
+   */
+  @Test(expected = IllegalArgumentException.class)
+  public void testBothNullTextSpan() {
+    new TextSpan(null, null);
+  }
+
+  /**
+   * Checks whether TextSpan is empty if start and end position are the same.
+   */
   @Test
-  public void testStartEqualsEnd() {
-    TextSpan emptyTestSpan = new TextSpan(chapter3Pos1, chapter3Pos1);
+  public void testEmptyTextSpan() {
+    TextSpan emptyTestSpan = new TextSpan(pos2, pos2);
     assertEquals(0, emptyTestSpan.getLength());
   }
 
+  /**
+   * Checks whether TextSpan length is calculated correctly.
+   */
   @Test
-  public void testEndSameChapterAfterStart() {
-    TextSpan testSpan = new TextSpan(chapter3Pos1, chapter3Pos2);
-    assertEquals(CHAPTER_3_OFFSET_DIFF, testSpan.getLength());
-  }
-
-  @Test
-  public void testWholeChapter() {
-    TextSpan wholeChapterSpan = new TextSpan(chapter3PosStart, chapter3PosEnd);
-    assertEquals(CHAPTER_3_LENGTH, wholeChapterSpan.getLength());
-  }
-
-  @Test
-  public void testEndNextChapterAfterStart() {
-    TextSpan testSpan = new TextSpan(chapter3Pos2, chapter4Pos1);
-    assertEquals(CHAPTER_3_2_TO_4_DIFF, testSpan.getLength());
-  }
-
-  @Test
-  public void testEndMultipleChaptersAfterStart() {
-    TextSpan testSpan = new TextSpan(chapter3Pos2, chapter5Pos1);
-    assertEquals(CHAPTER_3_2_TO_5_DIFF, testSpan.getLength());
-  }
-
-  @Test
-  public void testMultipleWholeChapters() {
-    TextSpan testSpan = new TextSpan(chapter3PosStart, chapter4PosEnd);
-    assertEquals(CHAPTER_3_LENGTH + CHAPTER_4_LENGTH, testSpan.getLength());
+  public void testTextSpan() {
+    TextSpan testTextSpan = new TextSpan(pos2, pos3);
+    assertEquals(DIFF, testTextSpan.getLength());
   }
 
 }
