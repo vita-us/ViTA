706c2cdf45b186ccb62abef514d2c5e24d0e2bed
Felix <felixhuan.do@googlemail.com>
2014-07-31 18:29:30 +0200

    Generate test class.

---
+++ src/test/java/de/unistuttgart/vis/vita/analysis/TestModuleRegistry.java
@@ -0,0 +1,17 @@
+package de.unistuttgart.vis.vita.analysis;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestModuleRegistry {
+  @Test
+  public void testRegisterModule() {
+    //TODO 
+    fail("TODO");
+  }
+ 
+}
+class Testclass<T> {
+  
+}
