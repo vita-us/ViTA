ada2a81e117cf521b4b1ce47175527ec231295e9
Marc <weisemc@studi.informatik.uni-stuttgart.de>
2014-08-01 14:19:14 +0200

    Add new model classes 'AnalysisProgress' and 'FeatureProgress' for issue #44
    - Create classes 'AnalysisProgress' and 'FeatureProgress'
    - Add private attributes
    - Add constructors
    - Generate getters and setters
    - Add JavaDoc comments

---
+++ src/main/java/de/unistuttgart/vis/vita/model/progress/AnalysisProgress.java
@@ -0,0 +1,129 @@
+package de.unistuttgart.vis.vita.model.progress;
+
+/**
+ * Represents the progress of a document analysis, holding the progresses of the separate software
+ * features.
+ * 
+ * @author Marc Weise
+ * @version 0.1 01.08.2014
+ */
+public class AnalysisProgress {
+
+  // attributes
+  private FeatureProgress graphViewProgress;
+  private FeatureProgress wordCloudProgress;
+  private FeatureProgress placesProgress;
+  private FeatureProgress personsProgress;
+  private FeatureProgress fingerPrintProgress;
+  private FeatureProgress textProgress;
+
+  /**
+   * Creates a new instance of AnalysisProgress, setting all FeatureProgresses to Zero and not
+   * ready.
+   */
+  public AnalysisProgress() {
+    this.graphViewProgress = new FeatureProgress();
+    this.wordCloudProgress = new FeatureProgress();
+    this.placesProgress = new FeatureProgress();
+    this.personsProgress = new FeatureProgress();
+    this.fingerPrintProgress = new FeatureProgress();
+    this.textProgress = new FeatureProgress();
+  }
+
+  /**
+   * @return the progress of the GraphView feature
+   */
+  public FeatureProgress getGraphViewProgress() {
+    return graphViewProgress;
+  }
+
+  /**
+   * Sets the progress for the GraphView feature.
+   *
+   * @param newGraphViewProgress - the new progress of the GraphView feature
+   */
+  public void setGraphViewProgress(FeatureProgress newGraphViewProgress) {
+    this.graphViewProgress = newGraphViewProgress;
+  }
+
+  /**
+   * @return the progress of the WordCloud feature
+   */
+  public FeatureProgress getWordCloudProgress() {
+    return wordCloudProgress;
+  }
+
+  /**
+   * Sets the progress of the WordCloud feature.
+   *
+   * @param newWordCloudProgress - the new progress of the WordCloud feature
+   */
+  public void setWordCloudProgress(FeatureProgress newWordCloudProgress) {
+    this.wordCloudProgress = newWordCloudProgress;
+  }
+
+  /**
+   * @return the progress of the Places feature
+   */
+  public FeatureProgress getPlacesProgress() {
+    return placesProgress;
+  }
+
+  /**
+   * Sets the progress for the Places feature.
+   *
+   * @param newPlacesProgress - the new progress for the Places feature
+   */
+  public void setPlacesProgress(FeatureProgress newPlacesProgress) {
+    this.placesProgress = newPlacesProgress;
+  }
+
+  /**
+   * @return the progress of the Persons feature
+   */
+  public FeatureProgress getPersonsProgress() {
+    return personsProgress;
+  }
+
+  /**
+   * Sets the progress of the Persons feature.
+   *
+   * @param newPersonsProgress - the new progress for the Persons feature
+   */
+  public void setPersonsProgress(FeatureProgress newPersonsProgress) {
+    this.personsProgress = newPersonsProgress;
+  }
+
+  /**
+   * @return the progress of the FingerPrint feature
+   */
+  public FeatureProgress getFingerPrintProgress() {
+    return fingerPrintProgress;
+  }
+
+  /**
+   * Sets the progress of the FingerPrint feature.
+   *
+   * @param newFingerPrintProgress - the new progress for the FingerPrint feature
+   */
+  public void setFingerPrintProgress(FeatureProgress newFingerPrintProgress) {
+    this.fingerPrintProgress = newFingerPrintProgress;
+  }
+
+  /**
+   * @return the progress of the text feature
+   */
+  public FeatureProgress getTextProgress() {
+    return textProgress;
+  }
+
+  /**
+   * Sets the progress of the text feature.
+   *
+   * @param newTextProgress - the new progress for the text feature
+   */
+  public void setTextProgress(FeatureProgress newTextProgress) {
+    this.textProgress = newTextProgress;
+  }
+
+}
---
+++ src/main/java/de/unistuttgart/vis/vita/model/progress/FeatureProgress.java
@@ -0,0 +1,65 @@
+package de.unistuttgart.vis.vita.model.progress;
+
+/**
+ * Represents the progress of the analysis for a specific feature of the software.
+ * 
+ * @author Marc Weise
+ * @version 0.1 01.08.2014
+ */
+public class FeatureProgress {
+  
+  // attributes
+  private double progress;
+  private boolean isReady;
+  
+  /**
+   * Creates a new instance of Feature Progress, setting the progress to 0 and ready flag to false.
+   */
+  public FeatureProgress() {
+    this(0.0, false);
+  }
+  
+  /**
+   * Creates a new instance of FeatureProgress with the given progress value and ready flag.
+   * 
+   * @param pProgress - the progress value between 0.0 and 1.0
+   * @param pReady - flag whether the feature this refers to is ready or not
+   */
+  public FeatureProgress(double pProgress, boolean pReady) {
+    this.progress = pProgress;
+    this.isReady = pReady;
+  }
+
+  /**
+   * @return the progress
+   */
+  public double getProgress() {
+    return progress;
+  }
+
+  /**
+   * Sets the progress.
+   *
+   * @param progress - the progress to set
+   */
+  public void setProgress(double progress) {
+    this.progress = progress;
+  }
+
+  /**
+   * @return the isReady
+   */
+  public boolean isReady() {
+    return isReady;
+  }
+
+  /**
+   * Sets the isReady.
+   *
+   * @param isReady - the isReady to set
+   */
+  public void setReady(boolean isReady) {
+    this.isReady = isReady;
+  }
+  
+}
