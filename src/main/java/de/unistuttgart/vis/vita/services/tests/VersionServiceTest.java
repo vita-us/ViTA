c2b03d5e9e534fa03a9c984a342aaa2df9ee5ba8
Jan Melcher <info@yogularm.de>
2014-08-01 23:54:29 +0200

    #47 VersionServiceTest uses real VersionService

--- src/main/java/de/unistuttgart/vis/vita/services/tests/VersionServiceTest.java
+++ src/main/java/de/unistuttgart/vis/vita/services/tests/VersionServiceTest.java
@@ -1,43 +1,27 @@
 package de.unistuttgart.vis.vita.services.tests;
 
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
-import javax.ws.rs.core.MediaType;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
 
+import de.unistuttgart.vis.vita.services.VersionService;
 import de.unistuttgart.vis.vita.services.responses.VersionInfo;
 
 
 public class VersionServiceTest extends JerseyTest {
-
-  @Path("versiontest")
-  public static class VersionResource {
-    @GET
-    public VersionInfo getApiAsJSON() {
-      VersionInfo versionInfo = new VersionInfo();
-      versionInfo.setApi("1.0-SNAPSHOT");
-      return versionInfo;
-
-    }
-  }
-
   @Override
   protected Application configure() {
-    return new ResourceConfig(VersionResource.class);
+    return new ResourceConfig(VersionService.class);
   }
 
   @Test
   public void test() {
-    VersionInfo versionInfoActual = target("versiontest").request().get(VersionInfo.class);
-    // System.out.println(target("versiontest").request().get().getStatusInfo().toString());
+    VersionInfo versionInfoActual = target("version").request().get(VersionInfo.class);
     VersionInfo versionInfoExpected = new VersionInfo();
     versionInfoExpected.setApi("1.0-SNAPSHOT");
     assertEquals(versionInfoExpected.getApi(), versionInfoActual.getApi());
