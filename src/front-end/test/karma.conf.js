43b1c08c3f8f92f1fef20835469280a694018710
Paul <st103454@stud.uni-stuttgart.de>
2014-08-02 00:43:02 +0200

    Add angular-dependencies in karma configuration issue #56

--- src/front-end/test/karma.conf.js
+++ src/front-end/test/karma.conf.js
@@ -4,7 +4,10 @@ module.exports = function(config) {
     basePath: '../',
 
     files: ['app/bower_components/angular/angular.js',
-        'app/bower_components/angular-route/angular-route.js', 'app/js/**/*.js',
+        'app/bower_components/angular-route/angular-route.js',
+        'app/bower_components/angular-resource/angular-resource.js',
+        'app/bower_components/angular-mocks/angular-mocks.js',		
+	    'app/js/**/*.js',
         'test/unit/**/*.js'],
 
     autoWatch: true,
