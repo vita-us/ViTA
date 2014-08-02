a3a7ddf387f9579096762f7bbdbbe81c35caf31c
Felix <felixhuan.do@googlemail.com>
2014-08-02 19:09:07 +0200

    Add method for ModuleResultProvider.

--- src/main/java/de/unistuttgart/vis/vita/analysis/ModuleRegistry.java
+++ src/main/java/de/unistuttgart/vis/vita/analysis/ModuleRegistry.java
@@ -1,16 +1,34 @@
 package de.unistuttgart.vis.vita.analysis;
 
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ModuleRegistry {
-  
+  Map<Class, Class> registry;
+  IModule module;
+
+  public ModuleRegistry() {
+    this.registry = new HashMap<>();
+    this.module = module;
+  }
+
   public static ModuleRegistry getDefaultRegistry() {
-    ModuleRegistry reg1 = new ModuleRegistry();
+
+    return new ModuleRegistry();
+
+  }
+
+  public List<Class> getModules() {
+
     return null;
 
   }
 
+  //
   public <TResult> void registerModule(Class<TResult> resultClass,
       Class<IModule<TResult>> moduleClass) {
-
+    registry.put(resultClass, moduleClass);
   }
 
 }
