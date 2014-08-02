ecac11d44f9feccedc4eaddd5d2448a4c57df3d3
Marvin Wyrich <st100245@stud.uni-stuttgart.de>
2014-08-02 13:47:23 +0200

    add header and corresponding partials

--- src/front-end/app/js/app.js
+++ src/front-end/app/js/app.js
@@ -7,6 +7,15 @@
     $routeProvider.when('/documents', {
       templateUrl: 'partials/documents.html',
       controller: ''
+    }).when('/settings', {
+      templateUrl: 'partials/settings.html',
+      controller: ''
+    }).when('/tutorial', {
+      templateUrl: 'partials/tutorial.html',
+      controller: ''
+    }).when('/about', {
+      templateUrl: 'partials/about.html',
+      controller: ''
     }).when('/documents/:documentId/overview', {
       templateUrl: 'partials/overview.html',
       controller: ''
