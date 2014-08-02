8decddb66e0217ef17e1a43094768068eca5017c
Marvin Wyrich <st100245@stud.uni-stuttgart.de>
2014-08-02 15:31:40 +0200

    tabs in header get active class on click

--- src/front-end/app/js/app.js
+++ src/front-end/app/js/app.js
@@ -26,4 +26,16 @@
     });
   }]);
 
+  app.controller("PanelController", function() {
+    this.tab = 1;
+    
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
+
 })(angular);
