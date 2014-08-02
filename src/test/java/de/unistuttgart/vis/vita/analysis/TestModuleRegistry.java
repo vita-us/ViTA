19b63bcb59d42dcc7edba546d756a0208be68624
Eduard <marbaced@studi.informatik.uni-stuttgart.de>
2014-08-02 19:07:11 +0200

    Write test class for >ModuleRegistry<

--- src/test/java/de/unistuttgart/vis/vita/analysis/TestModuleRegistry.java
+++ src/test/java/de/unistuttgart/vis/vita/analysis/TestModuleRegistry.java
@@ -1,17 +1,28 @@
 package de.unistuttgart.vis.vita.analysis;
 
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
 
 public class TestModuleRegistry {
+
+  private static ModuleRegistry registry;
+
+  @BeforeClass
+  public static void initialize() {
+    registry = new ModuleRegistry();
+  }
+
   @Test
   public void testRegisterModule() {
-    //TODO 
-    fail("TODO");
+    MockModule module = new MockModule();
+    registry.registerModule(String.class, MockModule.class);
+
+    assertThat(registry.getSize(), is(1));
+    assertEquals(registry.getModuleClassFor(String.class), MockModule.class);
   }
- 
-}
-class Testclass<T> {
-  
+
 }
