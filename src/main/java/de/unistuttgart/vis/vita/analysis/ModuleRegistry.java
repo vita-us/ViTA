4e7eb2b12c9f3973954b34ea406d915067e843db
Eduard <marbaced@studi.informatik.uni-stuttgart.de>
2014-08-02 19:06:02 +0200

    Implement the registry.
    With Felix.

--- src/main/java/de/unistuttgart/vis/vita/analysis/ModuleRegistry.java
+++ src/main/java/de/unistuttgart/vis/vita/analysis/ModuleRegistry.java
@@ -1,34 +1,74 @@
 package de.unistuttgart.vis.vita.analysis;
 
-import java.util.List;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+/**
+ * A registry for saving all modules which should be executed on a document.
+ *
+ */
 public class ModuleRegistry {
-  Map<Class, Class> registry;
-  IModule module;
 
+  private Map<Class<?>, Class<? extends IModule<?>>> registry;
+
+  /**
+   * Make a new instance of the registry which is empty.
+   */
   public ModuleRegistry() {
-    this.registry = new HashMap<>();
-    this.module = module;
+    registry = new HashMap<>();
+
   }
 
+  /**
+   * Returns the default registry which is an empty one.
+   * 
+   * @return Empty registry.
+   */
   public static ModuleRegistry getDefaultRegistry() {
-
     return new ModuleRegistry();
-
   }
 
-  public List<Class> getModules() {
+  /**
+   * Registers a new module to the registry.
+   * 
+   * @param resultClass The class type of the module.
+   * @param moduleClass The module class which will be registered.
+   */
+  public <TResult, TModule extends IModule<TResult>> void registerModule(
+      Class<TResult> resultClass, Class<TModule> moduleClass) {
+    registry.put(resultClass, moduleClass);
+  }
 
+  /**
+   * Returns the class of the module for given result class.
+   * 
+   * @param resultClass The result class to look for.
+   * @return Class of the correct module if found else null.
+   */
+  public <TResult, TModule extends IModule<TResult>> Class<TModule> getModuleClassFor(
+      Class<TResult> resultClass) {
+    if (registry.get(resultClass) != null) {
+      return (Class<TModule>) registry.get(resultClass);
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * @return The number of registered modules.
+   */
+  public int getSize() {
+    return registry.size();
+  }
+
+  /**
+   * @return The registered modules in a list.
+   */
+  public List<Class<? extends IModule<?>>> getModules() {
+    // TODO
     return null;
 
-  }
-
-  //
-  public <TResult> void registerModule(Class<TResult> resultClass,
-      Class<IModule<TResult>> moduleClass) {
-    registry.put(resultClass, moduleClass);
   }
 
 }
