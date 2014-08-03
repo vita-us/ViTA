0da43da6197f0da49a9407332d73f9b04021efd2
Eduard <marbaced@studi.informatik.uni-stuttgart.de>
2014-08-03 22:58:15 +0200

    First implementation of the result provider.
    Not really sure about the correct implementation!

--- src/main/java/de/unistuttgart/vis/vita/analysis/ModuleResultProvider.java
+++ src/main/java/de/unistuttgart/vis/vita/analysis/ModuleResultProvider.java
@@ -1,10 +1,23 @@
 package de.unistuttgart.vis.vita.analysis;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This class returns the result for a given module class.
+ * TODO über die genaue Implementierung nochmal reden!!
  *
  */
 public class ModuleResultProvider {
+  
+  private Map<Class<?>, ?> resultsModule;
+  
+  /**
+   * New instance of the result provider.
+   */
+  public ModuleResultProvider() {
+    resultsModule = new HashMap<>();
+  }
 
   /**
    * Return the result of the module class.
@@ -13,7 +26,6 @@ public class ModuleResultProvider {
    * @return The result.
    */
   private <T> T getResultFor(Class<T> module) {
-
-    return null;
+    return (T) resultsModule.get(module);
   }
 }
