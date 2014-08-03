cb6f6f766870f850b9028e6c564b6185d6792314
Eduard <marbaced@studi.informatik.uni-stuttgart.de>
2014-08-03 22:35:03 +0200

    Refactor test.

--- src/test/java/de/unistuttgart/vis/vita/analysis/TestModuleRegistry.java
+++ src/test/java/de/unistuttgart/vis/vita/analysis/TestModuleRegistry.java
@@ -5,24 +5,25 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-import static org.hamcrest.core.Is.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class TestModuleRegistry {
 
   private static ModuleRegistry registry;
 
   @BeforeClass
-  public static void initialize() {
+  public static void setUpBeforeClass() throws Exception {
     registry = new ModuleRegistry();
   }
 
   @Test
   public void testRegisterModule() {
     MockModule module = new MockModule();
-    registry.registerModule(String.class, MockModule.class);
+    registry.registerModule(String.class, module.getClass());
 
     assertThat(registry.getSize(), is(1));
-    assertEquals(registry.getModuleClassFor(String.class), MockModule.class);
+    assertEquals(registry.getModuleClassFor(String.class), module.getClass());
   }
 
 }
