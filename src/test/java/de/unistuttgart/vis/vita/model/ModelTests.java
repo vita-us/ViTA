e9d9bc3f2bda33aa78a8083796604509eaeca3ed
Marc <weisemc@studi.informatik.uni-stuttgart.de>
2014-08-05 23:05:25 +0200

    Add a new JUnit test suite 'ModelTests' and added all model class test cases for issue #70.

---
+++ src/test/java/de/unistuttgart/vis/vita/model/ModelTests.java
@@ -0,0 +1,20 @@
+package de.unistuttgart.vis.vita.model;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import de.unistuttgart.vis.vita.model.progress.FeatureProgressTest;
+
+/**
+ * Test suite which runs all tests related to model classes.
+ */
+@RunWith(Suite.class)
+@SuiteClasses({EntityRelationTest.class, TextPositionTest.class, TextSpanTest.class,
+    FeatureProgressTest.class})
+public class ModelTests {
+
+  // hidden constructor
+  private ModelTests() {}
+
+}
