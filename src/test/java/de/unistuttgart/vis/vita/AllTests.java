297ad7e9d8e0a7df06a89a3b07860217c535963c
Marc <weisemc@studi.informatik.uni-stuttgart.de>
2014-08-05 23:11:52 +0200

    Include new JUnit test suite 'ModelTests' into test suite 'AllTests' for issue #70.

--- src/test/java/de/unistuttgart/vis/vita/AllTests.java
+++ src/test/java/de/unistuttgart/vis/vita/AllTests.java
@@ -1,16 +1,17 @@
 package de.unistuttgart.vis.vita;
 
-import de.unistuttgart.vis.vita.services.ServiceTests;
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+
+import de.unistuttgart.vis.vita.model.ModelTests;
+import de.unistuttgart.vis.vita.services.ServiceTests;
 
 /**
  * A suite containing all back-end tests
  */
 @RunWith(Suite.class)
-@SuiteClasses({ServiceTests.class})
+@SuiteClasses({ServiceTests.class, ModelTests.class})
 public class AllTests {
 
   // conform checkstyle rule HideUtilityClassConstructor
