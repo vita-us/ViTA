4aaec1068c5db9f3d16adb7319c73fdbf3a49499
Marc <weisemc@studi.informatik.uni-stuttgart.de>
2014-08-09 15:10:37 +0200

    Add JPA annotations and new field 'id' to class 'EntityRelation'.
    - Add annotation 'Entity'
    - Add new field 'id'
    - Add Getter and Setter for the new field
    - Add annotations 'GeneratedValue' and 'Id' to new field
    - Add annotation 'ManyTo One' to field 'relatedEntity'

--- src/main/java/de/unistuttgart/vis/vita/model/entity/EntityRelation.java
+++ src/main/java/de/unistuttgart/vis/vita/model/entity/EntityRelation.java
@@ -1,10 +1,15 @@
 package de.unistuttgart.vis.vita.model.entity;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 /**
  * Represents a Relation between two Entities.
  *
  * @param <E> - the type of the other entity
  */
+@javax.persistence.Entity
 public class EntityRelation<E> {
 
   // constants
@@ -12,8 +17,29 @@ public class EntityRelation<E> {
   private static final int WEIGHT_MAX = 1;
 
   // attributes
+  @GeneratedValue
+  @Id
+  private int id;
   private double weight;
+  
+  @ManyToOne
   private E relatedEntity;
+
+  /**
+   * @return the id of this EntityRelation
+   */
+  public int getId() {
+    return id;
+  }
+
+  /**
+   * Sets the id for this EntityRelation
+   *
+   * @param newId - the new id for the EntityRelation
+   */
+  public void setId(int newId) {
+    this.id = newId;
+  }
 
   /**
    * Returns how strong this relation is, returning a value between 0 (very weak) and 1 (very
