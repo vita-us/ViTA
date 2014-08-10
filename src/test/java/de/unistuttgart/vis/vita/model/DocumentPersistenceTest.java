0941497b86c894ea5786cd973a13630e6c504991
Marc <weisemc@studi.informatik.uni-stuttgart.de>
2014-08-10 08:31:52 +0200

    Extend JUnit test case 'DocumentPersistenceTest' for issue #45.
    - Check whether document metadata is persisted correctly
    - Check whether document metrics are persisted correctly

--- src/test/java/de/unistuttgart/vis/vita/model/DocumentPersistenceTest.java
+++ src/test/java/de/unistuttgart/vis/vita/model/DocumentPersistenceTest.java
@@ -3,6 +3,7 @@ package de.unistuttgart.vis.vita.model;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -21,8 +22,16 @@ import de.unistuttgart.vis.vita.model.document.DocumentMetrics;
  */
 public class DocumentPersistenceTest {
 
-  // constants
+  // test data
   private static final String TEST_DOCUMENT_AUTHOR = "J. R. R. Tolkien";
+  private static final int TEST_DOCUMENT_CHAPTER_COUNT = 22;
+  private static final int TEST_DOCUMENT_CHARACTER_COUNT = 83920212;
+  private static final String TEST_DOCUMENT_EDITION = "First edition";
+  private static final String TEST_DOCUMENT_GENRE = "Fantasy";
+  private static final int TEST_DOCUMENT_PERSON_COUNT = 42;
+  private static final int TEST_DOCUMENT_PLACE_COUNT = 13;
+  private static final int TEST_DOCUMENT_PUPLICATION_YEAR = 1959;
+  private static final String TEST_DOCUMENT_PUBLISHER = "George Allen & Unwin";
   private static final String TEST_DOCUMENT_TITLE =
       "The Lord of the Rings - The Fellowship of the Ring";
   
@@ -56,10 +65,7 @@ public class DocumentPersistenceTest {
     em.close();
 
     // read document from database
-    em = model.getEntityManager();
-    em.getTransaction().begin();
-    List<Document> docs = em.createQuery("from Document", Document.class).getResultList();
-    em.getTransaction().commit();
+    List<Document> docs = readFromDb();
     
     // check if all data is correct
     assertEquals(1, docs.size());
@@ -68,21 +74,50 @@ public class DocumentPersistenceTest {
     
     DocumentMetadata savedMetadata = savedDocument.getMetadata();
     assertNotNull(savedMetadata);
-    assertEquals(TEST_DOCUMENT_TITLE, savedMetadata.getTitle());
     assertEquals(TEST_DOCUMENT_AUTHOR, savedMetadata.getAuthor());
+    assertEquals(TEST_DOCUMENT_EDITION, savedMetadata.getEdition());
+    assertEquals(TEST_DOCUMENT_GENRE, savedMetadata.getGenre());
+    assertEquals(TEST_DOCUMENT_TITLE, savedMetadata.getTitle());
+    assertEquals(TEST_DOCUMENT_PUBLISHER, savedMetadata.getPublisher());
+    Calendar savedPublishYear = Calendar.getInstance();
+    savedPublishYear.setTime(savedMetadata.getPublishYear());
+    assertEquals(TEST_DOCUMENT_PUPLICATION_YEAR, savedPublishYear.get(Calendar.YEAR));
+    
+    DocumentMetrics savedMetrics = savedDocument.getMetrics();
+    assertNotNull(savedMetrics);
+    assertEquals(TEST_DOCUMENT_CHAPTER_COUNT, savedMetrics.getChapterCount());
+    assertEquals(TEST_DOCUMENT_CHARACTER_COUNT, savedMetrics.getCharacterCount());
+    assertEquals(TEST_DOCUMENT_PERSON_COUNT, savedMetrics.getPersonCount());
+    assertEquals(TEST_DOCUMENT_PLACE_COUNT, savedMetrics.getPlaceCount());
   }
 
   /**
-   * Creates a test document using constants.
+   * Creates a test document using test data stored in constants.
    * 
    * @return a test document
    */
   private Document createTestDocument() {
     Document testDoc = new Document();
     DocumentContent content = new DocumentContent();
-    DocumentMetadata metaData = new DocumentMetadata(TEST_DOCUMENT_TITLE, TEST_DOCUMENT_AUTHOR);
-    DocumentMetrics metrics = new DocumentMetrics();
     
+    // create and set up metadata
+    DocumentMetadata metaData = new DocumentMetadata(TEST_DOCUMENT_TITLE, TEST_DOCUMENT_AUTHOR);
+    metaData.setEdition(TEST_DOCUMENT_EDITION);
+    metaData.setGenre(TEST_DOCUMENT_GENRE);
+    metaData.setPublisher(TEST_DOCUMENT_PUBLISHER);
+    Calendar publishYear = Calendar.getInstance();
+    publishYear.clear();
+    publishYear.set(TEST_DOCUMENT_PUPLICATION_YEAR, Calendar.JANUARY, 1);
+    metaData.setPublishYear(publishYear.getTime());
+    
+    // create and set up metrics
+    DocumentMetrics metrics = new DocumentMetrics();
+    metrics.setChapterCount(TEST_DOCUMENT_CHAPTER_COUNT);
+    metrics.setPersonCount(TEST_DOCUMENT_PERSON_COUNT);
+    metrics.setPlaceCount(TEST_DOCUMENT_PLACE_COUNT);
+    metrics.setCharacterCount(TEST_DOCUMENT_CHARACTER_COUNT);
+    
+    // set up document
     testDoc.setContent(content);
     testDoc.setMetadata(metaData);
     testDoc.setMetrics(metrics);
@@ -90,9 +125,25 @@ public class DocumentPersistenceTest {
     return testDoc;
   }
 
+  /**
+   * Reads a List of Documents from the database.
+   * 
+   * @return list of Documents
+   */
+  private List<Document> readFromDb() {
+    em = model.getEntityManager();
+    em.getTransaction().begin();
+    List<Document> docs = em.createQuery("from Document", Document.class).getResultList();
+    em.getTransaction().commit();
+    return docs;
+  }
+
+  /**
+   * Closes the EntityManager.
+   */
   @After
   public void tearDown() {
     em.close();
   }
-
+  
 }
