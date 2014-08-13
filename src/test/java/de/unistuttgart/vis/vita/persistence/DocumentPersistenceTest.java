5ecb7ca849cc8aba49dd1210ffe713bf965fa447
Marc <weisemc@studi.informatik.uni-stuttgart.de>
2014-08-13 12:14:08 +0200

    Expand 'DocumentPersistenceTest' to check multiple docs & named queries.
    - Add a new test method to test persistence of multiple documents
    - Add a new test method to test named queries of model class
      'Document'

--- src/test/java/de/unistuttgart/vis/vita/persistence/DocumentPersistenceTest.java
+++ src/test/java/de/unistuttgart/vis/vita/persistence/DocumentPersistenceTest.java
@@ -2,11 +2,13 @@ package de.unistuttgart.vis.vita.persistence;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import org.junit.After;
 import org.junit.Before;
@@ -33,9 +35,11 @@ public class DocumentPersistenceTest {
   private static final int TEST_DOCUMENT_PLACE_COUNT = 13;
   private static final int TEST_DOCUMENT_PUPLICATION_YEAR = 1959;
   private static final String TEST_DOCUMENT_PUBLISHER = "George Allen & Unwin";
-  private static final String TEST_DOCUMENT_TITLE =
+  private static final String TEST_DOCUMENT_TITLE_1 =
       "The Lord of the Rings - The Fellowship of the Ring";
-  
+  private static final String TEST_DOCUMENT_TITLE_2 = 
+      "The Lord of the Rings - The Two Towers";
+
   // attributes
   private EntityManager em;
   private Model model;
@@ -58,7 +62,7 @@ public class DocumentPersistenceTest {
   @Test
   public void testPersistOneDocument() {
     // create and set up a new test document
-    Document doc = createTestDocument();
+    Document doc = createTestDocument(TEST_DOCUMENT_TITLE_1);
 
     // persist test document
     em.persist(doc);
@@ -67,24 +71,28 @@ public class DocumentPersistenceTest {
 
     // read document from database
     List<Document> docs = readFromDb();
-    
+
     // check if all data is correct
     assertEquals(1, docs.size());
     Document savedDocument = docs.get(0);
     assertNotNull(savedDocument);
-    
-    DocumentMetadata savedMetadata = savedDocument.getMetadata();
+
+    checkData(savedDocument);
+  }
+
+  private void checkData(Document documentToCheck) {
+    DocumentMetadata savedMetadata = documentToCheck.getMetadata();
     assertNotNull(savedMetadata);
     assertEquals(TEST_DOCUMENT_AUTHOR, savedMetadata.getAuthor());
     assertEquals(TEST_DOCUMENT_EDITION, savedMetadata.getEdition());
     assertEquals(TEST_DOCUMENT_GENRE, savedMetadata.getGenre());
-    assertEquals(TEST_DOCUMENT_TITLE, savedMetadata.getTitle());
+    assertEquals(TEST_DOCUMENT_TITLE_1, savedMetadata.getTitle());
     assertEquals(TEST_DOCUMENT_PUBLISHER, savedMetadata.getPublisher());
     Calendar savedPublishYear = Calendar.getInstance();
     savedPublishYear.setTime(savedMetadata.getPublishYear());
     assertEquals(TEST_DOCUMENT_PUPLICATION_YEAR, savedPublishYear.get(Calendar.YEAR));
-    
-    DocumentMetrics savedMetrics = savedDocument.getMetrics();
+
+    DocumentMetrics savedMetrics = documentToCheck.getMetrics();
     assertNotNull(savedMetrics);
     assertEquals(TEST_DOCUMENT_CHAPTER_COUNT, savedMetrics.getChapterCount());
     assertEquals(TEST_DOCUMENT_CHARACTER_COUNT, savedMetrics.getCharacterCount());
@@ -97,12 +105,12 @@ public class DocumentPersistenceTest {
    * 
    * @return a test document
    */
-  private Document createTestDocument() {
+  private Document createTestDocument(String title) {
     Document testDoc = new Document();
     DocumentContent content = new DocumentContent();
-    
+
     // create and set up metadata
-    DocumentMetadata metaData = new DocumentMetadata(TEST_DOCUMENT_TITLE, TEST_DOCUMENT_AUTHOR);
+    DocumentMetadata metaData = new DocumentMetadata(title, TEST_DOCUMENT_AUTHOR);
     metaData.setEdition(TEST_DOCUMENT_EDITION);
     metaData.setGenre(TEST_DOCUMENT_GENRE);
     metaData.setPublisher(TEST_DOCUMENT_PUBLISHER);
@@ -110,19 +118,19 @@ public class DocumentPersistenceTest {
     publishYear.clear();
     publishYear.set(TEST_DOCUMENT_PUPLICATION_YEAR, Calendar.JANUARY, 1);
     metaData.setPublishYear(publishYear.getTime());
-    
+
     // create and set up metrics
     DocumentMetrics metrics = new DocumentMetrics();
     metrics.setChapterCount(TEST_DOCUMENT_CHAPTER_COUNT);
     metrics.setPersonCount(TEST_DOCUMENT_PERSON_COUNT);
     metrics.setPlaceCount(TEST_DOCUMENT_PLACE_COUNT);
     metrics.setCharacterCount(TEST_DOCUMENT_CHARACTER_COUNT);
-    
+
     // set up document
     testDoc.setContent(content);
     testDoc.setMetadata(metaData);
     testDoc.setMetrics(metrics);
-    
+
     return testDoc;
   }
 
@@ -138,6 +146,67 @@ public class DocumentPersistenceTest {
     em.getTransaction().commit();
     return docs;
   }
+  
+  /**
+   * Checks whether multiple documents can be persisted.
+   */
+  @Test
+  public void testPersistMultipleDocuments() {
+    Document d1 = createTestDocument(TEST_DOCUMENT_TITLE_1);
+    Document d2 = createTestDocument(TEST_DOCUMENT_TITLE_2);
+    
+    em.persist(d1);
+    em.persist(d2);
+    em.getTransaction().commit();
+    em.close();
+    
+    // read document from database
+    List<Document> docs = readFromDb();
+
+    // check if all data is correct
+    assertEquals(2, docs.size());
+  }
+
+  /**
+   * Checks the named queries for documents.
+   */
+  @Test
+  public void testNamedQueries() {
+    Document doc = createTestDocument(TEST_DOCUMENT_TITLE_1);
+
+    em.persist(doc);
+    em.getTransaction().commit();
+    em.close();
+
+    em = model.getEntityManager();
+    em.getTransaction().begin();
+    TypedQuery<Document> allQ = em.createNamedQuery("Document.findAllDocuments", Document.class);
+    List<Document> allResults = allQ.getResultList();
+    em.getTransaction().commit();
+
+    // check whether data is correct
+    assertTrue(allResults.size() > 0);
+    Document readDocument = allResults.get(0);
+    checkData(readDocument);
+
+    String id = readDocument.getId();
+
+    em.getTransaction().begin();
+    TypedQuery<Document> idQ = em.createNamedQuery("Document.findDocumentById", Document.class);
+    idQ.setParameter("documentId", id);
+    Document idResult = idQ.getSingleResult();
+    em.getTransaction().commit();
+
+    checkData(idResult);
+
+    em.getTransaction().begin();
+    TypedQuery<Document> titleQ = em.createNamedQuery("Document.findDocumentByTitle", Document.class);
+    titleQ.setParameter("documentTitle", TEST_DOCUMENT_TITLE_1);
+    Document titleResult = titleQ.getSingleResult();
+    em.getTransaction().commit();
+    
+    checkData(titleResult);
+  }
 
   /**
    * Closes the EntityManager.
@@ -146,5 +215,5 @@ public class DocumentPersistenceTest {
   public void tearDown() {
     em.close();
   }
-  
+
 }
