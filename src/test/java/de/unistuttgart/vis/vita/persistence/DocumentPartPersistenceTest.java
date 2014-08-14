52ae509bcff9de878be98c4b84663b274e1ceb51
Marc <weisemc@studi.informatik.uni-stuttgart.de>
2014-08-15 01:25:05 +0200

    Expand persistence test to check Named Queries of 'DocumentPart'.
    - Add new test method to JUnit test case 'DocumentPartPersistenceTest'
      checking Named Queries working correctly

--- src/test/java/de/unistuttgart/vis/vita/persistence/DocumentPartPersistenceTest.java
+++ src/test/java/de/unistuttgart/vis/vita/persistence/DocumentPartPersistenceTest.java
@@ -2,6 +2,7 @@ package de.unistuttgart.vis.vita.persistence;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
@@ -24,9 +25,7 @@ public class DocumentPartPersistenceTest extends AbstractPersistenceTest {
   @Test
   public void testPersistOnePart() {
     // first set up DocumentPart
-    DocumentPart part = new DocumentPart();
-    part.setNumber(TEST_DOCUMENT_PART_NUMBER);
-    part.setTitle(TEST_DOCUMENT_PART_TITLE);
+    DocumentPart part = createTestDocumentPart();
     
     // persist this part
     em.persist(part);
@@ -38,9 +37,30 @@ public class DocumentPartPersistenceTest extends AbstractPersistenceTest {
     // check whether data is correct
     assertEquals(1, parts.size());
     DocumentPart readPart = parts.get(0);
+    checkData(readPart);
+  }
+
+  /**
+   * Checks whether given data is not <code>null</code> and includes the test data.
+   * 
+   * @param readPart - the document which should be checked
+   */
+  private void checkData(DocumentPart readPart) {
     assertNotNull(readPart);
     assertEquals(TEST_DOCUMENT_PART_NUMBER, readPart.getNumber());
     assertEquals(TEST_DOCUMENT_PART_TITLE, readPart.getTitle());
+  }
+  
+  /**
+   * Creates a test document part including a test number and title.
+   * 
+   * @return test document part
+   */
+  private DocumentPart createTestDocumentPart() {
+    DocumentPart part = new DocumentPart();
+    part.setNumber(TEST_DOCUMENT_PART_NUMBER);
+    part.setTitle(TEST_DOCUMENT_PART_TITLE);
+    return part;
   }
 
   /**
@@ -53,4 +73,40 @@ public class DocumentPartPersistenceTest extends AbstractPersistenceTest {
     List<DocumentPart> parts = query.getResultList();
     return parts;
   }
+  
+  @Test
+  public void testNamedQueries() {
+    DocumentPart testPart = createTestDocumentPart();
+
+    em.persist(testPart);
+    startNewTransaction();
+
+    // check Named Query finding all parts
+    TypedQuery<DocumentPart> allQ =
+        em.createNamedQuery("DocumentPart.findAllParts", DocumentPart.class);
+    List<DocumentPart> allParts = allQ.getResultList();
+
+    assertTrue(allParts.size() > 0);
+    DocumentPart readPart = allParts.get(0);
+    checkData(readPart);
+
+    String id = readPart.getId();
+
+    // check Named Query finding document parts by id
+    TypedQuery<DocumentPart> idQ =
+        em.createNamedQuery("DocumentPart.findPartById", DocumentPart.class);
+    idQ.setParameter("partId", id);
+    DocumentPart idPart = idQ.getSingleResult();
+
+    checkData(idPart);
+
+    // check Named Query finding document parts by title
+    TypedQuery<DocumentPart> titleQ =
+        em.createNamedQuery("DocumentPart.findPartByTitle", DocumentPart.class);
+    titleQ.setParameter("partTitle", TEST_DOCUMENT_PART_TITLE);
+    DocumentPart titlePart = titleQ.getSingleResult();
+    
+    checkData(titlePart);
+  }
+  
 }
