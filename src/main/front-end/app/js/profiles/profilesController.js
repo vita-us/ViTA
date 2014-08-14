bf602d2f1bfbbd58603b161bc3e386854eb08932
christian <christian.richter@live.com>
2014-08-15 01:38:58 +0200

    profiles Controller offers init functions to load one or more profiles

--- src/main/front-end/app/js/profiles/profilesController.js
+++ src/main/front-end/app/js/profiles/profilesController.js
@@ -8,6 +8,21 @@
     Page.title = 'Profiles';
     Page.showMenu = false;
     Page.tab = 1;
-  }]);
 
+    $scope.initProfileList = function(documentID, profileOffset) {
+      $scope.profiles = Profiles.query(
+        {
+          documentId: documentID || 'documents',
+          offset: profileOffset || '1'
+        });
+    };
+
+    $scope.initProfile = function(documentID, profileID) {
+      $scope.profile = Profiles.get(
+        {
+          documentId: documentID || 'documents',
+          personId: profileID || '0'
+        });
+    };
+  }]);
 })(angular);
