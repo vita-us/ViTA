a199cd32e764bc3a24e8c656b1f56909b23d1473
Marvin Wyrich <st100245@stud.uni-stuttgart.de>
2014-08-14 14:07:33 +0200

    Fix overview controller to set correct page attributes

--- src/main/front-end/app/js/overview/overviewController.js
+++ src/main/front-end/app/js/overview/overviewController.js
@@ -4,11 +4,16 @@
   var vitaControllers = angular.module('vitaControllers');
 
   // Controller responsible for the overview page
-  vitaControllers.controller('OverviewCtrl', ['$scope', 'Document', function($scope, Document) {
-    // when rest-api exists instead documentId: $routeParams.documentId
-    $scope.document = Document.get({
-      documentId: 'single-document'
-    });
-  }]);
+  vitaControllers.controller('OverviewCtrl', ['$scope', 'Document', 'Page',
+      function($scope, Document, Page) {
+        // when rest-api exists instead documentId: $routeParams.documentId
+        Document.get({
+          documentId: 'single-document'
+        }, function(document) {
+          $scope.document = document;
+          Page.title = document.metadata.title;
+          Page.breadcrumbs = 'Overview';
+        });
+      }]);
 
 })(angular);
