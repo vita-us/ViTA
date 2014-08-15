22488f428fbb4a19344e9d1a779f63609f84d296
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2014-08-15 23:19:41 +0200

    Implement the executioner thread.

--- src/main/java/de/unistuttgart/vis/vita/analysis/ModuleExecutionThread.java
+++ src/main/java/de/unistuttgart/vis/vita/analysis/ModuleExecutionThread.java
@@ -1,18 +1,33 @@
 package de.unistuttgart.vis.vita.analysis;
 
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * Works through the list with modules.
  *
  */
 public class ModuleExecutionThread extends Thread {
 
-  public ModuleExecutionThread() {
-    // TODO Auto-generated constructor stub
+  private final AnalysisController analysisController;
+  private final Class<? extends Module> executeModule;
+
+  public ModuleExecutionThread(AnalysisController analysisController,
+                               Class<? extends Module> executeModule) {
+
+    this.analysisController = analysisController;
+    this.executeModule = executeModule;
   }
 
   @Override
   public void run() {
-    // TODO Auto-generated method stub
-    super.run();
+    try {
+      Module test = executeModule.getConstructor().newInstance();
+      // TODO execution of the module.
+      Object result = test.execute(analysisController.getResultProvider(), new MockProgressListener());
+      analysisController.getResultProvider().registerResult(executeModule.getClass(), result);
+      analysisController.continueAnalysis(executeModule.getClass());
+    } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+      e.printStackTrace();
+    }
   }
 }
