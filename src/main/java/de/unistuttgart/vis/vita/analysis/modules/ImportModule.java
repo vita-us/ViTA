6c87d1708d6ea906269cd2b5cd463c5bcbe60eaf
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2014-08-15 23:16:17 +0200

    Import necessary methods.

--- src/main/java/de/unistuttgart/vis/vita/analysis/modules/ImportModule.java
+++ src/main/java/de/unistuttgart/vis/vita/analysis/modules/ImportModule.java
@@ -5,9 +5,36 @@
 
 package de.unistuttgart.vis.vita.analysis.modules;
 
+import de.unistuttgart.vis.vita.analysis.Module;
+import de.unistuttgart.vis.vita.analysis.ModuleResultProvider;
+import de.unistuttgart.vis.vita.analysis.ProgressListener;
+
+import java.nio.file.Path;
+import java.util.Collection;
+
 /**
  * @author Vincent Link, Eduard Marbach
  */
-public class ImportModule {
+public class ImportModule implements Module {
 
+  private Path filePath;
+
+  public ImportModule(Path filePath) {
+    this.filePath = filePath;
+  }
+
+  @Override
+  public Collection<Class<?>> getDependencies() {
+    return null;
+  }
+
+  @Override
+  public Object execute(ModuleResultProvider result, ProgressListener progressListener) {
+    return null;
+  }
+
+  @Override
+  public void observeProgress(Class resultClass, double progress, boolean isReady) {
+
+  }
 }
--- src/main/java/de/unistuttgart/vis/vita/analysis/modules/MainAnalysisModule.java
+++ src/main/java/de/unistuttgart/vis/vita/analysis/modules/MainAnalysisModule.java
@@ -5,9 +5,29 @@
 
 package de.unistuttgart.vis.vita.analysis.modules;
 
-/**
- * @author Vincent Link, Eduard Marbach
- */
-public class MainAnalysisModule {
+import de.unistuttgart.vis.vita.analysis.Module;
+import de.unistuttgart.vis.vita.analysis.ModuleResultProvider;
+import de.unistuttgart.vis.vita.analysis.ProgressListener;
 
+import java.util.Collection;
+
+/**
+ *
+ */
+public class MainAnalysisModule implements Module<Void> {
+
+  @Override
+  public Collection<Class<?>> getDependencies() {
+    return null;
+  }
+
+  @Override
+  public <T> void observeProgress(Class<T> resultClass, double progress, boolean isReady) {
+
+  }
+
+  @Override
+  public Void execute(ModuleResultProvider result, ProgressListener progressListener) {
+    return null;
+  }
 }
