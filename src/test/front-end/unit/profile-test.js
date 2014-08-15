c1e4889acbddb3d9a734cfd5cfda66de44b4ecd1
christian <christian.richter@live.com>
2014-08-15 13:16:45 +0200

    Initial unit test for profiles controller

---
+++ src/test/front-end/unit/profile-test.js
@@ -0,0 +1,9 @@
+describe('ProfilesCtrl', function() {
+  it('should use the Profiles service to get a profile from the server', function($controller) {
+
+  });
+  
+  it('should use the Profiles service to get all profiles from the server', function($controller) {
+
+  });
+});
