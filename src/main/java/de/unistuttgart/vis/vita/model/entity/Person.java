eaa20920be26fbeb66225c8286cbbb0ada738dbf
Marc <weisemc@studi.informatik.uni-stuttgart.de>
2014-08-17 00:36:38 +0200

    Add 2 x 3 Named Queries to 'Person' and 'Place' for issue #45.
    - Add Named Query to find all persons and places
    - Add Named Query to find persons and places by id
    - Add Named Query to find persons and places by name

--- src/main/java/de/unistuttgart/vis/vita/model/entity/Person.java
+++ src/main/java/de/unistuttgart/vis/vita/model/entity/Person.java
@@ -1,12 +1,29 @@
 package de.unistuttgart.vis.vita.model.entity;
 
 import javax.persistence.DiscriminatorValue;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 /**
  * Represents a character in a document.
  */
 @javax.persistence.Entity
 @DiscriminatorValue(Person.DISCRIMINATOR_VALUE)
+@NamedQueries({
+  @NamedQuery(name = "Person.findAllPersons",
+      query = "SELECT ps "
+      + "FROM Person ps"),
+      
+  @NamedQuery(name = "Person.findPersonById",
+      query = "SELECT ps "
+      + "FROM Person ps "
+      + "WHERE ps.id = :personId"),
+  
+  @NamedQuery(name = "Person.findPersonByName",
+      query = "SELECT ps "
+      + "FROM Person ps "
+      + "WHERE ps.displayName = :personName")
+})
 public class Person extends Entity {
   
   // constants
--- src/main/java/de/unistuttgart/vis/vita/model/entity/Place.java
+++ src/main/java/de/unistuttgart/vis/vita/model/entity/Place.java
@@ -1,12 +1,29 @@
 package de.unistuttgart.vis.vita.model.entity;
 
 import javax.persistence.DiscriminatorValue;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 /**
  * Represents a place in a document.
  */
 @javax.persistence.Entity
 @DiscriminatorValue(Place.DISCRIMINATOR_VALUE)
+@NamedQueries({
+  @NamedQuery(name = "Place.findAllPlaces",
+      query = "SELECT pl "
+      + "FROM Place pl"),
+      
+  @NamedQuery(name = "Place.findPlaceById",
+      query = "SELECT pl "
+      + "FROM Place pl "
+      + "WHERE pl.id = :placeId"),
+  
+  @NamedQuery(name = "Place.findPlaceByName",
+      query = "SELECT pl "
+      + "FROM Place pl "
+      + "WHERE pl.displayName = :placeName")
+})
 public class Place extends Entity {
 
   // constants
