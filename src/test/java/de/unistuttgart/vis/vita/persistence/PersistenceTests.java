052d2b010583b08c6cf1ffdc51f4ec36c975e677
Marc <weisemc@studi.informatik.uni-stuttgart.de>
2014-08-17 17:22:15 +0200

    Add persistence test for 'EntityRelation' including Named Queries.
    - Create new JUnit test case 'EntityRelationPersistenceTest'
    - Add new test case to persistence test suite 'PersistenceTests'

--- src/test/java/de/unistuttgart/vis/vita/persistence/PersistenceTests.java
+++ src/test/java/de/unistuttgart/vis/vita/persistence/PersistenceTests.java
@@ -7,7 +7,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({AttributePersistenceTest.class, ChapterPersistenceTest.class,
     DocumentPersistenceTest.class, DocumentPartPersistenceTest.class, EntityPersistenceTests.class,
-    TextSpanPersistenceTest.class})
+    EntityRelationPesistenceTest.class, TextSpanPersistenceTest.class})
 public class PersistenceTests {
 
 }
