d63f67f3f7d09e919d542700165a5aa776ec26b2
Paul <st103454@stud.uni-stuttgart.de>
2014-08-18 20:14:15 +0200

    #78 Create service for test-data

--- src/main/front-end/app/js/mockBackend/mockBackend.js
+++ src/main/front-end/app/js/mockBackend/mockBackend.js
@@ -3,64 +3,11 @@
 
   var app = angular.module('vita');
 
-  app.run(['$httpBackend', function($httpBackend) {
-
-    var documents = {
-      "totalCount": 2,
-      "documents": [{
-        "id": "doc13a",
-        "metadata": {
-          "title": "Rotkaepchen",
-          "author": "Hans Mueller",
-          "publisher": "XY Verlag",
-          "publishYear": 1957,
-          "genre": "Fantasy",
-          "edition": "Limited Edition"
-        }
-      }, {
-        "id": "doc14",
-        "metadata": {
-          "title": "Hans guck in die Luft",
-          "author": "Peter Mayer",
-          "publisher": "ABC Verlag",
-          "publishYear": 1968,
-          "genre": "Krimi",
-          "edition": "Standard"
-        }
-      }]
-    };
-
-    var singleDocument = {
-      "id": "doc13a",
-      "metadata": {
-        "title": "Rotkaeppchen",
-        "author": "Hans Mueller",
-        "publisher": "XY Verlag",
-        "publishYear": 1957,
-        "genre": "Thriller",
-        "edition": "Limited Edition"
-      },
-      "metrics": {
-        "characterCount": 19231,
-        "chapterCount": 3,
-        "personCount": 8,
-        "placeCount": 2,
-        "wordCount": 1999
-      },
-      "content": {
-        "parts": {
-          "chapters": ["chapter1", "chapter2", "chapter3"],
-          "number": 1,
-          "title": "Der erste Teil"
-        },
-        "persons": ["person8Ben", "person10Bert"],
-        "places": ["place2Paris", "place10Mordor"]
-      }
-    };
+  app.run(['$httpBackend', 'TestData', function($httpBackend, TestData) {
 
     $httpBackend.whenGET(new RegExp('\.html$')).passThrough();
-    $httpBackend.whenGET(new RegExp('/documents$')).respond(documents);
-    $httpBackend.whenGET(new RegExp('/documents/[^/]+$')).respond(singleDocument);
+    $httpBackend.whenGET(new RegExp('/documents$')).respond(TestData.documents);
+    $httpBackend.whenGET(new RegExp('/documents/[^/]+$')).respond(TestData.singleDocument);
 
   }]);
 })(angular);
