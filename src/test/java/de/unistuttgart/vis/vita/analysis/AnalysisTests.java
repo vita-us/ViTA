f5dd586ad58b8f42fb3bbc77bd66cd4bd00c7b8d
Jan Melcher <info@yogularm.de>
2014-08-27 00:41:14 +0200

    #33 Added mock module and test for text import

---
+++ src/test/java/de/unistuttgart/vis/vita/analysis/AnalysisTests.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.vis.vita.analysis;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import de.unistuttgart.vis.vita.analysis.importer.TextImportModuleTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({TextImportModuleTest.class})
+public class AnalysisTests {
+
+}
