c59c36a14577a434520763c9cf9ca60386a37401
Sanjeev Balakrishnan <sanjeev.b@gmx.de>
2014-08-29 16:46:12 +0200

    #33 Add junit test for MetadataAnalyzer

---
+++ src/test/java/de/unistuttgart/vis/vita/analysis/importer/MetadataAnalyzerTxtTest.java
@@ -0,0 +1,59 @@
+package de.unistuttgart.vis.vita.analysis.importer;
+
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.unistuttgart.vis.vita.importer.txt.Line;
+import de.unistuttgart.vis.vita.importer.txt.MetadataAnalyzer;
+import de.unistuttgart.vis.vita.importer.txt.TextFileImporter;
+import de.unistuttgart.vis.vita.model.document.DocumentMetadata;
+
+/**
+ * JUnit test on MetadataAnalyzer
+ * 
+ *
+ */
+public class MetadataAnalyzerTxtTest {
+
+  private DocumentMetadata documentMetadata = new DocumentMetadata();
+
+  /**
+   * sets the values of documentMetadata after MetadataAnalyzer has analyzed the imported lines
+   * 
+   * @throws IllegalArgumentException
+   * @throws FileNotFoundException
+   * @throws IllegalStateException
+   * @throws SecurityException
+   * @throws URISyntaxException
+   */
+  @Before
+  public void setUp() throws IllegalArgumentException, FileNotFoundException,
+      IllegalStateException, SecurityException, URISyntaxException {
+
+    Path testPath = Paths.get(getClass().getResource("text1.txt").toURI());
+    TextFileImporter textFileImporter = new TextFileImporter(testPath);
+    List<Line> testList = textFileImporter.getResult();
+    MetadataAnalyzer metadataAnalyzer = new MetadataAnalyzer(testList, testPath);
+    documentMetadata = metadataAnalyzer.extractMetadata();
+
+  }
+
+  @Test
+  public void testMetadataTitle() {
+    assertEquals("The Lord of the Rings", documentMetadata.getTitle());
+  }
+
+  @Test
+  public void testMetadataAuthor() {
+    assertEquals("J.R.R. Tolkien", documentMetadata.getAuthor());
+
+  }
+}
