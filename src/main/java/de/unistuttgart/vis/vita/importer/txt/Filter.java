a75e58c559b80fa083dfb33170953d877ed42fc1
Sanjeev Balakrishnan <sanjeev.b@gmx.de>
2014-08-29 16:38:51 +0200

    #33 Add exitsEndBracket Method

--- src/main/java/de/unistuttgart/vis/vita/importer/txt/Filter.java
+++ src/main/java/de/unistuttgart/vis/vita/importer/txt/Filter.java
@@ -20,6 +20,8 @@ public class Filter {
   private static final String REPLACE_DEFAULT_COMMENT = "(\\[).+(\\])";
   private static final String REPLACE_DEFAULT_COMMENT_BEGIN = "(\\[).*";
   private static final String REPLACE_DEFAULT_COMMENT_END_3 = ".*(\\])";
+  private static final String DEFAULT__VERIFY_END_BRACKET = ".*(\\]).*[^\\[]*";
+
   private List<Line> entireEbookList = new ArrayList<Line>();
 
   public Filter(List<Line> newEntireEbookList) {
@@ -34,13 +36,13 @@ public class Filter {
   public List<Line> filterEbookText() {
     // In this line the part with "[.*" or ".*]" is removed
     String editedLine;
-    
+
     // The removeList contains the lines, which are between "[.*" and ".*]" or which ends with ".*]"
     List<Line> removeList = new ArrayList<Line>();
-    
+
     // The defaultCommentMap contains the positions of entireEbookList and the edited lines
     Map<Integer, Line> defaultCommentMap = new HashMap<Integer, Line>();
-    
+
     for (Line line : entireEbookList) {
       if (line.getText() != null) {
         if (line.getText().matches(DEFAULT_COMMENT_FILTER)) {
@@ -48,36 +50,42 @@ public class Filter {
           defaultCommentMap.put(entireEbookList.indexOf(line), new Line(editedLine));
 
         } else if (line.getText().matches(DEFAULT_COMMENT_BEGIN)) {
-          editedLine = line.getText().replaceAll(REPLACE_DEFAULT_COMMENT_BEGIN, "");
-          defaultCommentMap.put(entireEbookList.indexOf(line), new Line(editedLine));
-          addElementsToRemoveListAndMap(removeList, defaultCommentMap, line);
+          if (existsEndBracket(line)) {
+            editedLine = line.getText().replaceAll(REPLACE_DEFAULT_COMMENT_BEGIN, "");
+            defaultCommentMap.put(entireEbookList.indexOf(line), new Line(editedLine));
+            addElementsToRemoveListAndMap(removeList, defaultCommentMap, line);
+          } else {
+            continue;
+          }
         }
       }
     }
 
-    // Set the new edited line entry.getValue() in the entireEbookList at the position entry.getKey()
+    // Set the new edited line entry.getValue() in the entireEbookList at the position
+    // entry.getKey()
     for (Map.Entry<Integer, Line> entry : defaultCommentMap.entrySet()) {
       entireEbookList.set(entry.getKey(), entry.getValue());
     }
-    
+
     // Remove the unnecessary comments in the entireEbookList
     entireEbookList.removeAll(removeList);
-    
+
     removeList.clear();
     return entireEbookList;
 
   }
-  
+
   /**
    * Adds the unnecessary comments to the removeList and edited lines to the defaultCommentMap
-   * @param removeList 
+   * 
+   * @param removeList
    * @param defaultCommentMap
    * @param line, which helps to find the current position in the entireEbookList
    */
-  private void addElementsToRemoveListAndMap(List<Line> removeList, Map<Integer, Line> defaultCommentMap, Line line) {
+  private void addElementsToRemoveListAndMap(List<Line> removeList,
+      Map<Integer, Line> defaultCommentMap, Line currentLine) {
     String editedLine;
-    for (int i = entireEbookList.indexOf(line) + 1; i < entireEbookList.size(); i++) {
-
+    for (int i = entireEbookList.indexOf(currentLine) + 1; i < entireEbookList.size(); i++) {
       if (!entireEbookList.get(i).getText().matches(DEFAULT_COMMENT_END_1)) {
         removeList.add(entireEbookList.get(i));
 
@@ -91,5 +99,24 @@ public class Filter {
         break;
       }
     }
+  }
+
+  /**
+   * Checks whether one of the following lines after the current line have an end bracket
+   * 
+   * @param currentLine
+   * @return
+   */
+  private boolean existsEndBracket(Line currentLine) {
+    for (int i = entireEbookList.indexOf(currentLine) + 1; i < entireEbookList.size(); i++) {
+      if (!entireEbookList.get(i).getText().matches(".*\\[.*")) {
+        if (entireEbookList.get(i).getText().matches(DEFAULT__VERIFY_END_BRACKET)) {
+          return true;
+        }
+      } else {
+        return false;
+      }
+    }
+    return false;
   }
 }
