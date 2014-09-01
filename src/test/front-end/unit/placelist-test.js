d581d8dffd01d825dc75d7853a3168347d0637ad
Paul <st103454@stud.uni-stuttgart.de>
2014-09-01 21:09:44 +0200

    #61 #76 Add unit-tests for place-related controllers

---
+++ src/test/front-end/unit/placelist-test.js
@@ -0,0 +1,31 @@
+describe('PlaceListCtrl', function() {
+  var scope, $httpBackend, ctrl;
+
+  beforeEach(function() {
+    this.addMatchers({
+      toEqualData: function(expected) {
+        return angular.equals(this.actual, expected);
+      }
+    });
+  });
+
+  beforeEach(module('vita'));
+
+  beforeEach(inject(function(_$httpBackend_, $rootScope, $controller, $routeParams, TestData) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('/documents/123/places').respond(TestData.places);
+    $routeParams.documentId = '123';
+    scope = $rootScope.$new();
+    ctrl = $controller('PlaceListCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should create "place" model with 2 places', inject(function($controller, TestData) {
+
+    $httpBackend.flush();
+    expect(scope.places).toEqualData(TestData.places.places);
+
+  }));
+
+});
