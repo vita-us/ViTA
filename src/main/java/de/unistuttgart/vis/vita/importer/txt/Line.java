e77a15cb37d27486f61ee6193905b1e7af4af940
franksn <franksn@studi.informatik.uni-stuttgart.de>
2014-09-01 11:30:32 +0200

    #33 Added detection rules for special sign type; improved type detection

--- src/main/java/de/unistuttgart/vis/vita/importer/txt/Line.java
+++ src/main/java/de/unistuttgart/vis/vita/importer/txt/Line.java
@@ -13,33 +13,37 @@ public class Line {
   private static final String MARKEDHEADING = WHITESPACE + "#.*";
   private static final Pattern MARKEDHEADINGPATTERN = Pattern.compile(MARKEDHEADING);
 
-  private static final String PREFACE = WHITESPACE + "#?((Preface)|(To\\s*the\\s*Reader))([\\.:])?"
+  private static final String PREFACE = WHITESPACE + "((Preface)|(To\\s*the\\s*Reader))([\\.:])?"
       + WHITESPACE;
   private static final Pattern PREFACEPATTERN = Pattern.compile(PREFACE, Pattern.CASE_INSENSITIVE);
 
-  private static final String TABLEOFCONTENTS = WHITESPACE + ".*"
-      + "#?((Index)|(Contents)|(Table\\s*of\\s*Contents))([\\.:])?" + WHITESPACE;
+  private static final String TABLEOFCONTENTS = WHITESPACE
+      + "((Index)|(Contents)|(Table\\s*of\\s*Contents))([\\.:])?" + WHITESPACE;
   private static final Pattern TABLEOFCONTENTSPATTERN = Pattern.compile(TABLEOFCONTENTS,
       Pattern.CASE_INSENSITIVE);
 
-  private static final String BIGHEADING = WHITESPACE
-      + "([\\p{Upper}\\d\\p{Punct}][\\p{Upper}\\d\\p{Punct}\\s]*)";
+  private static final String BIGHEADING = WHITESPACE + "([\\p{Upper}\\d][^\\p{Lower}]*)";
   private static final String BIGHEADINGQUOTES = WHITESPACE + "(\"" + BIGHEADING + "\")" + "|"
       + "(\'" + BIGHEADING + "\')";
-  private static final Pattern BIGHEADINGPATTERN = Pattern.compile(BIGHEADING + "|"
-      + BIGHEADINGQUOTES);
+  private static final Pattern BIGHEADINGPATTERN = Pattern.compile("(" + BIGHEADING + ")|("
+      + BIGHEADINGQUOTES + ")");
 
   private static final String BIGSIGNS = "[\\p{Upper}\\d]";
   private static final Pattern BIGSIGNSPATTERN = Pattern.compile(BIGSIGNS);
 
-  private static final String NORMALHEADING = "\\p{Upper}[^\\.\\?\\!,]*\\.?";
-  private static final String ONEQUOTE = "(\"|\')\\p{Upper}[^\"\']*(\"|\')";
+  private static final String NORMALHEADING = "\\p{Upper}[^\\.\\?\\!]*\\.?";
+  private static final String ONEQUOTE =
+      "((\")\\p{Upper}[^\"\']*(\"))|((\')\\p{Upper}[^\"\']*(\'))";
   private static final String CHAPTER = "(?i)Chapter(?-i)" + WHITESPACE + "((\\d+)|([IVXML]+))?"
       + WHITESPACE + "\\p{Punct}?";
   private static final String SMALLHEADING = "(" + WHITESPACE + "(" + CHAPTER + ")?" + WHITESPACE
       + "((" + "([\\d]+\\.)?" + WHITESPACE + NORMALHEADING + ")" + "|" + "(" + ONEQUOTE + "))"
       + WHITESPACE + ")|(" + CHAPTER + ")";
   private static final Pattern SMALLHEADINGPATTERN = Pattern.compile(SMALLHEADING);
+
+  private static final String NOSPECIALSIGNS = "\\p{Alnum}";
+  private static final Pattern NOSPECIALSIGNSPATTERN = Pattern.compile(NOSPECIALSIGNS);
+
 
   private String text;
   private LineType type;
@@ -57,6 +61,9 @@ public class Line {
     }
     if (matchesPattern(PREFACEPATTERN)) {
       highestType = LineType.PREFACE;
+    }
+    if (!containsPattern(NOSPECIALSIGNSPATTERN) && !text.contains("...")) {
+      highestType = LineType.SPECIALSIGNS;
     }
     if (matchesPattern(MARKEDHEADINGPATTERN)) {
       highestType = LineType.MARKEDHEADING;
