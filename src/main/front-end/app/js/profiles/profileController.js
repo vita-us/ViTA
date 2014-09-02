4c00c44e6e70b594cae36505882146694678ae9a
Vincent Link <mail@linkvt.de>
2014-09-02 22:45:26 +0200

    Use routeParams for service parameters

--- src/main/front-end/app/js/profiles/profileController.js
+++ src/main/front-end/app/js/profiles/profileController.js
@@ -4,18 +4,15 @@
   var vitaControllers = angular.module('vitaControllers');
 
   // Controller responsible for the profiles page
-  vitaControllers.controller('ProfileCtrl', ['$scope', 'Profile', 'Page',
-      function($scope, Profile, Page) {
+  vitaControllers.controller('ProfileCtrl', ['$scope', 'Profile', 'Page', '$routeParams',
+      function($scope, Profile, Page, $routeParams) {
         Page.title = 'Profile';
         Page.showMenu = true;
         Page.tab = 1;
 
-        $scope.documentId = 'doc13a';
-        $scope.personId = 'person10Bert';
-
         $scope.profile = Profile.get({
-          documentId: $scope.documentId,
-          personId: $scope.personId
+          documentId: $routeParams.documentId,
+          personId: $routeParams.personId
         });
       }]);
 })(angular);
--- src/main/front-end/app/js/profiles/profilesController.js
+++ src/main/front-end/app/js/profiles/profilesController.js
@@ -4,15 +4,14 @@
   var vitaControllers = angular.module('vitaControllers');
 
   // Controller responsible for the profiles page
-  vitaControllers.controller('ProfilesCtrl', ['$scope', 'Profile', 'Page', function($scope, Profile, Page) {
-    Page.title = 'Profiles';
-    Page.showMenu = true;
-    Page.tab = 1;
+  vitaControllers.controller('ProfilesCtrl', ['$scope', 'Profile', 'Page', '$routeParams',
+      function($scope, Profile, Page, $routeParams) {
+        Page.title = 'Profiles';
+        Page.showMenu = true;
+        Page.tab = 1;
 
-    $scope.documentId = 'doc13a';
-
-    $scope.profiles = Profile.get({
-      documentId: $scope.documentId
-    });
-  }]);
+        $scope.profiles = Profile.get({
+          documentId: $routeParams.documentId
+        });
+      }]);
 })(angular);
