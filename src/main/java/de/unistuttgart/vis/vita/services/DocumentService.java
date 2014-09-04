7bc6b8f558f670aaab8bb8da7165e1f474e79def
Marc <weisemc@studi.informatik.uni-stuttgart.de>
2014-09-04 23:58:34 +0200

    #54 Implement 'DocumentService'
    - Implement a method for GET requests returning a document
    - Implement a method for PUT requests renaming a document
    - Implement a method for DELETE request deleting a document

--- src/main/java/de/unistuttgart/vis/vita/services/DocumentService.java
+++ src/main/java/de/unistuttgart/vis/vita/services/DocumentService.java
@@ -1,6 +1,9 @@
 package de.unistuttgart.vis.vita.services;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.RollbackException;
+import javax.persistence.TypedQuery;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -14,9 +17,11 @@ import javax.ws.rs.core.UriInfo;
 
 import de.unistuttgart.vis.vita.model.Model;
 import de.unistuttgart.vis.vita.model.document.Document;
-import de.unistuttgart.vis.vita.services.responses.DocumentIdResponse;
-import de.unistuttgart.vis.vita.services.responses.DocumentRenameResponse;
+import de.unistuttgart.vis.vita.services.requests.DocumentRenameRequest;
 
+/**
+ * Provides methods for GET, PUT and DELETE a document with a specific id.
+ */
 public class DocumentService {
   
   @Context
@@ -38,27 +43,77 @@ public class DocumentService {
     this.uriInfo = uriInfo;
     this.request = request;
     this.id = id;
-    this.em = new Model().getEntityManager();
+    this.em = Model.createUnitTestModelWithoutDrop().getEntityManager();
   }
   
+  /**
+   * Reads the document from the database and returns it.
+   * 
+   * @return the document with the current id
+   */
+  private Document readDocumentFromDatabase()  {
+    TypedQuery<Document> query = em.createNamedQuery("Document.findDocumentById", Document.class);
+    query.setParameter("documentId", id);
+    return query.getSingleResult();
+  }
+  
+  /**
+   * Reads the requested document from the database and returns it in JSON using the REST.
+   * 
+   * @return the document with the current id in JSON
+   */
   @GET
-  @Produces({MediaType.APPLICATION_JSON})
+  @Produces(MediaType.APPLICATION_JSON)
   public Document getDocument() {
-    // TODO not implemented yet!
-    return null;
+    return readDocumentFromDatabase();
   }
   
+  /**
+   * Renames the current document to the given name.
+   * 
+   * @param renameRequest - the renaming request including id and new name.
+   * @return a response with no content if renaming was successful, HTTP 404 if document was not
+   * found or 500 if an error occurred.
+   */
   @PUT
   @Consumes(MediaType.APPLICATION_JSON)
-  public DocumentRenameResponse putDocument() {
-    // TODO not implemented yet!
-    return null;
+  public Response putDocument(DocumentRenameRequest renameRequest) {
+    Response response = null;
+    try {
+      em.getTransaction().begin();
+      Document affectedDocument = readDocumentFromDatabase();
+      affectedDocument.getMetadata().setTitle(renameRequest.getName());
+      em.persist(affectedDocument);
+      em.getTransaction().commit();
+      
+      response = Response.noContent().build();
+    } catch (EntityNotFoundException enfe) {
+      response = Response.status(404).build();
+    } catch (RollbackException re) {
+      response =  Response.serverError().build();
+    }
+    return response;
   }
   
+  /**
+   * Deletes the document with the current id.
+   * 
+   * @return a response with no content if removal was successful, status 404 if document was not
+   * found
+   */
   @DELETE
-  public DocumentIdResponse deleteDocument() {
-    // TODO not implemented yet!
-    return null;
+  public Response deleteDocument() {
+    Response response = null;
+    try {
+      em.getTransaction().begin();
+      em.remove(readDocumentFromDatabase());
+      em.getTransaction().commit();
+      
+      response = Response.noContent().build();
+    } catch (IllegalArgumentException iae) {
+      response = Response.status(404).build();
+    }
+    return response;
   }
 
 }
