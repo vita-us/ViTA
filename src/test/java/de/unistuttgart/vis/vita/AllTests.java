82b5afe004df2032ef5eda9c35ac042307034fc3
Jan Melcher <info@yogularm.de>
2014-09-08 21:57:59 +0200

    Added AnalysisTests back to AllTests suite

--- src/test/java/de/unistuttgart/vis/vita/AllTests.java
+++ src/test/java/de/unistuttgart/vis/vita/AllTests.java
@@ -6,14 +6,13 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import de.unistuttgart.vis.vita.analysis.AnalysisTests;
 import de.unistuttgart.vis.vita.model.ModelTests;
-import de.unistuttgart.vis.vita.persistence.PersistenceTests;
 import de.unistuttgart.vis.vita.services.ServiceTests;
 
 /**
  * A suite containing all back-end tests
  */
 @RunWith(Suite.class)
-@SuiteClasses({ServiceTests.class, ModelTests.class})
+@SuiteClasses({ServiceTests.class, ModelTests.class, AnalysisTests.class})
 public class AllTests {
 
   // conform checkstyle rule HideUtilityClassConstructor
