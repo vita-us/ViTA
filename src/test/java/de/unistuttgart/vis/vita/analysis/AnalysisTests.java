9863094eb5538b98ad7a43607d001308129779cf
Sanjeev <sanjeev.b@gmx.de>
2014-09-08 14:07:35 +0200

    #33 Add ImportTests to AnalysisTests

--- src/test/java/de/unistuttgart/vis/vita/analysis/AnalysisTests.java
+++ src/test/java/de/unistuttgart/vis/vita/analysis/AnalysisTests.java
@@ -4,10 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import de.unistuttgart.vis.vita.analysis.importer.ImportTests;
 import de.unistuttgart.vis.vita.analysis.importer.TextImportModuleTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({TextImportModuleTest.class})
+@SuiteClasses({TextImportModuleTest.class, ImportTests.class})
 public class AnalysisTests {
 
 }
