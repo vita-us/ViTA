4cf9bd2bb0c8a1dcd1770a14a3817eb676f8232b
Jan Melcher <info@yogularm.de>
2014-09-08 21:07:17 +0200

    #34 Implement StanfordNLPModule

---
+++ src/test/java/de/unistuttgart/vis/vita/analysis/modules/AnalysisModulesTests.java
@@ -0,0 +1,16 @@
+package de.unistuttgart.vis.vita.analysis.modules;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Unit tests for analysis modules
+ */
+@RunWith(Suite.class)
+@SuiteClasses({StanfordNLPModuleTest.class})
+public class AnalysisModulesTests {
+  // conform checkstyle rule HideUtilityClassConstructor
+  private AnalysisModulesTests() {
+  }
+}
