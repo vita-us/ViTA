a4eb968f67247f9bf9910941826e766696ffd5bb
Marvin Wyrich <st100245@stud.uni-stuttgart.de>
2014-09-08 16:53:59 +0200

    add filterable list of characters

--- src/main/front-end/app/js/characters/characterListController.js
+++ src/main/front-end/app/js/characters/characterListController.js
@@ -4,16 +4,20 @@
   var vitaControllers = angular.module('vitaControllers');
 
   // Controller responsible for the characters page
-  vitaControllers.controller('CharacterListCtrl', ['$scope', 'Character', 'Page', '$routeParams',
-      function($scope, Character, Page, $routeParams) {
-        Page.title = 'Characters';
-        Page.showMenu = true;
-        Page.tab = 1;
-
+  vitaControllers.controller('CharacterListCtrl', ['$scope', 'Document', 'Page', 'Character', '$routeParams',
+      function($scope, Document, Page, Character, $routeParams) {
         Character.get({
           documentId: $routeParams.documentId
         }, function(response) {
           $scope.characters = response.characters;
+        });
+        
+        Document.get({
+          documentId: $routeParams.documentId
+        }, function(document) {
+          $scope.document = document;
+          Page.breadcrumbs = 'Characters';
+          Page.setUpForDocument(document);
         });
       }]);
 })(angular);
