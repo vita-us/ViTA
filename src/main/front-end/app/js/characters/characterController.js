7f448220839449795646b0f77984912ff33bd562
Marvin Wyrich <st100245@stud.uni-stuttgart.de>
2014-09-08 17:29:53 +0200

    initial work on single character and place pages

--- src/main/front-end/app/js/characters/characterController.js
+++ src/main/front-end/app/js/characters/characterController.js
@@ -4,15 +4,24 @@
   var vitaControllers = angular.module('vitaControllers');
 
   // Controller responsible for the characters page
-  vitaControllers.controller('CharacterCtrl', ['$scope', 'Character', 'Page', '$routeParams',
-      function($scope, Character, Page, $routeParams) {
-        Page.title = 'Character';
-        Page.showMenu = true;
-        Page.tab = 1;
-
-        $scope.character = Character.get({
+  vitaControllers.controller('CharacterCtrl', ['$scope', 'Document', 'Page', 'Character', '$routeParams',
+      function($scope, Document, Page, Character, $routeParams) {
+        var charName = '';
+    
+        Character.get({
           documentId: $routeParams.documentId,
           characterId: $routeParams.characterId
+        }, function(singleCharacter) {
+          $scope.character = singleCharacter;
+          charName = singleCharacter.displayName;
+        });
+        
+        Document.get({
+          documentId: $routeParams.documentId
+        }, function(document) {
+          $scope.document = document;
+          Page.breadcrumbs = 'Characters > ' + charName;
+          Page.setUpForDocument(document);
         });
       }]);
 })(angular);
