0db9a64d2b92f9d3ca6a8d582f392ab4e38238f7
Sanjeev <sanjeev.b@gmx.de>
2014-09-08 14:11:39 +0200

    #33 Add JUnit Test for Filter

---
+++ src/test/java/de/unistuttgart/vis/vita/analysis/importer/FilterTxtTest.java
@@ -0,0 +1,37 @@
+package de.unistuttgart.vis.vita.analysis.importer;
+
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import de.unistuttgart.vis.vita.importer.txt.Filter;
+import de.unistuttgart.vis.vita.importer.txt.Line;
+import de.unistuttgart.vis.vita.importer.txt.TextFileImporter;
+
+public class FilterTxtTest {
+
+  private List<Line> filteredList = new ArrayList<Line>();
+
+  @Before
+  public void setUp() throws URISyntaxException, IllegalArgumentException, FileNotFoundException,
+      IllegalStateException, SecurityException {
+    Path testPath = Paths.get(getClass().getResource("text1.txt").toURI());
+    TextFileImporter textFileImporter = new TextFileImporter(testPath);
+    Filter filter = new Filter(textFileImporter.getResult());
+    filteredList = filter.filterEbookText();
+  }
+
+  @Test
+  public void testFilterEbookText() {
+    assertFalse(filteredList.get(11).getText().equals("[Ebook The Lord of the Rings]"));
+    assertEquals(filteredList.get(12).getText(), "The text of the first chapter.");
+  }
+}
