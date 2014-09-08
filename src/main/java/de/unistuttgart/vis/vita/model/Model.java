a687b40f68818f818dddbdd8d2fdf20baa187c1b
Jan Melcher <info@yogularm.de>
2014-09-08 14:24:35 +0200

    #54 Work around glassfish issue of not loading jdbc driver from war

--- src/main/java/de/unistuttgart/vis/vita/model/Model.java
+++ src/main/java/de/unistuttgart/vis/vita/model/Model.java
@@ -1,5 +1,8 @@
 package de.unistuttgart.vis.vita.model;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -17,6 +20,14 @@ public class Model {
   private static final String UNITTEST_PERSISTENCE_UNIT_NAME = "de.unistuttgart.vis.vita.unittest.drop";
   private static final String UNITTEST_PERSISTENCE_UNIT_NAME_NODROP = "de.unistuttgart.vis.vita.unittest";
 
+  static {
+    /* 
+     * Glassfish does not use the driver provided in the war if not explicitly loaded:
+     * https://java.net/jira/browse/GLASSFISH-19451
+     */
+    loadDriver();
+  }
+  
   /**
    * Create a Model to be used in unit tests
    * <p>
@@ -26,7 +37,7 @@ public class Model {
   public static Model createUnitTestModel() {
     return new Model(UNITTEST_PERSISTENCE_UNIT_NAME);
   }
-  
+
   public static Model createUnitTestModelWithoutDrop() {
     return new Model(UNITTEST_PERSISTENCE_UNIT_NAME_NODROP);
   }
@@ -38,8 +49,8 @@ public class Model {
     this(PERSISTENCE_UNIT_NAME);
   }
   
-  private Model(String persistenceUnitName) {  
-	  entityManagerFactory = Persistence.createEntityManagerFactory(persistenceUnitName);
+  private Model(String persistenceUnitName) {
+	entityManagerFactory = Persistence.createEntityManagerFactory(persistenceUnitName);
   }
   
   /**
@@ -55,5 +66,12 @@ public class Model {
   public TextRepository getTextRepository() {
     return textRepo;
   }
-
+  
+  private static void loadDriver() {
+    try {
+      Class.forName("org.h2.Driver");
+    } catch (ClassNotFoundException e) {
+      Logger.getLogger(Model.class.getName()).log(Level.WARNING, "Unable to load H2 driver", e);
+    }
+  }
 }
