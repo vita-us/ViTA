c37ed357e706e5b2fa321815cd9153c7afd96f3e
Sanjeev <sanjeev.b@gmx.de>
2014-09-10 17:01:20 +0200

    #36 Change the result to IndexSearcher

--- src/main/java/de/unistuttgart/vis/vita/analysis/modules/LuceneModule.java
+++ src/main/java/de/unistuttgart/vis/vita/analysis/modules/LuceneModule.java
@@ -1,15 +1,25 @@
 package de.unistuttgart.vis.vita.analysis.modules;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.lucene.search.IndexSearcher;
+
 import de.unistuttgart.vis.vita.analysis.Module;
 import de.unistuttgart.vis.vita.analysis.ModuleResultProvider;
 import de.unistuttgart.vis.vita.analysis.ProgressListener;
 import de.unistuttgart.vis.vita.analysis.annotations.AnalysisModule;
+import de.unistuttgart.vis.vita.analysis.results.ImportResult;
 import de.unistuttgart.vis.vita.model.Model;
-import de.unistuttgart.vis.vita.model.TextRepository;
+import de.unistuttgart.vis.vita.model.document.DocumentPart;
 
-@AnalysisModule(dependencies = Model.class)
-public class LuceneModule implements Module<TextRepository> {
+@AnalysisModule(dependencies = {ImportResult.class,Model.class})
+public class LuceneModule implements Module<IndexSearcher> {
 
+  private ImportResult importResult;
+  private List<DocumentPart> documentParts = new ArrayList<DocumentPart>();
+
+  
   @Override
   public void observeProgress(double progress) {
     // TODO Auto-generated method stub
@@ -17,9 +27,10 @@ public class LuceneModule implements Module<TextRepository> {
   }
 
   @Override
-  public TextRepository execute(ModuleResultProvider result, ProgressListener progressListener)
+  public IndexSearcher execute(ModuleResultProvider result, ProgressListener progressListener)
       throws Exception {
-    // TODO Auto-generated method stub
+    importResult = result.getResultFor(ImportResult.class);
+    documentParts = importResult.getParts();
     return null;
   }
 
