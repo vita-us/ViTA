0062e286b782d192c2b5287a000ded003ad91b9f
Sanjeev <sanjeev.b@gmx.de>
2014-09-11 23:44:12 +0200

    #36 Add LuceneModuleTesr to AnalysisTests

--- src/test/java/de/unistuttgart/vis/vita/analysis/AnalysisTests.java
+++ src/test/java/de/unistuttgart/vis/vita/analysis/AnalysisTests.java
@@ -4,12 +4,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import de.unistuttgart.vis.vita.analysis.modules.LuceneModuleTest;
+
 /**
  * A suite containing all back-end tests
  */
 @RunWith(Suite.class)
 @SuiteClasses({AnalysisControllerTest.class, TestModuleRegistry.class, ModuleClassTest.class,
-    AnalysisSchedulerTest.class, ModuleExecutionStateTest.class, AnalysisExecutorTest.class})
+    AnalysisSchedulerTest.class, ModuleExecutionStateTest.class, AnalysisExecutorTest.class, LuceneModuleTest.class})
 public class AnalysisTests {
 
   // conform checkstyle rule HideUtilityClassConstructor
