19fa88ef1f44136e2565e53bdcc613a953677398
Vincent Link <mail@linkvt.de>
2014-09-12 18:45:54 +0200

    Refactor code

--- src/main/front-end/app/js/filters/entityNameFilter.js
+++ src/main/front-end/app/js/filters/entityNameFilter.js
@@ -5,21 +5,19 @@
 
   app.filter('entityNameFilter', function() {
     return function(entities, query) {
-      if (!query || !(entities instanceof Array)) {
-        return entities;
-      }
-      
+      if (!query || !(entities instanceof Array)) { return entities; }
+
       var filteredEntities = [];
       for (var i = 0, l = entities.length; i < l; i++) {
         var entity = entities[i];
-        
+
         if (entity.displayName.indexOf(query) > -1) {
           filteredEntities.push(entity);
         }
       }
-      
+
       return filteredEntities;
     };
   });
-  
+
 })(angular);
