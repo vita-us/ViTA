bf6e390bd80fe43744bfcc946d9365d008035549
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2014-09-13 14:56:46 +0200

    Add method to the result class.

--- src/main/java/de/unistuttgart/vis/vita/analysis/results/AnnieNLPResult.java
+++ src/main/java/de/unistuttgart/vis/vita/analysis/results/AnnieNLPResult.java
@@ -2,10 +2,20 @@ package de.unistuttgart.vis.vita.analysis.results;
 
 import de.unistuttgart.vis.vita.model.document.Chapter;
 
+import java.util.Set;
+
+import gate.Annotation;
+
 /**
  * The result of ANNIE NLP analysis
  */
 public interface AnnieNLPResult {
+
+  /**
+   * @param chapter The desired chapter.
+   * @return the set with annotations for given chapter.
+   */
+  public Set<Annotation> getAnnotationsForChapter(Chapter chapter);
 
   /**
    * Gets the ANNIE result for a single chapter
