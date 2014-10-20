c1fa947003a1cf1b4694f7668b82843ea9e35f45
Marvin Wyrich <st100245@stud.uni-stuttgart.de>
2014-10-20 16:43:34 +0200

    pass file to documents controller
    just an intermediate result for further discussions

--- src/main/front-end/app/js/documentManagement/documentController.js
+++ src/main/front-end/app/js/documentManagement/documentController.js
@@ -7,10 +7,25 @@
   vitaControllers.controller('DocumentsCtrl', ['$scope', 'Document', 'Page',
       function($scope, Document, Page) {
         Page.setUp('Documents', 1);
+        Page.selectedFiles = null;
 
         Document.get(function(response) {
           $scope.documents = response.documents;
         });
+        
+        $scope.saveFileLocally = function(selectedFiles) {
+          Page.selectedFiles = selectedFiles;
+        };
+        
+        $scope.uploadSelectedFile = function() {
+          if (Page.selectedFiles != null) {
+            // TODO this is where the import starts
+            console.log(Page.selectedFiles[0]);
+          } else {
+            alert("Please select a document first.");
+          }
+        };
+        
       }]);
 
 })(angular);
