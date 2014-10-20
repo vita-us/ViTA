da92427d878bde615b6b0638a0ad1f57e4d45221
Paul <st103454@stud.uni-stuttgart.de>
2014-10-20 20:32:41 +0200

    #85 Add unit-tests for wordcloud-directive

--- src/test/front-end/karma.conf.js
+++ src/test/front-end/karma.conf.js
@@ -9,6 +9,7 @@ module.exports = function(config) {
         'src/main/front-end/app/bower_components/angular-resource/angular-resource.js',
         'src/main/front-end/app/bower_components/angular-mocks/angular-mocks.js',
         'src/main/front-end/app/bower_components/d3/d3.js',
+        'src/main/front-end/app/bower_components/d3-cloud/d3.layout.cloud.js',
         'src/main/front-end/app/js/app.js',
         'src/main/front-end/app/js/*/*.js',
         'src/test/front-end/unit/**/*.js'],
