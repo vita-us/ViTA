546b699b30c2bb5f8c819cc96177c1999acb4879
Vincent Link <mail@linkvt.de>
2014-10-27 15:56:59 +0100

    Implement file upload of document

--- src/main/front-end/app/js/documentManagement/documentController.js
+++ src/main/front-end/app/js/documentManagement/documentController.js
@@ -4,28 +4,28 @@
   var vitaControllers = angular.module('vitaControllers');
 
   // Controller responsible for the documents page
-  vitaControllers.controller('DocumentsCtrl', ['$scope', 'Document', 'Page',
-      function($scope, Document, Page) {
+  vitaControllers.controller('DocumentsCtrl', ['$scope', 'Document', 'Page', 'FileUpload',
+      function($scope, Document, Page, FileUpload) {
         Page.setUp('Documents', 1);
-        Page.selectedFiles = null;
 
         Document.get(function(response) {
           $scope.documents = response.documents;
         });
-        
-        $scope.saveFileLocally = function(selectedFiles) {
-          Page.selectedFiles = selectedFiles;
-        };
-        
+
         $scope.uploadSelectedFile = function() {
-          if (Page.selectedFiles != null) {
-            // TODO this is where the import starts
-            console.log(Page.selectedFiles[0]);
+          if ($scope.file) {
+            FileUpload.uploadFileToUrl($scope.file, '/documents', function(data, status) {
+              // TODO on success
+              console.log(data, status);
+            }, function(data, status) {
+              // TODO error handling
+              console.log(data, status);
+            });
           } else {
             alert("Please select a document first.");
           }
         };
-        
+
       }]);
 
 })(angular);
