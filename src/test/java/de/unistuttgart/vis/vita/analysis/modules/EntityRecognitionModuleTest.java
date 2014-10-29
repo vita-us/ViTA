ea5be429717fb2e38156df612b7d7df2fb61eb93
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2014-10-29 14:09:00 +0100

    Write tests for person and place recognition.

--- src/test/java/de/unistuttgart/vis/vita/analysis/modules/EntityRecognitionModuleTest.java
+++ src/test/java/de/unistuttgart/vis/vita/analysis/modules/EntityRecognitionModuleTest.java
@@ -7,10 +7,19 @@ import de.unistuttgart.vis.vita.analysis.results.BasicEntityCollection;
 import de.unistuttgart.vis.vita.analysis.results.ImportResult;
 import de.unistuttgart.vis.vita.model.document.Chapter;
 import de.unistuttgart.vis.vita.model.document.DocumentPart;
+import de.unistuttgart.vis.vita.model.document.TextPosition;
+import de.unistuttgart.vis.vita.model.document.TextSpan;
+import de.unistuttgart.vis.vita.model.entity.Attribute;
+import de.unistuttgart.vis.vita.model.entity.BasicEntity;
+import de.unistuttgart.vis.vita.model.entity.EntityType;
 
 import org.apache.commons.lang.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -92,8 +101,51 @@ public class EntityRecognitionModuleTest {
   }
 
   @Test
-  public void seeAnnotations() throws Exception {
-    System.out.println(collection.getEntities().size());
-    System.out.println(StringUtils.join(collection.getEntities(), "\n"));
+  public void checkPersonRecognition() throws Exception {
+    BasicEntity person = getEntityByName("Frodo");
+
+    assertNotNull(person);
+    assertThat(person.getType(), is(EntityType.PERSON));
+    assertThat(person.getOccurences(), hasItem(
+        new TextSpan(new TextPosition(chapterObjects.get(0), 5750),
+                     new TextPosition(chapterObjects.get(0), 5759))));
+    assertTrue(checkIfNameExists(person, "Frodo"));
+    assertTrue(checkIfNameExists(person, "Mr. Frodo"));
+  }
+
+  @Test
+  public void checkPlaceRecognition() throws Exception {
+    BasicEntity person = getEntityByName("Buckland");
+
+    assertNotNull(person);
+    assertThat(person.getType(), is(EntityType.PLACE));
+    assertThat(person.getOccurences(), hasItem(
+        new TextSpan(new TextPosition(chapterObjects.get(0), 4103),
+                     new TextPosition(chapterObjects.get(0), 4111))));
+    assertTrue(checkIfNameExists(person, "Buckland"));
+
+    //System.out.println(StringUtils.join(collection.getEntities(), "\n"));
+  }
+
+  private boolean checkIfNameExists(BasicEntity entity, String nameToSearch) {
+    for(Attribute attribute : entity.getNameAttributes()) {
+      if(attribute.getContent().equals(nameToSearch)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private BasicEntity getEntityByName(String name) {
+    for(BasicEntity entity : collection.getEntities()) {
+      for(Attribute attribute : entity.getNameAttributes()) {
+          if(attribute.getContent().equals(name)) {
+            return entity;
+          }
+      }
+    }
+
+    return null;
   }
 }
