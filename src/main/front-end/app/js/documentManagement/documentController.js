ada7ad0071ade9cb010a6dbb5c06fb06ed5c1dd3
Vincent Link <mail@linkvt.de>
2014-10-30 01:41:02 +0100

    Use shared worker services exemplary

--- src/main/front-end/app/js/documentManagement/documentController.js
+++ src/main/front-end/app/js/documentManagement/documentController.js
@@ -4,12 +4,17 @@
   var vitaControllers = angular.module('vitaControllers');
 
   // Controller responsible for the documents page
-  vitaControllers.controller('DocumentsCtrl', ['$scope', 'Document', 'Page',
-      function($scope, Document, Page) {
+  vitaControllers.controller('DocumentsCtrl', ['$scope', 'Document', 'Page', 'DocumentViewSender',
+      function($scope, Document, Page, DocumentViewSender) {
         Page.setUp('Documents', 1);
 
         Document.get(function(response) {
           $scope.documents = response.documents;
+        });
+
+        DocumentViewSender.sendTestMessage();
+        DocumentViewSender.onReceive(function(data) {
+          console.log(data);
         });
       }]);
 
