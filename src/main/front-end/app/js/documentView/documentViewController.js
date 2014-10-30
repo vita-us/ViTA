2c3f9dd09a29eb638ffb5f9872520a41e2e9eec2
Vincent Link <mail@linkvt.de>
2014-10-30 11:43:27 +0100

    Add missing controller for document view

---
+++ src/main/front-end/app/js/documentView/documentViewController.js
@@ -0,0 +1,18 @@
+(function(angular) {
+  'use strict';
+
+  var vitaControllers = angular.module('vitaControllers');
+
+  vitaControllers.controller('DocumentViewCtrl', ['$scope', 'DocumentViewReceiver',
+      function($scope, DocumentViewReceiver) {
+        DocumentViewReceiver.sendTestMessage();
+
+        $scope.response = "nothing received";
+        DocumentViewReceiver.onReceive(function(messageData) {
+          $scope.response = messageData.message || "no message";
+          $scope.$apply();
+        });
+
+      }]);
+
+})(angular);
