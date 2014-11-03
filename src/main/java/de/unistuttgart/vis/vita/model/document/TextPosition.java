e359be077f390ad6197e4eebdec5d7e0e57a694e
Marc <weisemc@studi.informatik.uni-stuttgart.de>
2014-11-03 14:44:27 +0100

    Fix parameter names in 'TextPosition's factory methods and its JavaDoc.

--- src/main/java/de/unistuttgart/vis/vita/model/document/TextPosition.java
+++ src/main/java/de/unistuttgart/vis/vita/model/document/TextPosition.java
@@ -41,22 +41,22 @@ public class TextPosition implements Comparable<TextPosition> {
    * @param pChapter - the chapter this TextPosition lies in
    * @param pOffset - the offset of this TextPosition within the chapter
    */
-  public static TextPosition fromLocalOffset(Chapter chapter, int localOffset) {
+  public static TextPosition fromLocalOffset(Chapter pChapter, int localOffset) {
     return new TextPosition(
-        chapter,
-        chapter.getRange().getStart().getOffset() + localOffset);
+        pChapter,
+        pChapter.getRange().getStart().getOffset() + localOffset);
   }
 
   /**
    * Creates a new TextPosition by specifying the chapter and the document-wide character offset
    *
    * @param pChapter - the chapter this TextPosition lies in
-   * @param pOffset - the global offset of this TextPosition within the document
+   * @param globalOffset - the global offset of this TextPosition within the document
    */
-  public static TextPosition fromGlobalOffset(Chapter chapter, int localOffset) {
+  public static TextPosition fromGlobalOffset(Chapter pChapter, int globalOffset) {
     return new TextPosition(
-        chapter,
-        localOffset);
+        pChapter,
+        globalOffset);
   }
 
   /**
