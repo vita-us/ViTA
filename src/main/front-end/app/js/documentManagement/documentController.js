1212403eb8cf9e1d2c70f7b094f100e03c9b18a6
Vincent Link <mail@linkvt.de>
2014-11-03 15:09:49 +0100

    Poll documents every 5 seconds

--- src/main/front-end/app/js/documentManagement/documentController.js
+++ src/main/front-end/app/js/documentManagement/documentController.js
@@ -5,18 +5,16 @@
 
   // Controller responsible for the documents page
   vitaControllers.controller('DocumentsCtrl', ['$scope', 'Document', 'Page', 'FileUpload',
-      function($scope, Document, Page, FileUpload) {
+      '$interval', function($scope, Document, Page, FileUpload, $interval) {
         Page.setUp('Documents', 1);
 
-        Document.get(function(response) {
-          $scope.documents = response.documents;
-        });
+        loadDocuments();
+        var timerId = $interval(loadDocuments, 5000);
 
         $scope.uploadSelectedFile = function() {
           if ($scope.file) {
             FileUpload.uploadFileToUrl($scope.file, '/documents', function(data, status) {
-              // nothing to do: angular requests the latest document listing and
-              // we poll them too
+              // nothing to do: we poll the documents every X seconds
             }, function(data, status) {
               alert('Upload of ' + $scope.file.name + ' failed.');
             });
@@ -25,6 +23,17 @@
           }
         };
 
+        function loadDocuments() {
+          Document.get(function(response) {
+            $scope.documents = response.documents;
+          });
+        }
+
+        $scope.$on('$destroy', function() {
+          if (timerId) {
+            $interval.cancel(timerId);
+          }
+        });
       }]);
 
 })(angular);
