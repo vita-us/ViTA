2337f9481f4d4e4ee667741caa4adda740d6f904
Paul <st103454@stud.uni-stuttgart.de>
2014-11-04 21:51:25 +0100

    Add onClick to persons list

--- src/main/front-end/app/js/wordcloud/wordcloudController.js
+++ src/main/front-end/app/js/wordcloud/wordcloudController.js
@@ -7,6 +7,10 @@
   vitaControllers.controller('WordcloudCtrl', ['$scope', 'Wordcloud', 'Page', '$routeParams',
       function($scope, Wordcloud, Page, $routeParams) {
 
+        $scope.onClick = function(person) {
+          // Use the clicked person as you want
+        }
+
         Wordcloud.get({
           documentId: $routeParams.documentId
         }, function(response) {
