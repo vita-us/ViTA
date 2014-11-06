b34bf147f98a4d093e0e5e034bdd8e2b8b1d3b63
Sanjeev <sanjeev.b@gmx.de>
2014-11-06 21:51:59 +0100

    #32 Add junit Test to the ContentBuilder class

---
+++ src/test/java/de/unistuttgart/vis/vita/analysis/importer/epub/ContentBuilderTest.java
@@ -0,0 +1,36 @@
+package de.unistuttgart.vis.vita.analysis.importer.epub;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.unistuttgart.vis.vita.importer.epub.ContentBuilder;
+import de.unistuttgart.vis.vita.importer.epub.EpubFileImporter;
+
+public class ContentBuilderTest {
+
+  private ContentBuilder contentBuilder = new ContentBuilder();
+  private String contentBuilderResult;
+
+  @Before
+  public void setUp() throws URISyntaxException, IOException {
+    Path testPath = Paths.get(getClass().getResource("moby-dick-mo-20120214.epub").toURI());
+    EpubFileImporter epubFileImporter = new EpubFileImporter(testPath);
+    contentBuilderResult =
+        contentBuilder.getStringFromInputStream(epubFileImporter.getEbook().getContents().get(6)
+            .getInputStream());
+  }
+
+  @Test
+  public void testStringContent(){
+    assertTrue(contentBuilderResult.contains("<h1 id=\"c01h01\">Chapter 1. Loomings.</h1>"));
+    assertTrue(contentBuilderResult.endsWith("and see what sort of a place this “Spouter\" may be.</p></section></body></html>"));  
+  }
+}
