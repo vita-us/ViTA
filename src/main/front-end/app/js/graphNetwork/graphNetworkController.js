23cf8fa693cbb90c64fa7ae35afb5d215b2131c1
Christian Richter <christian.richter@live.com>
2014-11-06 20:47:13 +0100

    Graphnetwork slider logic

--- src/main/front-end/app/js/graphNetwork/graphNetworkController.js
+++ src/main/front-end/app/js/graphNetwork/graphNetworkController.js
@@ -7,6 +7,21 @@
       function($scope, $routeParams, TestData) {
         // TODO replace when implementing the graph network page
         $scope.entities = TestData.graphNetworkEntities;
+
+        var test = function() {
+          $( "#slider-range" ).slider({
+            range: true,
+            min: 0,
+            max: 100,
+            values: [ 0, 100 ],
+            slide: function( event, ui ) {
+              $( "#amount" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+            }
+          });
+          $( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) +
+               " - " + $( "#slider-range" ).slider( "values", 1 ) );
+        };
+	test();
       }]);
 
 })(angular);
