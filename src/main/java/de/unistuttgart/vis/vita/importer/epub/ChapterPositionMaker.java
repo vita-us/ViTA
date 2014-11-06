64859ed9c7c4055f430d6aa0a4515f5669b9306f
Sanjeev <sanjeev.b@gmx.de>
2014-11-06 21:47:52 +0100

    #32 Adjust the calculate methods of the ChapterPostions

--- src/main/java/de/unistuttgart/vis/vita/importer/epub/ChapterPositionMaker.java
+++ src/main/java/de/unistuttgart/vis/vita/importer/epub/ChapterPositionMaker.java
@@ -10,35 +10,34 @@ public class ChapterPositionMaker {
   private EpublineTraitsExtractor epublineTraitsExtractor;
 
   public ChapterPosition calculateChapterPositionsEpub2(List<List<Epubline>> newChapters,
-      Book newBook) {
+      Book newBook, int textStartOffset) {
 
     epublineTraitsExtractor = new EpublineTraitsExtractor(newBook);
     int currentSize = 0;
     ChapterPosition chapterPosition = new ChapterPosition();
     for (List<Epubline> chapter : newChapters) {
-      
+
       chapterPosition
           .addChapter(chapter.indexOf(epublineTraitsExtractor.getHeading(chapter)) + currentSize,
-              chapter.indexOf(epublineTraitsExtractor.getTextStart(chapter)) + currentSize,
+              chapter.indexOf(epublineTraitsExtractor.getTextStart(chapter))-textStartOffset + currentSize,
               chapter.indexOf(epublineTraitsExtractor.getTextEnd(chapter)) + currentSize);
-      
+
       currentSize = currentSize + chapter.size();
     }
     return chapterPosition;
   }
 
-  public ChapterPosition calculateChapterPositionsEpub3(List<List<String>> newChapters){
-    
+  public ChapterPosition calculateChapterPositionsEpub3(List<List<String>> newChapters) {
+
     int currentSize = 0;
     ChapterPosition chapterPosition = new ChapterPosition();
     for (List<String> chapter : newChapters) {
-      
-      chapterPosition
-          .addChapter(chapter.indexOf(chapter.get(0)) + currentSize,
-              chapter.indexOf(chapter.get(1)) + currentSize,
-              chapter.indexOf(chapter.get(chapter.size()-1)) + currentSize);
-      currentSize = currentSize + chapter.size();
-
+     
+       
+        chapterPosition.addChapter(chapter.indexOf(chapter.get(0)) + currentSize,
+            chapter.indexOf(chapter.get(2)) + currentSize,
+            chapter.indexOf(chapter.get(chapter.size() - 2)) + currentSize);
+        currentSize = currentSize + chapter.size();
     }
     return chapterPosition;
 
