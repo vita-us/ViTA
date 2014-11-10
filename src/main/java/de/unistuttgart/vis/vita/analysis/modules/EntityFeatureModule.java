61d3aa86a0511a5869e759be18506e3116a936cf
Jan Melcher <info@yogularm.de>
2014-11-10 16:37:13 +0100

    Implement EntiryRelationModule

--- src/main/java/de/unistuttgart/vis/vita/analysis/modules/EntityFeatureModule.java
+++ src/main/java/de/unistuttgart/vis/vita/analysis/modules/EntityFeatureModule.java
@@ -1,7 +1,9 @@
 package de.unistuttgart.vis.vita.analysis.modules;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.EntityManager;
 
@@ -9,10 +11,12 @@ import de.unistuttgart.vis.vita.analysis.ModuleResultProvider;
 import de.unistuttgart.vis.vita.analysis.annotations.AnalysisModule;
 import de.unistuttgart.vis.vita.analysis.results.DocumentPersistenceContext;
 import de.unistuttgart.vis.vita.analysis.results.EntityRanking;
+import de.unistuttgart.vis.vita.analysis.results.EntityRelations;
 import de.unistuttgart.vis.vita.model.Model;
 import de.unistuttgart.vis.vita.model.document.Document;
 import de.unistuttgart.vis.vita.model.entity.BasicEntity;
 import de.unistuttgart.vis.vita.model.entity.Entity;
+import de.unistuttgart.vis.vita.model.entity.EntityRelation;
 import de.unistuttgart.vis.vita.model.entity.Person;
 import de.unistuttgart.vis.vita.model.entity.Place;
 import de.unistuttgart.vis.vita.model.progress.AnalysisProgress;
@@ -28,13 +32,15 @@ import de.unistuttgart.vis.vita.model.progress.FeatureProgress;
  * TextSpans to be persistable
  */
 @AnalysisModule(dependencies = {EntityRanking.class, DocumentPersistenceContext.class,
-    Model.class, TextFeatureModule.class})
+    Model.class, TextFeatureModule.class, EntityRelations.class})
 public class EntityFeatureModule extends AbstractFeatureModule<EntityFeatureModule> {
   @Override
   public EntityFeatureModule storeResults(ModuleResultProvider result, Document document,
       EntityManager em) throws Exception {
     List<BasicEntity> basicEntities =
         result.getResultFor(EntityRanking.class).getRankedEntities();
+    EntityRelations relations = result.getResultFor(EntityRelations.class);
+    Map<BasicEntity, Entity> realEntities = new HashMap<>();
     
     int currentRanking = 1;
     for (BasicEntity basicEntity : basicEntities) {
@@ -58,11 +64,26 @@ public class EntityFeatureModule extends AbstractFeatureModule<EntityFeatureModu
       entity.setDisplayName(basicEntity.getDisplayName());
       entity.getAttributes().addAll(basicEntity.getNameAttributes());
       entity.getOccurrences().addAll(basicEntity.getOccurences());
-      // TODO: relations, more attributes
       
       em.persist(entity);
+      realEntities.put(basicEntity, entity);
       
       currentRanking++;
+    }
+    
+    for (BasicEntity basicEntity : basicEntities) {
+      Entity source = realEntities.get(basicEntity);
+      Map<BasicEntity, Double> weights = relations.getRelatedEntities(basicEntity);
+      if (weights == null)
+        continue;
+      for (Map.Entry<BasicEntity, Double> entry : weights.entrySet()) {
+        Entity target = realEntities.get(entry.getKey());
+        EntityRelation<Entity> relation = new EntityRelation<Entity>();
+        relation.setOriginEntity(source);
+        relation.setRelatedEntity(target);
+        relation.setWeight(entry.getValue());
+        em.persist(relation);
+      }
     }
     
     return this;
