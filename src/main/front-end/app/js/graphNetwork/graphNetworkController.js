ca3667342b04eaf210e77d94add280bcbd060fa3
Christian Richter <christian.richter@live.com>
2014-11-13 17:36:11 +0100

    Styleguide fixes

--- src/main/front-end/app/js/graphNetwork/graphNetworkController.js
+++ src/main/front-end/app/js/graphNetwork/graphNetworkController.js
@@ -3,25 +3,26 @@
 
   var vitaControllers = angular.module('vitaControllers');
 
-  vitaControllers.controller('GraphNetworkCtrl', ['$scope', '$routeParams', 'TestData',
+  vitaControllers.controller('GraphNetworkCtrl',
+    ['$scope', '$routeParams', 'TestData',
       function($scope, $routeParams, TestData) {
         // TODO replace when implementing the graph network page
         $scope.entities = TestData.graphNetworkEntities;
 
         var test = function() {
-          $( "#slider-range" ).slider({
+          $('#slider-range').slider({
             range: true,
             min: 0,
             max: 100,
-            values: [ 0, 100 ],
-            slide: function( event, ui ) {
-              $( "#amount" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
-              $scope.rangeStart = ui.values[ 0 ];
-              $scope.rangeEnd = ui.values[ 1 ];
+            values: [0, 100],
+            slide: function(event, ui) {
+              $('#amount').val(ui.values[0] + ' - ' + ui.values[1]);
+              $scope.rangeStart = ui.values[0];
+              $scope.rangeEnd = ui.values[1];
             }
           });
-          $( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) +
-               " - " + $( "#slider-range" ).slider( "values", 1 ) );
+          $('#amount').val($('#slider-range').slider('values', 0) +
+               ' - ' + $('#slider-range').slider('values', 1));
         };
         test();
       }]);
