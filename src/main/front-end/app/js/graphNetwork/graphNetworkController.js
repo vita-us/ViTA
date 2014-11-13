97836573f53722c2bad55e46492b91cc604fee2c
Vincent Link <mail@linkvt.de>
2014-11-13 18:32:00 +0100

    Refactor graph network slider code

--- src/main/front-end/app/js/graphNetwork/graphNetworkController.js
+++ src/main/front-end/app/js/graphNetwork/graphNetworkController.js
@@ -11,24 +11,29 @@
         // TODO replace when implementing the graph network page
         $scope.entities = TestData.graphNetworkEntities;
 
-        var test = function() {
-          $('#slider-range').slider({
-            range: true,
-            min: 0,
-            max: 100,
-            values: [0, 100],
-            slide: function(event, ui) {
-              $("#amount").val(ui.values[0] + ' - ' + ui.values[1]);
-              $scope.rangeStart = ui.values[0] / 100;
-              $scope.rangeEnd = ui.values[1] / 100;
-              $scope.$apply();
-            }
-          });
-          $('#amount').val(
-                  $('#slider-range').slider('values', 0) + ' - '
-                          + $('#slider-range').slider('values', 1));
-        };
-        test();
+        var sliderMin = 0, sliderMax = 100;
+
+        $('#slider-range').slider({
+          range: true,
+          min: sliderMin,
+          max: sliderMax,
+          values: [sliderMin, sliderMax],
+          slide: function(event, ui) {
+            var start = ui.values[0], end = ui.values[1];
+
+            setSliderLabel(start, end);
+
+            $scope.rangeStart = start / 100;
+            $scope.rangeEnd = end / 100;
+            $scope.$apply();
+          }
+        });
+
+        setSliderLabel(sliderMin, sliderMax)
+
+        function setSliderLabel(start, end) {
+          $('#amount').val(start + ' - ' + end);
+        }
       }]);
 
 })(angular);
