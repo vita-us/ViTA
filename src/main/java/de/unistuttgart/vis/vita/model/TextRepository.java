f074e88813b47e1a26d65f93a0484dbd9b95839a
Sanjeev <sanjeev.b@gmx.de>
2014-11-20 20:34:00 +0100

    #32 Remove unused import

--- src/main/java/de/unistuttgart/vis/vita/model/TextRepository.java
+++ src/main/java/de/unistuttgart/vis/vita/model/TextRepository.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.analysis.util.CharArraySet;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
