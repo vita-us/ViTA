eaff071ec7d6f76b320d63be4cb4573a7083a13b
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2014-11-21 18:55:55 +0100

    Use the right importer depending on the file type.

--- src/main/java/de/unistuttgart/vis/vita/analysis/modules/ImportModule.java
+++ src/main/java/de/unistuttgart/vis/vita/analysis/modules/ImportModule.java
@@ -8,10 +8,13 @@ package de.unistuttgart.vis.vita.analysis.modules;
 import java.nio.file.Path;
 
 import de.unistuttgart.vis.vita.analysis.Module;
+import de.unistuttgart.vis.vita.analysis.ModuleExecutionException;
 import de.unistuttgart.vis.vita.analysis.ModuleResultProvider;
 import de.unistuttgart.vis.vita.analysis.ProgressListener;
 import de.unistuttgart.vis.vita.analysis.annotations.AnalysisModule;
 import de.unistuttgart.vis.vita.analysis.results.ImportResult;
+
+import org.apache.commons.io.FilenameUtils;
 
 /**
  * Module to provide the correct importing method either for plain text files or for epub files.
@@ -28,6 +31,37 @@ public class ImportModule extends Module<ImportResult> {
   @Override
   public ImportResult execute(ModuleResultProvider result, ProgressListener progressListener)
       throws Exception {
-    return new TextImportModule(filePath).execute(result, progressListener);
+    String extension = FilenameUtils.getExtension(filePath.getFileName().toString());
+    extension = extension.toLowerCase();
+
+    if(extension.equals(FILETYPE.EPUB.toString())) {
+      return new EpubImportModule(filePath).execute(result, progressListener);
+    } else if (extension.equals(FILETYPE.TXT.toString())) {
+      return new TextImportModule(filePath).execute(result, progressListener);
+    } else {
+      throw new ModuleExecutionException("No supported filetype: " + extension);
+    }
+  }
+}
+
+enum FILETYPE{
+  EPUB("epub"),
+  TXT("txt");
+
+  private final String text;
+
+  /**
+   * @param text
+   */
+  private FILETYPE(final String text) {
+    this.text = text;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Enum#toString()
+   */
+  @Override
+  public String toString() {
+    return text;
   }
 }
