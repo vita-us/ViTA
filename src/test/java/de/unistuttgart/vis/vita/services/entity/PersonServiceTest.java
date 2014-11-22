414f0ef3bcf3dd40aebf8affbb1d874327dfb200
Marc <weisemc@studi.informatik.uni-stuttgart.de>
2014-11-22 11:30:17 +0100

    #54 Add service returning Entities.
    - Add new NamedQuery to find Entities with a specific id
    - Insert Xml-Annotations into 'Entity' for JAXB
    - Add new service method to 'EntityService'
    - Create new test case super class 'EntityServiceTest' and adjust child
      classes 'PersonServiceTest' and 'PlaceServiceTest'

--- src/test/java/de/unistuttgart/vis/vita/services/entity/PersonServiceTest.java
+++ src/test/java/de/unistuttgart/vis/vita/services/entity/PersonServiceTest.java
@@ -1,61 +1,46 @@
 package de.unistuttgart.vis.vita.services.entity;
 
-import static org.junit.Assert.*;
-
-import java.util.Set;
-
-import javax.persistence.EntityManager;
 import javax.ws.rs.core.Application;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.junit.Test;
 
-import de.unistuttgart.vis.vita.data.AttributeTestData;
-import de.unistuttgart.vis.vita.data.DocumentTestData;
-import de.unistuttgart.vis.vita.data.EntityRelationTestData;
-import de.unistuttgart.vis.vita.data.PersonTestData;
-import de.unistuttgart.vis.vita.model.document.Document;
 import de.unistuttgart.vis.vita.model.entity.Attribute;
 import de.unistuttgart.vis.vita.model.entity.EntityRelation;
 import de.unistuttgart.vis.vita.model.entity.Person;
-import de.unistuttgart.vis.vita.services.ServiceTest;
-import de.unistuttgart.vis.vita.services.entity.PersonService;
 
 /**
- * Performs test on PersonService to check whether GET works correctly.
+ * Performs tests on PersonService to check whether GET works correctly.
  */
-public class PersonServiceTest extends ServiceTest {
+public class PersonServiceTest extends EntityServiceTest {
   
-  private String docId;
-  private String personId;
-  private PersonTestData personTestData;
-  private AttributeTestData attributeTestData;
-  private EntityRelationTestData relationTestData;
+  @Override
+  protected Application configure() {
+    return new ResourceConfig(PersonService.class);
+  }
 
   @Override
   public void setUp() throws Exception {
     super.setUp();
-    
-    EntityManager em = getModel().getEntityManager();
-    
-    // set up test data
-    personTestData = new PersonTestData();
-    attributeTestData = new AttributeTestData();
-    relationTestData = new EntityRelationTestData();
-    Attribute testAttribute = attributeTestData.createTestAttribute();
+
+    // create persons
     Person testPerson = personTestData.createTestPerson(1);
     Person relatedPerson = personTestData.createTestPerson(2);
+
+    // create relation
     EntityRelation testRelation = relationTestData.createTestRelation(testPerson, relatedPerson);
     testPerson.getEntityRelations().add(testRelation);
-    
+
+    // create attribute
+    Attribute testAttribute = attributeTestData.createTestAttribute();
     testPerson.getAttributes().add(testAttribute);
-    Document testDoc = new DocumentTestData().createTestDocument(1);
+
+    // add persons to document
     testDoc.getContent().getPersons().add(testPerson);
-    
-    // save ids for request
-    docId = testDoc.getId();
-    personId = testPerson.getId();
-    
+
+    // save id for request
+    entityId = testPerson.getId();
+
     // persist test data
     em.getTransaction().begin();
     em.persist(testAttribute);
@@ -66,36 +51,19 @@ public class PersonServiceTest extends ServiceTest {
     em.getTransaction().commit();
     em.close();
   }
-  
-  @Override
-  protected Application configure() {
-    return new ResourceConfig(PersonService.class);
-  }
-  
+
   /**
    * Checks whether a Person can be got using REST.
    */
   @Test
   public void testGetPerson() {
-    String path = "documents/" + docId + "/persons/" + personId;
-    Person responsePerson = target(path).request().get(Person.class);
-    personTestData.checkData(responsePerson, 1);
-    
-    // check the attributes
-    Set<Attribute> personsAttributes = responsePerson.getAttributes();
-    
-    assertNotNull(personsAttributes);
-    assertEquals(1, personsAttributes.size());
-    
-    for (Attribute attribute: personsAttributes) {
-      attributeTestData.checkData(attribute, 1);
-    }
-    
-    Set<EntityRelation> relations = responsePerson.getEntityRelations();
-    assertEquals(1, relations.size());
-    
-    // can only check whether the weight is correct, other fields are null
-    assertEquals(EntityRelationTestData.TEST_ENTITY_RELATION_WEIGHT, relations.iterator().next().getWeight(), 0.001);
+    Person responsePerson = target(getPath("persons")).request().get(Person.class);
+    check(responsePerson);
   }
 
+  @Override
+  public void testGetEntity() {
+    Person responseEntity = target(getPath("entities")).request().get(Person.class);
+    check(responseEntity);
+  }
 }
--- src/test/java/de/unistuttgart/vis/vita/services/entity/PlaceServiceTest.java
+++ src/test/java/de/unistuttgart/vis/vita/services/entity/PlaceServiceTest.java
@@ -1,61 +1,45 @@
 package de.unistuttgart.vis.vita.services.entity;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-import java.util.Set;
-
-import javax.persistence.EntityManager;
 import javax.ws.rs.core.Application;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.junit.Test;
 
-import de.unistuttgart.vis.vita.data.AttributeTestData;
-import de.unistuttgart.vis.vita.data.DocumentTestData;
-import de.unistuttgart.vis.vita.data.EntityRelationTestData;
-import de.unistuttgart.vis.vita.data.PlaceTestData;
-import de.unistuttgart.vis.vita.model.document.Document;
 import de.unistuttgart.vis.vita.model.entity.Attribute;
 import de.unistuttgart.vis.vita.model.entity.EntityRelation;
 import de.unistuttgart.vis.vita.model.entity.Place;
-import de.unistuttgart.vis.vita.services.ServiceTest;
-import de.unistuttgart.vis.vita.services.entity.PlaceService;
 
 /**
- * Performs test on PlaceService to check whether GET works correctly.
+ * Performs tests on PlaceService to check whether GET works correctly.
  */
-public class PlaceServiceTest extends ServiceTest {
-  
-  private String docId;
-  private String placeId;
-  private PlaceTestData placeTestData;
-  private AttributeTestData attributeTestData;
-  private EntityRelationTestData relationTestData;
+public class PlaceServiceTest extends EntityServiceTest {
+
+  @Override
+  protected Application configure() {
+    return new ResourceConfig(PlaceService.class);
+  }
 
   @Override
   public void setUp() throws Exception {
     super.setUp();
-    
-    EntityManager em = getModel().getEntityManager();
-    
-    // set up test data
-    placeTestData = new PlaceTestData();
-    attributeTestData = new AttributeTestData();
-    relationTestData = new EntityRelationTestData();
-    Attribute testAttribute = attributeTestData.createTestAttribute(1);
+
+    // create places
     Place testPlace = placeTestData.createTestPlace(1);
     Place relatedPlace = placeTestData.createTestPlace(2);
+
+    // create relation
     EntityRelation testRelation = relationTestData.createTestRelation(testPlace, relatedPlace);
     testPlace.getEntityRelations().add(testRelation);
-    
+
+    // create attribute
+    Attribute testAttribute = attributeTestData.createTestAttribute(1);
     testPlace.getAttributes().add(testAttribute);
-    Document testDoc = new DocumentTestData().createTestDocument(1);
+
+    // add places to document
     testDoc.getContent().getPlaces().add(testPlace);
     
-    // save ids for request
-    docId = testDoc.getId();
-    placeId = testPlace.getId();
+    // save id for request
+    entityId = testPlace.getId();
     
     // persist test data
     em.getTransaction().begin();
@@ -67,10 +51,11 @@ public class PlaceServiceTest extends ServiceTest {
     em.getTransaction().commit();
     em.close();
   }
-  
+
   @Override
-  protected Application configure() {
-    return new ResourceConfig(PlaceService.class);
+  public void testGetEntity() {
+    Place responseEntity = target(getPath("entities")).request().get(Place.class);
+    check(responseEntity);
   }
 
   /**
@@ -78,26 +63,8 @@ public class PlaceServiceTest extends ServiceTest {
    */
   @Test
   public void testGetPerson() {
-    String path = "documents/" + docId + "/places/" + placeId;
-    Place responsePlace = target(path).request().get(Place.class);
-    
-    placeTestData.checkData(responsePlace, 1);
-    
-    // check the attributes
-    Set<Attribute> personsAttributes = responsePlace.getAttributes();
-    
-    assertNotNull(personsAttributes);
-    assertEquals(1, personsAttributes.size());
-    
-    for (Attribute attribute: personsAttributes) {
-      attributeTestData.checkData(attribute, 1);
-    }
-    
-    Set<EntityRelation> relations = responsePlace.getEntityRelations();
-    assertEquals(1, relations.size());
-    
-    // can only check whether the weight is correct, other fields are null
-    assertEquals(EntityRelationTestData.TEST_ENTITY_RELATION_WEIGHT, relations.iterator().next().getWeight(), 0.001);
+    Place responsePlace = target(getPath("places")).request().get(Place.class);
+    check(responsePlace);
   }
 
 }
