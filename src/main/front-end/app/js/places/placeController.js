52971a566849301b6061aebc27a1753b85255168
Christian Richter <christian.richter@live.com>
2014-11-23 18:28:29 +0100

    #116 place page has related persons

--- src/main/front-end/app/js/places/placeController.js
+++ src/main/front-end/app/js/places/placeController.js
@@ -4,16 +4,33 @@
   var vitaControllers = angular.module('vitaControllers');
 
   // Controller responsible for the place page
-  vitaControllers.controller('PlaceCtrl', ['$scope', 'Document', 'Page', 'Place', '$routeParams', 'DocumentParts',
-      function($scope, Document, Page, Place, $routeParams, DocumentParts) {
+  vitaControllers.controller('PlaceCtrl',
+      ['$scope', 'Document', 'Page', 'Place', '$routeParams',
+        'DocumentParts', 'Person',
+      function($scope, Document, Page, Place, $routeParams,
+        DocumentParts, Person) {
+        $scope.relatedEntities = [];
         Place.get({
           documentId: $routeParams.documentId,
           placeId: $routeParams.placeId
         }, function(place) {
           $scope.place = place;
 
+        for (var i = 0; i < $scope.place.entityRelations.length; i++) {
+          retrieveEntityName($scope.place.entityRelations[i].relatedEntity);
+        }
+
           Page.breadcrumbs = 'Places > ' + place.displayName;
         });
+
+        var retrieveEntityName = function(id) {
+          var entity = Person.get({
+            documentId: $routeParams.documentId,
+            personId: id
+          }, function(person) {
+            $scope.relatedEntities.push(person);
+          });
+        };
 
         Document.get({
           documentId: $routeParams.documentId
