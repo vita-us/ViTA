fb41bd85ac75d1d6b16c90f0cb11549740b546c1
Christian Richter <christian.richter@live.com>
2014-11-23 17:25:51 +0100

    #116 fingerprint works

--- src/main/front-end/app/js/persons/personController.js
+++ src/main/front-end/app/js/persons/personController.js
@@ -4,8 +4,8 @@
   var vitaControllers = angular.module('vitaControllers');
 
   // Controller responsible for the person page
-  vitaControllers.controller('PersonCtrl', ['$scope', 'Document', 'Page', 'Person', '$routeParams',
-      function($scope, Document, Page, Person, $routeParams) {
+  vitaControllers.controller('PersonCtrl', ['$scope', 'Document', 'Page', 'Person', '$routeParams', 'DocumentParts',
+      function($scope, Document, Page, Person, $routeParams, DocumentParts) {
 
         $scope.relatedEntities = [];
         Person.get({
@@ -34,6 +34,12 @@
         }, function(document) {
           $scope.document = document;
           Page.setUpForDocument(document);
+        });
+
+        DocumentParts.get({
+          documentId: $routeParams.documentId
+        }, function(response) {
+          $scope.parts = response.parts;
         });
       }]);
 })(angular);
