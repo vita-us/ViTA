e80898052d1748cb71f619879b61340eb438b01b
Jan Melcher <info@yogularm.de>
2014-11-23 18:59:14 +0100

    Implement SearchInDocumentService

--- src/main/java/de/unistuttgart/vis/vita/services/search/SearchInDocumentService.java
+++ src/main/java/de/unistuttgart/vis/vita/services/search/SearchInDocumentService.java
@@ -1,23 +1,41 @@
 package de.unistuttgart.vis.vita.services.search;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.annotation.ManagedBean;
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.lucene.queryparser.classic.ParseException;
+
+import de.unistuttgart.vis.vita.model.Model;
+import de.unistuttgart.vis.vita.model.document.Chapter;
+import de.unistuttgart.vis.vita.model.document.Document;
+import de.unistuttgart.vis.vita.model.document.DocumentPart;
+import de.unistuttgart.vis.vita.model.document.TextSpan;
+import de.unistuttgart.vis.vita.model.search.Searcher;
+import de.unistuttgart.vis.vita.services.occurrence.IllegalRangeException;
 import de.unistuttgart.vis.vita.services.occurrence.OccurrencesService;
+import de.unistuttgart.vis.vita.services.responses.occurrence.Occurrence;
 import de.unistuttgart.vis.vita.services.responses.occurrence.OccurrencesResponse;
 
 @ManagedBean
 public class SearchInDocumentService extends OccurrencesService {
+  private static final Logger LOGGER = Logger.getLogger(SearchInDocumentService.class.getName());
 
-  private String documentId;
+  private List<TextSpan> textSpans;
 
   @Inject
-  private EntityManager em;
+  private Model model;
 
   public SearchInDocumentService setDocumentId(String documentId) {
     this.documentId = documentId;
@@ -28,18 +46,84 @@ public class SearchInDocumentService extends OccurrencesService {
   @Produces(MediaType.APPLICATION_JSON)
   public OccurrencesResponse getOccurrences(@QueryParam("steps") int steps,
                                             @QueryParam("rangeStart") double rangeStart,
-                                            @QueryParam("rangeEnd") double rangeEnd,
-                                            @QueryParam("query") String searchQuery) {
+                                            @QueryParam("rangeEnd") @DefaultValue("1") double rangeEnd,
+                                            @QueryParam("query") String query) throws IOException {
+    // first check amount of steps
+    if (steps < 0 || steps > 1000) {
+      throw new WebApplicationException(new IllegalArgumentException("Illegal amount of steps!"), 500);
+    }
 
-    // TODO implement SearchInDocumentService
+    // check range
+    if (rangeEnd < rangeStart) {
+      throw new WebApplicationException("Illegal range!");
+    }
 
-    return new OccurrencesResponse();
+    int startOffset;
+    int endOffset;
+
+    // calculate offsets
+    try {
+      startOffset = getStartOffset(rangeStart);
+      endOffset = getEndOffset(rangeEnd);
+    } catch(IllegalRangeException ire) {
+      throw new WebApplicationException(ire);
+    }
+
+    Chapter startChapter = getSurroundingChapter(startOffset);
+    Chapter endChapter = getSurroundingChapter(endOffset);
+    List<Chapter> chapters = getChaptersInRange(startChapter, endChapter);
+
+    Searcher searcher = new Searcher();
+    try {
+      textSpans = searcher.searchString(documentId, query, chapters, model);
+    } catch (ParseException e) {
+      LOGGER.log(Level.INFO, "Invalid search query: " + query, e);
+      return new OccurrencesResponse(new ArrayList<Occurrence>());
+    }
+
+    List<Occurrence> occs = null;
+    if (steps == 0) {
+      occs = convertSpansToOccurrences(textSpans);
+    } else {
+      occs = getGranularEntityOccurrences(steps, startOffset, endOffset);
+    }
+
+    // put occurrences into a response and send it
+    return new OccurrencesResponse(occs);
+  }
+
+  private List<Chapter> getChaptersInRange(Chapter startChapter, Chapter endChapter) {
+    Document document =
+        em.createNamedQuery("Document.findDocumentById", Document.class)
+        .setParameter("documentId", documentId).getSingleResult();
+    boolean within = false;
+    List<Chapter> chapters = new ArrayList<>();
+    for (DocumentPart part : document.getContent().getParts()) {
+      for (Chapter chapter : part.getChapters()) {
+        if (chapter.equals(startChapter)) {
+          within = true;
+        }
+        if (within) {
+          chapters.add(chapter);
+        }
+        if (chapter.equals(endChapter)) {
+          return chapters;
+        }
+      }
+    }
+    return chapters;
   }
 
   @Override
   protected long getNumberOfSpansInStep(int stepStart, int stepEnd) {
-    // TODO Auto-generated method stub
-    return 0;
+    int count = 0;
+    for (TextSpan span : textSpans) {
+      if (span.getEnd().getOffset() > stepEnd)
+        break;
+      if (span.getStart().getOffset() >= stepStart)
+        count++;
+    }
+    return count;
   }
 
 }
