5038861151a9b868bc1f6051d2c2937159bccd68
Vincent Link <mail@linkvt.de>
2014-11-24 17:32:51 +0100

    Display 'failed' if analysis failed

--- src/main/front-end/app/js/overview/progressStatusDirective.js
+++ src/main/front-end/app/js/overview/progressStatusDirective.js
@@ -12,8 +12,26 @@
         image: '@',
         title: '@'
       },
+      link: link,
       templateUrl: 'templates/progressstatus.html'
     };
+
+    function link(scope) {
+      var status = scope.status;
+
+      scope.isVisible = status && !status.isReady;
+
+      if (status.isFailed) {
+        scope.statusText = 'failed';
+      } else {
+        var progressPercentage = +status.progress * 100;
+
+        // round on 2 decimal places
+        scope.statusText = Math.round(progressPercentage * 100) / 100 + '%';
+      }
+      console.log(scope.status);
+
+    }
 
     return directive;
   });
