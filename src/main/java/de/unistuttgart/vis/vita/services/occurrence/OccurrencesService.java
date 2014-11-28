93a5e27912429815d81947f4b6232124d784bdb5
Marc <weisemc@studi.informatik.uni-stuttgart.de>
2014-11-28 21:47:38 +0100

    #132 Extract new class 'RangeService' from 'OccurrenceService'
    - Extract new abstract super class 'RangeService' from existing
      'OccurrenceService' having all methods related to services on specific
      ranges
    - Made 'EntityRelationsService' a child class of this new super class

--- src/main/java/de/unistuttgart/vis/vita/services/occurrence/OccurrencesService.java
+++ src/main/java/de/unistuttgart/vis/vita/services/occurrence/OccurrencesService.java
@@ -4,14 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.ManagedBean;
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
 import de.unistuttgart.vis.vita.model.document.Chapter;
-import de.unistuttgart.vis.vita.model.document.Document;
 import de.unistuttgart.vis.vita.model.document.TextPosition;
 import de.unistuttgart.vis.vita.model.document.TextSpan;
+import de.unistuttgart.vis.vita.services.RangeService;
 import de.unistuttgart.vis.vita.services.responses.occurrence.Occurrence;
 
 /**
@@ -19,13 +17,7 @@ import de.unistuttgart.vis.vita.services.responses.occurrence.Occurrence;
  * Lists of TextSpans into Lists of Occurrences and get the Document length.
  */
 @ManagedBean
-public abstract class OccurrencesService {
-
-  @Inject
-  protected EntityManager em;
-
-  protected String documentId;
-  private int documentLength;
+public abstract class OccurrencesService extends RangeService {
 
   /**
    * Converts a given List of TextSpans into Occurrences.
@@ -42,69 +34,6 @@ public abstract class OccurrencesService {
     }
 
     return occurrences;
-  }
-
-  /**
-   * Returns the length of the current document.
-   *
-   * @return document length
-   */
-  public int getDocumentLength() {
-    if (documentLength <= 0) {
-      Document doc = readDocumentFromDatabase();
-      documentLength = doc.getMetrics().getCharacterCount();
-    }
-    return documentLength;
-  }
-
-  /**
-   * Reads the current document from the database and returns it.
-   *
-   * @return current document
-   */
-  private Document readDocumentFromDatabase() {
-    TypedQuery<Document> docQuery = em.createNamedQuery("Document.findDocumentById",
-                                                        Document.class);
-    docQuery.setParameter("documentId", documentId);
-    return docQuery.getSingleResult();
-  }
-
-  /**
-   * Returns the absolute offset for a given range start.
-   *
-   * @param rangeStart - the start of a range as a value between 0 and 1
-   * @return the absolute offset (rounded down)
-   * @throws IllegalRangeException if given range value is not valid
-   */
-  public int getStartOffset(double rangeStart) throws IllegalRangeException {
-    if (!isValidRangeValue(rangeStart)) {
-      throw new IllegalRangeException("Illegal range start!");
-    }
-    return (int) Math.floor(getDocumentLength() * rangeStart);
-  }
-
-  /**
-   * Returns the absolute offset for a given range End.
-   *
-   * @param rangeEnd - the end of the range as a value between 0 and 1
-   * @return the absolute offset (rounded up)
-   * @throws IllegalRangeException if given range value is not valid
-   */
-  public int getEndOffset(double rangeEnd) throws IllegalRangeException {
-    if (!isValidRangeValue(rangeEnd)) {
-      throw new IllegalRangeException("Illegal range end!");
-    }
-    return (int) Math.ceil(getDocumentLength() * rangeEnd);
-  }
-
-  /**
-   * Checks whether a given range value is between 0 and 1.
-   *
-   * @param rangeValue - the range value to be checked
-   * @return true if given range value is valid, false otherwise
-   */
-  public boolean isValidRangeValue(double rangeValue) {
-    return rangeValue >= 0 && rangeValue <= 1;
   }
 
   /**
