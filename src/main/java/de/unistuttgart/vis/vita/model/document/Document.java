174181f46932aa2722f571541d03788f1d575ab8
Sanjeev <sanjeev.b@gmx.de>
2014-12-18 00:07:25 +0100

    #156 Add upload date field to Document

--- src/main/java/de/unistuttgart/vis/vita/model/document/Document.java
+++ src/main/java/de/unistuttgart/vis/vita/model/document/Document.java
@@ -2,8 +2,10 @@ package de.unistuttgart.vis.vita.model.document;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Date;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.NamedQueries;
@@ -21,7 +23,8 @@ import de.unistuttgart.vis.vita.model.progress.AnalysisProgress;
 @NamedQueries({
     @NamedQuery(name = "Document.findAllDocuments",
                 query = "SELECT d "
-                      + "FROM Document d"),
+                      + "FROM Document d "
+                      + "ORDER BY d.uploadDate"),
 
     @NamedQuery(name = "Document.findDocumentById",
                 query = "SELECT d "
@@ -44,6 +47,9 @@ public class Document extends AbstractEntityBase {
   private AnalysisProgress progress;
 
   private String filePath;
+  
+  @Column(length = 1000)
+  private Date uploadDate;
 
   /**
    * Creates a new empty document, setting all fields to default values.
@@ -138,4 +144,20 @@ public class Document extends AbstractEntityBase {
     this.filePath = filePath.toString();
   }
 
+  /**
+   * Gets the upload date to the uploaded file
+   * @return the upload date to the uploaded file
+   */
+  public Date getUploadDate() {
+    return uploadDate;
+  }
+  
+  /**
+   * Sets the upload date of the uploaded file
+   * 
+   * @param uploadDate
+   */
+  public void setUploadDate(Date uploadDate) {
+    this.uploadDate = uploadDate;
+  } 
 }
