0404ef45d2f14f0c6d9765d4cd3ea0960ba829bd
Marc <weisemc@studi.informatik.uni-stuttgart.de>
2014-12-25 19:42:33 +0100

    Create new DAO "AnalysisProgressDao".
    - Create and add new DAO class "AnalysisProgressDao"

---
+++ src/main/java/de/unistuttgart/vis/vita/model/dao/AnalysisProgressDao.java
@@ -0,0 +1,32 @@
+package de.unistuttgart.vis.vita.model.dao;
+
+import javax.persistence.TypedQuery;
+
+import de.unistuttgart.vis.vita.model.progress.AnalysisProgress;
+
+/**
+ * Represents a data access object for accessing the progress of the document analysis.
+ */
+public class AnalysisProgressDao extends JpaDao<AnalysisProgress, String> {
+
+  /**
+   * Creates a new data access object for accessing the progress of the analysis.
+   */
+  public AnalysisProgressDao() {
+    super(AnalysisProgress.class);
+  }
+  
+  /**
+   * Finds the AnalysisProgress for the document with the given id.
+   * 
+   * @param docId - the id of the document, which progress should be found
+   * @return the AnalysisProgress for the given document
+   */
+  public AnalysisProgress findByDocumentId(String docId) {
+    TypedQuery<AnalysisProgress> docQuery = em.createNamedQuery("AnalysisProgress.findProgressByDocumentId",
+        AnalysisProgress.class);
+    docQuery.setParameter("docId", docId);
+    return docQuery.getSingleResult();
+  }
+
+}
