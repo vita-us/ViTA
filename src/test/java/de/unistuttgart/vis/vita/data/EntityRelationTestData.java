131cfc77c715d860d20d46fee6f22760bb914d20
Jan Melcher <info@yogularm.de>
2014-12-27 17:30:23 +0100

    #195 Adapt persistence test to change of relation query

--- src/test/java/de/unistuttgart/vis/vita/data/EntityRelationTestData.java
+++ src/test/java/de/unistuttgart/vis/vita/data/EntityRelationTestData.java
@@ -8,17 +8,17 @@ import de.unistuttgart.vis.vita.model.entity.Person;
 import de.unistuttgart.vis.vita.model.entity.Place;
 
 /**
- * Holds test data for relations between entities and methods to create test relations and check 
+ * Holds test data for relations between entities and methods to create test relations and check
  * whether given data matches the test data.
  */
 public class EntityRelationTestData {
-  
+
   public static final double TEST_ENTITY_RELATION_WEIGHT = 0.5;
   public static final double DELTA = 0.001;
-  
+
   private PlaceTestData placeTestData;
   private PersonTestData personTestData;
-  
+
   /**
    * Creates a new instance of EntityRelationTestData.
    */
@@ -29,9 +29,9 @@ public class EntityRelationTestData {
 
   /**
    * Creates a new test relation a given entity.
-   * 
+   *
    * @param relatedEntity - the entity which should be related
-   * @param relatedPerson 
+   * @param relatedPerson
    * @return test relation to given entity
    */
   public EntityRelation createTestRelation(Entity originEntity, Entity relatedEntity) {
@@ -45,25 +45,37 @@ public class EntityRelationTestData {
   /**
    * Checks whether the given entity relation is not <code>null</code> and includes the correct
    * test data.
-   * 
+   *
    * @param entityRelationToCheck - the entity relation to be checked
    */
   public void checkData(EntityRelation entityRelationToCheck) {
     assertNotNull(entityRelationToCheck);
-  
+
     Entity relatedEntity = entityRelationToCheck.getRelatedEntity();
     switch (relatedEntity.getType()) {
       case PERSON:
-        personTestData.checkData((Person) relatedEntity, 2);
+        if (relatedEntity.getDisplayName().equals(PersonTestData.TEST_PERSON_1_NAME)) {
+          personTestData.checkData((Person) entityRelationToCheck.getOriginEntity(), 2);
+          personTestData.checkData((Person) relatedEntity, 1);
+        } else {
+          personTestData.checkData((Person) entityRelationToCheck.getOriginEntity(), 1);
+          personTestData.checkData((Person) relatedEntity, 2);
+        }
         break;
       case PLACE:
-        placeTestData.checkData((Place) relatedEntity, 2);
+        if (relatedEntity.getDisplayName().equals(PlaceTestData.TEST_PLACE_1_NAME)) {
+          placeTestData.checkData((Place) entityRelationToCheck.getOriginEntity(), 2);
+          placeTestData.checkData((Place) relatedEntity, 1);
+        } else {
+          placeTestData.checkData((Place) entityRelationToCheck.getOriginEntity(), 1);
+          placeTestData.checkData((Place) relatedEntity, 2);
+        }
         break;
       default:
         throw new IllegalArgumentException();
     }
-  
+
     assertEquals(TEST_ENTITY_RELATION_WEIGHT, entityRelationToCheck.getWeight(), DELTA);
   }
-  
+
 }
