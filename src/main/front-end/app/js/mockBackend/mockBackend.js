297bf936564d414581598b9381e8cba44b9345c6
Vincent Link <mail@linkvt.de>
2014-12-29 22:13:37 +0100

    Fix formatting

--- src/main/front-end/app/js/mockBackend/mockBackend.js
+++ src/main/front-end/app/js/mockBackend/mockBackend.js
@@ -4,14 +4,14 @@
   var app = angular.module('vita');
 
   app.run([
-      '$httpBackend',
-      'TestData',
-      function($httpBackend, TestData) {
-        $httpBackend.whenGET(new RegExp('/documents/[^/]+/plotview')).respond(TestData.plotviewData);
+    '$httpBackend',
+    'TestData',
+    function($httpBackend, TestData) {
+      $httpBackend.whenGET(new RegExp('/documents/[^/]+/plotview')).respond(TestData.plotviewData);
 
-        $httpBackend.whenGET(new RegExp('.*')).passThrough();
-        $httpBackend.whenPOST(new RegExp('.*')).passThrough();
-        $httpBackend.whenPUT(new RegExp('.*')).passThrough();
-        $httpBackend.whenDELETE(new RegExp('.*')).passThrough();
-      }]);
+      $httpBackend.whenGET(new RegExp('.*')).passThrough();
+      $httpBackend.whenPOST(new RegExp('.*')).passThrough();
+      $httpBackend.whenPUT(new RegExp('.*')).passThrough();
+      $httpBackend.whenDELETE(new RegExp('.*')).passThrough();
+    }]);
 })(angular);
