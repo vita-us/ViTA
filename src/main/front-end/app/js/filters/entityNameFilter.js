6dc4cda40c2460821424abf5d543015701646209
Christian Richter <domyos@localhost.localdomain>
2015-01-06 21:11:24 +0100

    #159 lists will be filtered for alternative names

--- src/main/front-end/app/js/filters/entityNameFilter.js
+++ src/main/front-end/app/js/filters/entityNameFilter.js
@@ -10,12 +10,23 @@
       }
 
       var filteredEntities = [];
-      for (var i = 0, l = entities.length; i < l; i++) {
+      for (var i = 0; i < entities.length; i++) {
         var entity = entities[i];
 
         // Look for the query string case insensitive
         if (containsQueryCaseInsensitive(entity.displayName, query)) {
           filteredEntities.push(entity);
+        }
+
+        // Search in attributes for alternative names
+        for (var j = 0; j < entity.attributes.length; j++) {
+          var attribute = entity.attributes[j];
+          if(attribute.attributetype == 'name' &&
+            containsQueryCaseInsensitive(attribute.content, query)) {
+            if(filteredEntities.indexOf(entity) == -1) {
+              filteredEntities.push(entity);
+            }
+          }
         }
       }
 
