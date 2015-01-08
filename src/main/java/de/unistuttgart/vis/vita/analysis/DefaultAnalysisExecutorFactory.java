2ff8794134b8e05f337986088a4ea07d842f1ba3
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2015-01-08 13:27:01 +0100

    Change analysis factories to work the new document context module.

--- src/main/java/de/unistuttgart/vis/vita/analysis/DefaultAnalysisExecutorFactory.java
+++ src/main/java/de/unistuttgart/vis/vita/analysis/DefaultAnalysisExecutorFactory.java
@@ -1,21 +1,19 @@
 package de.unistuttgart.vis.vita.analysis;
 
-import java.nio.file.Path;
-
 import de.unistuttgart.vis.vita.analysis.modules.DocumentPersistenceContextModule;
 import de.unistuttgart.vis.vita.analysis.modules.ImportModule;
 import de.unistuttgart.vis.vita.analysis.modules.MainAnalysisModule;
 import de.unistuttgart.vis.vita.analysis.modules.ModelProviderModule;
 import de.unistuttgart.vis.vita.model.Model;
+import de.unistuttgart.vis.vita.model.document.Document;
 
 /**
  * Default implementation of {@link AnalysisExecutorFactory}, using a {@link AnalysisScheduler}
  */
 public class DefaultAnalysisExecutorFactory implements AnalysisExecutorFactory {
+  private static final Class<?> TARGET_MODULE = MainAnalysisModule.class;
   private Model model;
   private ModuleRegistry moduleRegistry;
-
-  private static final Class<?> TARGET_MODULE = MainAnalysisModule.class;
 
   public DefaultAnalysisExecutorFactory(Model model, ModuleRegistry moduleRegistry) {
     this.model = model;
@@ -23,11 +21,11 @@ public class DefaultAnalysisExecutorFactory implements AnalysisExecutorFactory {
   }
 
   @Override
-  public AnalysisExecutor createExecutor(String documentId, Path documentPath) {
-    ImportModule importModule = new ImportModule(documentPath);
+  public AnalysisExecutor createExecutor(Document document) {
+    ImportModule importModule = new ImportModule(document.getFilePath());
     ModelProviderModule modelModule = new ModelProviderModule(model);
     DocumentPersistenceContextModule documentPersistenceContextModule =
-        new DocumentPersistenceContextModule(documentId, documentPath);
+        new DocumentPersistenceContextModule(document);
     AnalysisScheduler scheduler = new AnalysisScheduler(moduleRegistry, ModuleClass.get(TARGET_MODULE),
         importModule, modelModule, documentPersistenceContextModule);
     return new AnalysisExecutor(scheduler.getScheduledModules());
