73dd2c733c10fb5e0acfa07d684e4c400a41bf6d
Vincent Link <mail@linkvt.de>
2015-01-14 16:47:25 +0100

    Refactor code

--- src/main/front-end/app/js/plotview/plotviewDirective.js
+++ src/main/front-end/app/js/plotview/plotviewDirective.js
@@ -39,6 +39,139 @@
       var RAW_CHART_HEIGHT = 360;
 
 
+      var WHITE_BACKGROUND_FOR_NAMES = true;
+
+
+
+      function draw_chart(container, safe_name, data, tie_breaker, center_sort) {
+        var margin = {top: 20, right: 25, bottom: 20, left: 1};
+        var width = RAW_CHART_WIDTH - margin.left - margin.right;
+
+        var jscenes = data['scenes'];
+        // This calculation is only relevant for equal_scenes = true
+        var scene_width = (width - longest_name) / (jscenes.length + 1);
+
+        var total_panels = 0;
+        var scenes = [];
+        for (var i = 0; i < jscenes.length; i++) {
+          var duration = parseInt(jscenes[i]['duration']);
+          var start;
+          if (equal_scenes) {
+            start = i * scene_width + longest_name;
+          } else {
+            start = parseInt(jscenes[i]['start']);
+          }
+          var chars = jscenes[i]['chars'];
+          //if (chars.length == 0) continue;
+          scenes[scenes.length] = new SceneNode(jscenes[i]['chars'],
+              start, duration,
+              parseInt(jscenes[i]['id']), jscenes[i].title);
+          scenes[scenes.length - 1].comic_name = safe_name;
+          total_panels += duration;
+        }
+
+        scenes.sort(function(a, b) {
+          return a.start - b.start;
+        });
+        total_panels -= scenes[scenes.length - 1].duration;
+        scenes[scenes.length - 1].duration = 0;
+
+
+        // Make space at the leftmost end of the chart for character names
+        var panel_width = Math.min((width - longest_name) / total_panels, 15);
+        var panel_shift = Math.round(longest_name / panel_width);
+        total_panels += panel_shift;
+        panel_width = Math.min(width / total_panels, 15);
+
+        var xchars = data.characters;
+
+        // Calculate chart height based on the number of characters
+        // TODO: Redo this calculation
+        //var raw_chart_height = xchars.length*(link_width + link_gap + group_gap);// - (link_gap + group_gap);
+        var height = RAW_CHART_HEIGHT - margin.top - margin.bottom;
+
+        var svg = container.append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .attr("class", "chart")
+            .attr("id", safe_name)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+        var chars = [];
+        var char_map = []; // maps id to pointer
+        for (var i = 0; i < xchars.length; i++) {
+          chars[chars.length] = new Character_(xchars[i].name, xchars[i].id, xchars[i].group);
+          char_map[xchars[i].id] = chars[chars.length - 1];
+        }
+
+        var groups = define_groups(chars);
+        find_median_groups(groups, scenes, chars, char_map, tie_breaker);
+        groups = sort_groups_main(groups, center_sort);
+
+        var links = generate_links(chars, scenes);
+        var char_scenes = add_char_scenes(chars, scenes, links, groups, panel_shift, safe_name);
+
+
+        // Determine the position of each character in each group
+        // (if it ever appears in the scenes that appear in that
+        // group)
+        groups.forEach(function(g) {
+          g.all_chars.sort(function(a, b) {
+            return a.group_ptr.order - b.group_ptr.order;
+          });
+          var y = g.min;
+          for (var i = 0; i < g.all_chars.length; i++) {
+            g.all_chars[i].group_positions[g.id] = y + i * (text_height);
+          }
+        });
+
+
+        calculate_node_positions(chars, scenes, total_panels,
+            width, height, char_scenes, groups, panel_width,
+            panel_shift, char_map);
+
+
+        scenes.forEach(function(s) {
+          if (!s.char_node) {
+            var first_scenes = [];
+            //ys = [];
+            s.in_links.forEach(function(l) {
+              if (l.from.char_node) {
+                first_scenes[first_scenes.length] = l.from;
+              }
+            });
+            for (var i = 0; i < first_scenes.length; i++) {
+              first_scenes[i].y = s.y + s.height / 2.0 + i * text_height;
+            }
+          }
+        });
+
+        // Determining the y-positions of the names (i.e. the char scenes)
+        // if the appear at the beginning of the chart
+        char_scenes.forEach(function(cs) {
+
+          var character = char_map[cs.chars[0]];
+          if (character.first_scene.x < per_width * width) {
+            // The median group of the first scene in which the character appears
+            // We want the character's name to appear in that group
+            var first_group = character.first_scene.median_group;
+            cs.y = character.group_positions[first_group.id];
+          }
+        });
+
+        calculate_link_positions(scenes, chars, groups, char_map);
+
+        height = groups[groups.length - 1].max + group_gap * 5;
+        RAW_CHART_HEIGHT = height + margin.top + margin.bottom;
+        d3.select('svg#' + safe_name).style("height", RAW_CHART_HEIGHT);
+
+        draw_links(links, svg);
+        draw_nodes(scenes, svg, width, height, RAW_CHART_HEIGHT, safe_name);
+      }
+
+
       // Height of empty gaps between groups
       // (Sparse groups and group ordering already
       // provide a lot of whitespace though.)
@@ -57,8 +190,6 @@
       // scene's x value by this many pixels
       var name_shift = 10;
 
-      // True: Use a white background for names
-      var name_bg = true;
 
       // True: Disregard actual scene duration and make
       // all the scenes equal.
@@ -603,7 +734,7 @@
             });
 
         // White background for the names
-        if (name_bg) {
+        if (WHITE_BACKGROUND_FOR_NAMES) {
           node.append("rect")
               .filter(function(d) {
                 return d.char_node;
@@ -702,133 +833,6 @@
         }
       }
 
-      function draw_chart(container, safe_name, data, tie_breaker, center_sort) {
-        var margin = {top: 20, right: 25, bottom: 20, left: 1};
-        var width = RAW_CHART_WIDTH - margin.left - margin.right;
-
-        var jscenes = data['scenes'];
-        // This calculation is only relevant for equal_scenes = true
-        var scene_width = (width - longest_name) / (jscenes.length + 1);
-
-        var total_panels = 0;
-        var scenes = [];
-        for (var i = 0; i < jscenes.length; i++) {
-          var duration = parseInt(jscenes[i]['duration']);
-          var start;
-          if (equal_scenes) {
-            start = i * scene_width + longest_name;
-          } else {
-            start = parseInt(jscenes[i]['start']);
-          }
-          var chars = jscenes[i]['chars'];
-          //if (chars.length == 0) continue;
-          scenes[scenes.length] = new SceneNode(jscenes[i]['chars'],
-              start, duration,
-              parseInt(jscenes[i]['id']), jscenes[i].title);
-          scenes[scenes.length - 1].comic_name = safe_name;
-          total_panels += duration;
-        }
-
-        scenes.sort(function(a, b) {
-          return a.start - b.start;
-        });
-        total_panels -= scenes[scenes.length - 1].duration;
-        scenes[scenes.length - 1].duration = 0;
-
-
-        // Make space at the leftmost end of the chart for character names
-        var panel_width = Math.min((width - longest_name) / total_panels, 15);
-        var panel_shift = Math.round(longest_name / panel_width);
-        total_panels += panel_shift;
-        panel_width = Math.min(width / total_panels, 15);
-        
-        var xchars = data.characters;
-
-        // Calculate chart height based on the number of characters
-        // TODO: Redo this calculation
-        //var raw_chart_height = xchars.length*(link_width + link_gap + group_gap);// - (link_gap + group_gap);
-        var height = RAW_CHART_HEIGHT - margin.top - margin.bottom;
-
-        var svg = container.append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .attr("class", "chart")
-            .attr("id", safe_name)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-
-        var chars = [];
-        var char_map = []; // maps id to pointer
-        for (var i = 0; i < xchars.length; i++) {
-          chars[chars.length] = new Character_(xchars[i].name, xchars[i].id, xchars[i].group);
-          char_map[xchars[i].id] = chars[chars.length - 1];
-        }
-
-        var groups = define_groups(chars);
-        find_median_groups(groups, scenes, chars, char_map, tie_breaker);
-        groups = sort_groups_main(groups, center_sort);
-
-        var links = generate_links(chars, scenes);
-        var char_scenes = add_char_scenes(chars, scenes, links, groups, panel_shift, safe_name);
-
-
-        // Determine the position of each character in each group
-        // (if it ever appears in the scenes that appear in that
-        // group)
-        groups.forEach(function(g) {
-          g.all_chars.sort(function(a, b) {
-            return a.group_ptr.order - b.group_ptr.order;
-          });
-          var y = g.min;
-          for (var i = 0; i < g.all_chars.length; i++) {
-            g.all_chars[i].group_positions[g.id] = y + i * (text_height);
-          }
-        });
-
-
-        calculate_node_positions(chars, scenes, total_panels,
-            width, height, char_scenes, groups, panel_width,
-            panel_shift, char_map);
-
-
-        scenes.forEach(function(s) {
-          if (!s.char_node) {
-            var first_scenes = [];
-            //ys = [];
-            s.in_links.forEach(function(l) {
-              if (l.from.char_node) {
-                first_scenes[first_scenes.length] = l.from;
-              }
-            });
-            for (var i = 0; i < first_scenes.length; i++) {
-              first_scenes[i].y = s.y + s.height / 2.0 + i * text_height;
-            }
-          }
-        });
-
-        // Determining the y-positions of the names (i.e. the char scenes)
-        // if the appear at the beginning of the chart
-        char_scenes.forEach(function(cs) {
-
-          var character = char_map[cs.chars[0]];
-          if (character.first_scene.x < per_width * width) {
-            // The median group of the first scene in which the character appears
-            // We want the character's name to appear in that group
-            var first_group = character.first_scene.median_group;
-            cs.y = character.group_positions[first_group.id];
-          }
-        });
-
-        calculate_link_positions(scenes, chars, groups, char_map);
-
-        height = groups[groups.length - 1].max + group_gap * 5;
-        RAW_CHART_HEIGHT = height + margin.top + margin.bottom;
-        d3.select('svg#' + safe_name).style("height", RAW_CHART_HEIGHT);
-
-        draw_links(links, svg);
-        draw_nodes(scenes, svg, width, height, RAW_CHART_HEIGHT, safe_name);
-      }
 
       return {
         restrict: 'A',
