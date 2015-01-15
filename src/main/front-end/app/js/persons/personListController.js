d419deacd1b53ee91ddcd3e5085911332b2bf30c
Christian Richter <christian.richter@live.com>
2015-01-15 15:35:00 +0100

    #222 cleanup according to style guide

--- src/main/front-end/app/js/persons/personListController.js
+++ src/main/front-end/app/js/persons/personListController.js
@@ -4,31 +4,32 @@
   var vitaControllers = angular.module('vitaControllers');
 
   // Controller responsible for the persons page
-  vitaControllers.controller('PersonListCtrl', ['$scope', 'Document', 'DocumentParts', 'Page', 'Person', 'Entity',
-      '$routeParams', 'CssClass', function($scope, Document, DocumentParts, Page, Person, Entity, $routeParams, CssClass) {
+  vitaControllers.controller('PersonListCtrl',
+    ['$scope', 'Document', 'DocumentParts', 'Page', 'Person', 'Entity', '$routeParams', 'CssClass',
+      function($scope, Document, DocumentParts, Page, Person, Entity, $routeParams, CssClass) {
 
         var MAX_DISPLAYED_COOCCURRENCES = 5;
 
         // Provide the service for direct usage in the scope
         $scope.CssClass = CssClass;
 
-	// Create an empty selected character
-	$scope.selected = {};
+        // Create an empty selected character
+        $scope.selected = {};
 
-	// Entities related to the selected character	
+        // Entities related to the selected character
         $scope.relatedEntities = [];
 
-	// Ids of the selected entities for the corresponding fingerprint
+        // Ids of the selected entities for the corresponding fingerprint
         $scope.fingerprintIds = [];
 
-	// Used to select a person from the list
-	$scope.select = function(person) {
-	  $scope.selected = person;
- 	  $scope.fingerprintIds = [];
-	  $scope.relatedEntities = [];
-	  $scope.fingerprintIds.push(person.id);
+        // Used to select a person from the list
+        $scope.select = function(person) {
+          $scope.selected = person;
+          $scope.fingerprintIds = [];
+          $scope.relatedEntities = [];
+          $scope.fingerprintIds.push(person.id);
 
-	  var relations = person.entityRelations;
+          var relations = person.entityRelations;
           relations.sort(function(a, b) {
             // move relations with high weights to the front
             return b.weight - a.weight;
@@ -37,20 +38,20 @@
           for (var i = 0; i < displayedOccurrenceCount; i++) {
             retrieveEntity(relations[i].relatedEntity, i);
           }
-	};
+        };
 
-	var retrieveEntity = function(id, index) {
+        var retrieveEntity = function(id, index) {
           var entity = Entity.get({
             documentId: $routeParams.documentId,
             entityId: id
           }, function(entity) {
-	    if($scope.relatedEntities.indexOf(entity) == -1) {
+            if ($scope.relatedEntities.indexOf(entity) == -1) {
               $scope.relatedEntities.push(entity);
-	    }
+          }
           });
         };
 
-	$scope.setFingerprint = function(id) {
+        $scope.setFingerprint = function(id) {
           var position = $scope.fingerprintIds.indexOf(id);
           if (position > -1) {
             $scope.fingerprintIds.splice(position, 1);
@@ -59,24 +60,24 @@
           }
         };
 
-	$scope.isSelected = function(person) {
-	  return person == $scope.selected;
-	};
+        $scope.isSelected = function(person) {
+          return person == $scope.selected;
+        };
 
-	$scope.isMarked = function(id) {
+        $scope.isMarked = function(id) {
           return ($scope.fingerprintIds.indexOf(id) > -1);
         };
 
 
-	// Get a list of characters from the server
+        // Get a list of characters from the server
         Person.get({
           documentId: $routeParams.documentId
         }, function(response) {
           $scope.persons = response.persons;
-	  $scope.select(response.persons[0]);
+          $scope.select(response.persons[0]);
         });
 
-	// Get document related details from the server
+        // Get document related details from the server
         Document.get({
           documentId: $routeParams.documentId
         }, function(document) {
--- src/main/front-end/app/js/places/placeListController.js
+++ src/main/front-end/app/js/places/placeListController.js
@@ -4,24 +4,25 @@
   var vitaControllers = angular.module('vitaControllers');
 
   // Controller responsible for the places page
-  vitaControllers.controller('PlaceListCtrl', ['$scope', 'Document', 'DocumentParts', 'Page', 'Place', 'Entity',
-      '$routeParams', function($scope, Document, DocumentParts, Page, Place, Entity, $routeParams) {
+  vitaControllers.controller('PlaceListCtrl',
+    ['$scope', 'Document', 'DocumentParts', 'Page', 'Place', 'Entity', '$routeParams'
+    function($scope, Document, DocumentParts, Page, Place, Entity, $routeParams) {
 
         var MAX_DISPLAYED_COOCCURRENCES = 5;
 
-	$scope.selected = {};
+        $scope.selected = {};
 
         $scope.relatedEntities = [];
         $scope.fingerprintIds = [];
 
-	// Used to select a person from the list
-	$scope.select = function(place) {
-	  $scope.selected = place;
- 	  $scope.fingerprintIds = [];
-	  $scope.relatedEntities = [];
-	  $scope.fingerprintIds.push(place.id);
+        // Used to select a person from the list
+        $scope.select = function(place) {
+          $scope.selected = place;
+          $scope.fingerprintIds = [];
+          $scope.relatedEntities = [];
+          $scope.fingerprintIds.push(place.id);
 
-	  var relations = place.entityRelations;
+          var relations = place.entityRelations;
           relations.sort(function(a, b) {
             // move relations with high weights to the front
             return b.weight - a.weight;
@@ -30,20 +31,20 @@
           for (var i = 0; i < displayedOccurrenceCount; i++) {
             retrieveEntity(relations[i].relatedEntity, i);
           }
-	};
+        };
 
-	var retrieveEntity = function(id, index) {
+        var retrieveEntity = function(id, index) {
           var entity = Entity.get({
             documentId: $routeParams.documentId,
             entityId: id
           }, function(entity) {
-	    if($scope.relatedEntities.indexOf(entity) == -1) {
+            if ($scope.relatedEntities.indexOf(entity) == -1) {
               $scope.relatedEntities.push(entity);
-	    }
+            }
           });
         };
 
-	$scope.setFingerprint = function(id) {
+        $scope.setFingerprint = function(id) {
           var position = $scope.fingerprintIds.indexOf(id);
           if (position > -1) {
             $scope.fingerprintIds.splice(position, 1);
@@ -52,11 +53,11 @@
           }
         };
 
-	$scope.isSelected = function(place) {
-	  return place == $scope.selected;
-	};
+        $scope.isSelected = function(place) {
+          return place == $scope.selected;
+        };
 
-	$scope.isMarked = function(id) {
+        $scope.isMarked = function(id) {
           return ($scope.fingerprintIds.indexOf(id) > -1);
         };
 
@@ -64,7 +65,7 @@
           documentId: $routeParams.documentId
         }, function(placesWrapper) {
           $scope.places = placesWrapper.places;
-	  $scope.select(placesWrapper.places[0]);
+          $scope.select(placesWrapper.places[0]);
         });
 
         Document.get({
