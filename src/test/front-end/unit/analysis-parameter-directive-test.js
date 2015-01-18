b09b7e72b21bab475d2326a41cfcf29ceac99b7c
Paul <st103454@stud.uni-stuttgart.de>
2015-01-18 18:01:20 +0100

    #224 Add unit tests for model changes

--- src/test/front-end/unit/analysis-parameter-directive-test.js
+++ src/test/front-end/unit/analysis-parameter-directive-test.js
@@ -16,6 +16,7 @@ describe('Analysis Parameter Directive', function() {
 
     element = $compile(element)(scope);
     scope.$digest();
+
   }));
 
   it('should set correct default values', function() {
@@ -32,6 +33,32 @@ describe('Analysis Parameter Directive', function() {
         expect(inputElement.val()).toBe(parameter.value);
       }
     });
+  });
+
+  it('should change string model when input value changes', function() {
+    var inputElement = element.find('#' + scope.analysisParameters[5].name);
+    inputElement.val('bla').trigger('input');
+    expect(scope.analysisParameters[5].value).toBe('bla');
+  });
+
+  it('should change number model when input value changes', function() {
+    var inputElement = element.find('#' + scope.analysisParameters[0].name);
+    inputElement.val(15).trigger('input');
+    expect(scope.analysisParameters[0].value).toBe(15);
+  });
+
+  it('should change boolean model when input value changes', function() {
+    var oldValue = scope.analysisParameters[2].value;
+    var inputElement = element.find('#' + scope.analysisParameters[2].name);
+    inputElement.trigger('click');
+    expect(scope.analysisParameters[2].value).toBe(!oldValue);
+  });
+
+  it('should change enum model when input value changes', function() {
+    var newValue = scope.analysisParameters[4].values[0];
+    var inputElement = element.find('#' + newValue.name);
+    inputElement.prop("checked", true).trigger("click");
+    expect(scope.analysisParameters[4].value).toBe(newValue.name);
   });
 
 });
