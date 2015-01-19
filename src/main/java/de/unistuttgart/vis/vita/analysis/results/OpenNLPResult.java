8739216c52adcd37b7ed0332efd68c10a4dc43bd
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2015-01-19 15:33:58 +0100

    Extend interfaces.

--- src/main/java/de/unistuttgart/vis/vita/analysis/results/OpenNLPResult.java
+++ src/main/java/de/unistuttgart/vis/vita/analysis/results/OpenNLPResult.java
@@ -5,23 +5,9 @@
 
 package de.unistuttgart.vis.vita.analysis.results;
 
-import de.unistuttgart.vis.vita.model.document.Chapter;
-
-import java.util.Set;
-
-import gate.Annotation;
-
 /**
  *
  */
-public interface OpenNLPResult {
-
-  /**
-   * Gets the OpenNLP result for a single chapter
-   *
-   * @param chapter The desired chapter.
-   * @return the set with annotations for given chapter.
-   */
-  public Set<Annotation> getAnnotationsForChapter(Chapter chapter);
+public interface OpenNLPResult extends NLPResult {
 
 }
