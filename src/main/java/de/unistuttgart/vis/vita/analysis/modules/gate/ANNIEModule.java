f550955e12b20f77eab51544f8131f6330dee64d
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2015-01-20 23:13:24 +0100

    Use the abstract nlp module.

--- src/main/java/de/unistuttgart/vis/vita/analysis/modules/gate/ANNIEModule.java
+++ src/main/java/de/unistuttgart/vis/vita/analysis/modules/gate/ANNIEModule.java
@@ -5,37 +5,23 @@
 
 package de.unistuttgart.vis.vita.analysis.modules.gate;
 
-import de.unistuttgart.vis.vita.analysis.Module;
-import de.unistuttgart.vis.vita.analysis.ModuleResultProvider;
-import de.unistuttgart.vis.vita.analysis.ProgressListener;
 import de.unistuttgart.vis.vita.analysis.annotations.AnalysisModule;
-import de.unistuttgart.vis.vita.analysis.modules.GateInitializeModule;
-import de.unistuttgart.vis.vita.analysis.modules.gate.GateControllerProgress;
 import de.unistuttgart.vis.vita.analysis.results.AnnieDatastore;
 import de.unistuttgart.vis.vita.analysis.results.AnnieNLPResult;
 import de.unistuttgart.vis.vita.analysis.results.DocumentPersistenceContext;
 import de.unistuttgart.vis.vita.analysis.results.ImportResult;
 import de.unistuttgart.vis.vita.model.document.Chapter;
-import de.unistuttgart.vis.vita.model.document.DocumentPart;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 import gate.Annotation;
-import gate.AnnotationSet;
-import gate.Corpus;
-import gate.Document;
-import gate.Factory;
 import gate.Gate;
 import gate.creole.ANNIEConstants;
 import gate.creole.ConditionalSerialAnalyserController;
-import gate.creole.ExecutionException;
 import gate.creole.ResourceInstantiationException;
-import gate.util.GateException;
+import gate.persist.PersistenceException;
 import gate.util.persistence.PersistenceManager;
 
 /**
@@ -43,89 +29,14 @@ import gate.util.persistence.PersistenceManager;
  */
 @AnalysisModule(dependencies = {GateInitializeModule.class, AnnieDatastore.class,
                                 DocumentPersistenceContext.class, ImportResult.class}, weight = 500)
-public class ANNIEModule extends Module<AnnieNLPResult> {
-  private ImportResult importResult;
-  private ProgressListener progressListener;
-  private ConditionalSerialAnalyserController controller;
-  private Map<Document, Chapter> docToChapter = new HashMap<>();
-  private Map<Chapter, Set<Annotation>> chapterToAnnotation = new HashMap<>();
-  private Corpus corpus;
-
-  @Override
-  public AnnieNLPResult execute(ModuleResultProvider result, ProgressListener progressListener)
-      throws Exception {
-    importResult = result.getResultFor(ImportResult.class);
-    DocumentPersistenceContext documentIdModule =
-        result.getResultFor(DocumentPersistenceContext.class);
-    AnnieDatastore storeModule = result.getResultFor(AnnieDatastore.class);
-    this.progressListener = progressListener;
-    String documentName = documentIdModule.getFileName();
-
-    corpus = storeModule.getStoredAnalysis(documentName);
-
-    // Persist the annie analysis into the datastore with the document id
-    if (corpus == null) {
-      loadAnnie();
-      createCorpus();
-      startAnnie();
-      storeModule.storeResult(corpus, documentName);
-    }
-
-    createResultMap();
-
-    progressListener.observeProgress(1);
-    return buildResult();
-  }
-
-  private void createResultMap() {
-    for (Object docObj : corpus) {
-      Document doc = (Document) docObj;
-      AnnotationSet defaultAnnotSet = doc.getAnnotations();
-      Set<String> annotTypesRequired = new HashSet<>();
-      annotTypesRequired.add(ANNIEConstants.PERSON_ANNOTATION_TYPE);
-      annotTypesRequired.add(ANNIEConstants.LOCATION_ANNOTATION_TYPE);
-      Set<Annotation> peopleAndPlaces = new HashSet<>(defaultAnnotSet.get(annotTypesRequired));
-      chapterToAnnotation.put(docToChapter.get(doc), peopleAndPlaces);
-    }
-  }
-
-  /**
-   * Starts the real execution on the corpus with the annie controller.
-   */
-  private void startAnnie() throws ExecutionException {
-    controller.setCorpus(corpus);
-
-    int maxDocuments = corpus.size();
-
-    controller.addProgressListener(
-        new GateControllerProgress(progressListener, maxDocuments));
-
-    try {
-      controller.execute();
-    } catch (IllegalStateException e) {
-      System.out.println(e.getCause().toString());
-    }
-  }
-
-  /**
-   * Creates the Gate corpus out of the available chapters.
-   */
-  private void createCorpus() throws ResourceInstantiationException {
-    corpus = Factory.newCorpus("ViTA Corpus");
-
-    for (DocumentPart part : importResult.getParts()) {
-      for (Chapter chapter : part.getChapters()) {
-        Document doc = Factory.newDocument(chapter.getText());
-        docToChapter.put(doc, chapter);
-        corpus.add(doc);
-      }
-    }
-  }
+public class ANNIEModule extends AbstractNLPModule<AnnieNLPResult> {
 
   /**
    * Initialize the ANNIE plugin for execution.
    */
-  private void loadAnnie() throws GateException, IOException {
+  @Override
+  protected void loadEngine()
+      throws ResourceInstantiationException, IOException, PersistenceException {
     if (controller != null) {
       return;
     }
@@ -141,7 +52,8 @@ public class ANNIEModule extends Module<AnnieNLPResult> {
   /**
    * @return The result with a set of annotations.
    */
-  private AnnieNLPResult buildResult() {
+  @Override
+  protected AnnieNLPResult buildResult() {
     return new AnnieNLPResult() {
       @Override
       public Set<Annotation> getAnnotationsForChapter(Chapter chapter) {
--- src/main/java/de/unistuttgart/vis/vita/analysis/modules/gate/OpenNLPModule.java
+++ src/main/java/de/unistuttgart/vis/vita/analysis/modules/gate/OpenNLPModule.java
@@ -5,35 +5,20 @@
 
 package de.unistuttgart.vis.vita.analysis.modules.gate;
 
-import de.unistuttgart.vis.vita.analysis.Module;
-import de.unistuttgart.vis.vita.analysis.ModuleResultProvider;
-import de.unistuttgart.vis.vita.analysis.ProgressListener;
 import de.unistuttgart.vis.vita.analysis.annotations.AnalysisModule;
-import de.unistuttgart.vis.vita.analysis.modules.GateInitializeModule;
-import de.unistuttgart.vis.vita.analysis.modules.gate.GateControllerProgress;
 import de.unistuttgart.vis.vita.analysis.results.AnnieDatastore;
 import de.unistuttgart.vis.vita.analysis.results.DocumentPersistenceContext;
 import de.unistuttgart.vis.vita.analysis.results.ImportResult;
 import de.unistuttgart.vis.vita.analysis.results.OpenNLPResult;
 import de.unistuttgart.vis.vita.model.document.Chapter;
-import de.unistuttgart.vis.vita.model.document.DocumentPart;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 import gate.Annotation;
-import gate.AnnotationSet;
-import gate.Corpus;
-import gate.Document;
-import gate.Factory;
 import gate.Gate;
 import gate.creole.ConditionalSerialAnalyserController;
-import gate.creole.ExecutionException;
-import gate.creole.ResourceInstantiationException;
 import gate.util.GateException;
 import gate.util.persistence.PersistenceManager;
 
@@ -43,87 +28,13 @@ import gate.util.persistence.PersistenceManager;
 @AnalysisModule(dependencies = {GateInitializeModule.class, AnnieDatastore.class,
                                 DocumentPersistenceContext.class,
                                 ImportResult.class}, weight = 500)
-public class OpenNLPModule extends Module<OpenNLPResult> {
-
+public class OpenNLPModule extends AbstractNLPModule<OpenNLPResult> {
   public static final String PLUGIN_DIR = "OpenNLP";
   public static final String DEFAULT_FILE = "opennlp_state.xgapp";
-  private static final String ANNOTATION_TYPE_PERSON = "Person";
-  private static final String ANNOTATION_TYPE_ORGANIZATION = "Organization";
-  private static final String ANNOTATION_TYPE_LOCATION = "Location";
-
-  private ImportResult importResult;
-  private DocumentPersistenceContext documentIdModule;
-  private ProgressListener progressListener;
-  private ConditionalSerialAnalyserController controller;
-  private Map<Document, Chapter> docToChapter = new HashMap<>();
-  private Map<Chapter, Set<Annotation>> chapterToAnnotation = new HashMap<>();
-  private Corpus corpus;
 
   @Override
-  public OpenNLPResult execute(ModuleResultProvider results, ProgressListener progressListener)
-      throws Exception {
-    this.progressListener = progressListener;
-    importResult = results.getResultFor(ImportResult.class);
-    documentIdModule = results.getResultFor(DocumentPersistenceContext.class);
-    AnnieDatastore storeModule = results.getResultFor(AnnieDatastore.class);
-    String documentName = documentIdModule.getFileName();
-
-    corpus = storeModule.getStoredAnalysis(documentName);
-
-    // Persist the annie analysis into the datastore with the document id
-    if (corpus == null) {
-      loadEngine();
-      createCorpus();
-      startAnalysis();
-      storeModule.storeResult(corpus, documentName);
-    }
-
-    createResultMap();
-    progressListener.observeProgress(1);
-
-    return buildResult();
-  }
-
-  private OpenNLPResult buildResult() {
-    return new OpenNLPResult() {
-      @Override
-      public Set<Annotation> getAnnotationsForChapter(Chapter chapter) {
-        if (!chapterToAnnotation.containsKey(chapter)) {
-          throw new IllegalArgumentException("This chapter has not been analyzed");
-        }
-
-        return chapterToAnnotation.get(chapter);
-      }
-    };
-  }
-
-  private void createResultMap() {
-    for (Object docObj : corpus) {
-      Document doc = (Document) docObj;
-      AnnotationSet defaultAnnotSet = doc.getAnnotations();
-      Set<String> annotTypesRequired = new HashSet<>();
-      annotTypesRequired.add(ANNOTATION_TYPE_LOCATION);
-      Set<Annotation> peopleAndPlaces = new HashSet<>(defaultAnnotSet.get(annotTypesRequired));
-      chapterToAnnotation.put(docToChapter.get(doc), peopleAndPlaces);
-    }
-  }
-
-  private void startAnalysis() throws ExecutionException {
-    controller.setCorpus(corpus);
-
-    int maxDocuments = corpus.size();
-
-    controller.addProgressListener(
-        new GateControllerProgress(progressListener, maxDocuments));
-
-    try {
-      controller.execute();
-    } catch (IllegalStateException e) {
-      System.out.println(e.getCause().toString());
-    }
-  }
-
-  private void loadEngine() throws GateException, IOException {
+  protected void loadEngine()
+      throws GateException, IOException {
     if (controller != null) {
       return;
     }
@@ -137,18 +48,17 @@ public class OpenNLPModule extends Module<OpenNLPResult> {
         (ConditionalSerialAnalyserController) PersistenceManager.loadObjectFromFile(engineState);
   }
 
-  /**
-   * Creates the Gate corpus out of the available chapters.
-   */
-  private void createCorpus() throws ResourceInstantiationException {
-    corpus = Factory.newCorpus("ViTA Corpus");
+  @Override
+  protected OpenNLPResult buildResult() {
+    return new OpenNLPResult() {
+      @Override
+      public Set<Annotation> getAnnotationsForChapter(Chapter chapter) {
+        if (!chapterToAnnotation.containsKey(chapter)) {
+          throw new IllegalArgumentException("This chapter has not been analyzed");
+        }
 
-    for (DocumentPart part : importResult.getParts()) {
-      for (Chapter chapter : part.getChapters()) {
-        Document doc = Factory.newDocument(chapter.getText());
-        docToChapter.put(doc, chapter);
-        corpus.add(doc);
+        return chapterToAnnotation.get(chapter);
       }
-    }
+    };
   }
 }
