72abf92b7ab721af09e1a8f756d72506f57685f6
Sanjeev <sanjeev.b@gmx.de>
2015-01-21 19:29:40 +0100

    #242 Add delete service for entity deletion

--- src/main/java/de/unistuttgart/vis/vita/services/entity/EntityService.java
+++ src/main/java/de/unistuttgart/vis/vita/services/entity/EntityService.java
@@ -1,17 +1,29 @@
 package de.unistuttgart.vis.vita.services.entity;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 import javax.annotation.ManagedBean;
 import javax.inject.Inject;
 import javax.persistence.NoResultException;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import de.unistuttgart.vis.vita.model.dao.EntityDao;
+import de.unistuttgart.vis.vita.model.dao.EntityRelationDao;
+import de.unistuttgart.vis.vita.model.dao.WordCloudDao;
 import de.unistuttgart.vis.vita.model.entity.Entity;
+import de.unistuttgart.vis.vita.model.entity.EntityRelation;
+import de.unistuttgart.vis.vita.model.wordcloud.WordCloud;
+import de.unistuttgart.vis.vita.model.wordcloud.WordCloudItem;
 import de.unistuttgart.vis.vita.services.BaseService;
 import de.unistuttgart.vis.vita.services.occurrence.EntityOccurrencesService;
 import de.unistuttgart.vis.vita.services.search.SearchEntityService;
@@ -21,7 +33,7 @@ import de.unistuttgart.vis.vita.services.search.SearchEntityService;
  */
 @ManagedBean
 public class EntityService extends BaseService {
-  
+
   private String documentId;
   private String entityId;
 
@@ -33,12 +45,19 @@ public class EntityService extends BaseService {
   @Inject
   private EntityOccurrencesService entityOccurrencesService;
 
+  private EntityRelationDao entityRelationDao;
+
+  private WordCloudDao wordCloudDao;
+
   @Inject
   SearchEntityService searchEntityService;
 
-  @Override public void postConstruct() {
+  @Override
+  public void postConstruct() {
     super.postConstruct();
     entityDao = getDaoFactory().getEntityDao();
+    entityRelationDao = getDaoFactory().getEntityRelationDao();
+    wordCloudDao = getDaoFactory().getWordCloudDao();
   }
 
   /**
@@ -93,7 +112,7 @@ public class EntityService extends BaseService {
   public AttributesService getAttributes() {
     return attributesService.setDocumentId(documentId).setEntityId(entityId);
   }
-  
+
   /**
    * Returns the EntityOccurrencesService for the current Entity.
    * 
@@ -103,9 +122,29 @@ public class EntityService extends BaseService {
   public EntityOccurrencesService getOccurrences() {
     return entityOccurrencesService.setEntityId(entityId).setDocumentId(documentId);
   }
-  
+
   public SearchEntityService getSearch() {
     return searchEntityService.setDocumentId(documentId).setEntityId(entityId);
   }
 
+  /**
+   * Deletes the entity and actualize the entity relations and wordclouds
+   *
+   * @return a response with no content if removal was successful, status 404 if document was not
+   *         found
+   */
+  @Path("/delete")
+  @DELETE
+  public Response deleteEntity(@PathParam("entityId") String entityId) {
+
+    Response response;
+    wordCloudDao.updateEntityIdOfItems(entityId);
+    wordCloudDao.remove(wordCloudDao.findByEntity(entityId));
+    entityRelationDao.deleteEntityRelations(entityId);
+    entityDao.deleteEntityById(entityId);
+    // create the response
+    response = Response.noContent().build();
+
+    return response;
+  }
 }
