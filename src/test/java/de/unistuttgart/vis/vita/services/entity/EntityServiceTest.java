8864a5855a6c5b752aa152626c11133b09441a88
Sanjeev <sanjeev.b@gmx.de>
2015-01-21 19:33:32 +0100

    #242 Rename EntityServiceTest to AbstractEntityServiceTest

--- src/test/java/de/unistuttgart/vis/vita/services/entity/EntityServiceTest.java
+++ src/test/java/de/unistuttgart/vis/vita/services/entity/EntityServiceTest.java
@@ -1,116 +1,125 @@
 package de.unistuttgart.vis.vita.services.entity;
 
-import de.unistuttgart.vis.vita.data.*;
-import de.unistuttgart.vis.vita.model.document.Document;
-import de.unistuttgart.vis.vita.model.entity.*;
-import de.unistuttgart.vis.vita.services.ServiceTest;
-import org.junit.Test;
-
-import javax.persistence.EntityManager;
+import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
 
-public abstract class EntityServiceTest extends ServiceTest {
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
-  protected EntityManager em;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.Test;
 
-  protected String docId;
-  protected String entityId;
+import de.unistuttgart.vis.vita.model.dao.EntityDao;
+import de.unistuttgart.vis.vita.model.dao.EntityRelationDao;
+import de.unistuttgart.vis.vita.model.dao.WordCloudDao;
+import de.unistuttgart.vis.vita.model.document.DocumentContent;
+import de.unistuttgart.vis.vita.model.entity.Attribute;
+import de.unistuttgart.vis.vita.model.entity.Entity;
+import de.unistuttgart.vis.vita.model.entity.EntityRelation;
+import de.unistuttgart.vis.vita.model.wordcloud.WordCloud;
+import de.unistuttgart.vis.vita.model.wordcloud.WordCloudItem;
 
-  protected PersonTestData personTestData;
-  protected PlaceTestData placeTestData;
-  protected AttributeTestData attributeTestData;
-  protected EntityRelationTestData relationTestData;
 
-  protected Document testDoc;
+public class EntityServiceTest extends AbstractEntityServiceTest {
+
+  private Entity testPerson;
+  private Entity relatedPerson;
+  private WordCloud entityWordCloud;
+  private WordCloud globalWordCloud;
+  private EntityDao entityDao;
+  private EntityRelationDao entityRelationDao;
+  private WordCloudDao wordCloudDao;
+
+  @Override
+  protected Application configure() {
+    return new ResourceConfig(EntityService.class);
+  }
 
   @Override
   public void setUp() throws Exception {
     super.setUp();
 
-    em = getModel().getEntityManager();
+    testPerson = personTestData.createTestPerson(1);
+    relatedPerson = personTestData.createTestPerson(2);
 
-    // set up test data
-    personTestData = new PersonTestData();
-    placeTestData = new PlaceTestData();
-    attributeTestData = new AttributeTestData();
-    relationTestData = new EntityRelationTestData();
+    entityId = "frodo";
+    docId = "document";
+    testPerson.setId("frodo");
+    relatedPerson.setId("gandalf");
+    testDoc.setId("document");
 
-    // create document
-    testDoc = new DocumentTestData().createTestDocument(1);
-    docId = testDoc.getId();
+    EntityRelation testRelation = relationTestData.createTestRelation(testPerson, relatedPerson);
+    testPerson.getEntityRelations().add(testRelation);
+
+    Attribute testAttribute = attributeTestData.createTestAttribute();
+    testPerson.getAttributes().add(testAttribute);
+
+    createEntityWordCloud();
+    createGlobalWordCloud();
+    DocumentContent content = new DocumentContent();
+    content.setGlobalWordCloud(globalWordCloud);
+
+    testDoc.setContent(content);
+    testPerson.setWordCloud(entityWordCloud);
+
+    em.getTransaction().begin();
+    em.persist(testAttribute);
+    em.persist(testPerson);
+    em.persist(testRelation);
+    em.persist(relatedPerson);
+    em.persist(testDoc);
+    em.persist(entityWordCloud);
+    em.persist(globalWordCloud);
+    em.getTransaction().commit();
+
   }
 
-  /**
-   * Checks whether an Entity can be caught using REST.
-   */
+  private void createEntityWordCloud() {
+    entityWordCloud = new WordCloud();
+    Set<WordCloudItem> items = new HashSet<WordCloudItem>();
+    items.add(new WordCloudItem("brave", 2));
+    items.add(new WordCloudItem("beutlin", 1));
+    entityWordCloud.getItems().addAll(items);
+  }
+
+  private void createGlobalWordCloud() {
+    globalWordCloud = new WordCloud();
+    Set<WordCloudItem> items = new HashSet<WordCloudItem>();
+    items.add(new WordCloudItem("Frodo Baggins", 1));
+    items.add(new WordCloudItem("beutlin", 1));
+    items.add(new WordCloudItem("Gandalf", 1));
+
+    globalWordCloud.getItems().addAll(items);
+  }
+
+  @Override
+  public void testGetEntity() {
+    Entity responseEntity = target(getPath("entities")).request().get(Entity.class);
+    assertThat(responseEntity.getDisplayName(), is("Frodo Baggins"));
+    assertThat(responseEntity.getAttributes().iterator().next().getContent(), is("Bilbo Baggins"));
+  }
+
   @Test
-  public abstract void testGetEntity();
+  public void testDeletion() {
 
-  /**
-   * Checks a given Entities values, including attributes and relations matching the test data.
-   *
-   * @param entityToCheck - the Entity to be checked
-   */
-  protected void check(Entity entityToCheck) {
-    checkEntity(entityToCheck);
-    checkAttributes(entityToCheck.getAttributes());
-    checkRelations(entityToCheck.getEntityRelations());
-  }
+    entityDao = new EntityDao(em);
+    entityRelationDao = new EntityRelationDao(em);
+    wordCloudDao = new WordCloudDao(em);
+    Response actualResponse = target("documents/document/entities/frodo/delete").request().delete();
+    assertEquals(204, actualResponse.getStatus());
 
-  /**
-   * Checks whether a given Entity matches the test Data.
-   *
-   * @param entityToCheck - the entity to be checked
-   */
-  private void checkEntity(Entity entityToCheck) {
-    switch (entityToCheck.getType()) {
-      case PERSON:
-        personTestData.checkData((Person) entityToCheck);
-        break;
+    em.refresh(testDoc);
 
-      case PLACE:
-        placeTestData.checkData((Place) entityToCheck);
-        break;
+    assertThat(entityDao.findAll().size(), is(1));
+    assertThat(entityRelationDao.findAll().size(), is(0));
+
+    assertThat(wordCloudDao.findAll().size(), is(1));
+
+    for(WordCloudItem item: wordCloudDao.getGlobalWordCloud(docId).getItems()){
+      assertThat(item.getEntityId(), nullValue());
     }
   }
-
-  /**
-   * Checks whether the given Set of Attributes matches the test data.
-   *
-   * @param attributesToCheck - the Set of Attribute to be checked
-   */
-  private void checkAttributes(Set<Attribute> attributesToCheck) {
-    assertNotNull(attributesToCheck);
-    assertEquals(1, attributesToCheck.size());
-
-    for (Attribute attribute: attributesToCheck) {
-      attributeTestData.checkData(attribute, 1);
-    }
-  }
-
-  /**
-   * Checks whether the given Set of EntityRelations matches the test data.
-   *
-   * @param relationsToCheck - the Set of EntityRelations to be checked
-   */
-  private void checkRelations(Set<EntityRelation> relationsToCheck) {
-    assertEquals(1, relationsToCheck.size());
-
-    // can only check whether the weight is correct, other fields are null
-    assertEquals(EntityRelationTestData.TEST_ENTITY_RELATION_WEIGHT, relationsToCheck.iterator().next().getWeight(), 0.001);
-  }
-
-  /**
-   * Returns the path to a service returning the given result.
-   *
-   * @param resultName - the name of the result which the service returns.
-   * @return the service path
-   */
-  protected String getPath(String resultName) {
-    return "documents/" + docId + "/" + resultName + "/" + entityId;
-  }
-
 }
