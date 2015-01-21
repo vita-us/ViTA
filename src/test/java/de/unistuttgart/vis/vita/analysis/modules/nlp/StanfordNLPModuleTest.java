279e8fee5e6d92ee182885576a302475339f6edb
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2015-01-21 01:30:27 +0100

    Fix files.

--- src/test/java/de/unistuttgart/vis/vita/analysis/modules/nlp/StanfordNLPModuleTest.java
+++ src/test/java/de/unistuttgart/vis/vita/analysis/modules/nlp/StanfordNLPModuleTest.java
@@ -1,111 +1,60 @@
 package de.unistuttgart.vis.vita.analysis.modules.nlp;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
-import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.*;
+import de.unistuttgart.vis.vita.analysis.modules.gate.StanfordNLPModule;
+import de.unistuttgart.vis.vita.analysis.results.StanfordNLPResult;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import de.unistuttgart.vis.vita.analysis.ModuleResultProvider;
-import de.unistuttgart.vis.vita.analysis.ProgressListener;
-import de.unistuttgart.vis.vita.analysis.results.ImportResult;
-import de.unistuttgart.vis.vita.analysis.results.StanfordNLPResult;
-import de.unistuttgart.vis.vita.model.document.Chapter;
-import de.unistuttgart.vis.vita.model.document.DocumentPart;
-import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.pipeline.Annotation;
+import java.util.Set;
 
-public class StanfordNLPModuleTest {
-  private StanfordNLPModule module;
-  private List<DocumentPart> parts = new ArrayList<>();
-  private ModuleResultProvider resultProvider;
-  private ProgressListener progressListener;
-  private List<Chapter> chapterObjects;
-  
-  private final static String[] CHAPTERS = {
-    "The text of the first chapter which consists of two sentences. The second one is short.",
-    "Dave went to New York this summer.",
-    "Just some more chapters ...",
-    "... to properly test the progress feature ...",
-    "... because the granularity can not be higher than the number of chapters."
-  };
-  
-  @Before
-  public void setUp() {
+import gate.Annotation;
+
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isOneOf;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.doubleThat;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+
+public class StanfordNLPModuleTest extends NLPTestSetup {
+
+  private static StanfordNLPModule module;
+  private static StanfordNLPResult stanfordNLPResult;
+
+  @BeforeClass
+  public static void setUp() throws Exception {
+    NLPTestSetup.setUp();
     module = new StanfordNLPModule();
-    resultProvider = mock(ModuleResultProvider.class);
-    ImportResult importResult = mock(ImportResult.class);
-    when(importResult.getParts()).thenReturn(parts);
-    when(resultProvider.getResultFor(ImportResult.class)).thenReturn(importResult);
-    progressListener = mock(ProgressListener.class);
-    fillText();
+    stanfordNLPResult = module.execute(resultProvider, progressListener);
   }
-  
-  private void fillText() {
-    DocumentPart part = new DocumentPart();
-    parts.add(part);
 
-    chapterObjects = new ArrayList<>();
-    for (String chapterText : CHAPTERS) {
-      Chapter chapter = new Chapter();
-      chapter.setText(chapterText);
-      chapter.setLength(chapterText.length());
-      part.getChapters().add(chapter);
-      chapterObjects.add(chapter);
-    }
-  }
-  
-  @Test
-  public void testBasicAnnotations() throws Exception {
-    StanfordNLPResult result = module.execute(resultProvider, progressListener);
-    
-    Annotation annotation1 = result.getAnnotationForChapter(chapterObjects.get(0));
-    assertThat(annotation1, is(not(nullValue())));
-    assertThat(annotation1.get(SentencesAnnotation.class), hasSize(2));
-    assertThat(annotation1.get(TokensAnnotation.class), hasSize(18));
-  }
-  
   @Test
   public void testEntityAnnotations() throws Exception {
-    StanfordNLPResult result = module.execute(resultProvider, progressListener);
-    
-    Annotation annotation2 = result.getAnnotationForChapter(chapterObjects.get(1));
-    assertThat(annotation2, is(not(nullValue())));
-    
-    List<String> actualEntities = new ArrayList<String>();
-    for (CoreLabel token : annotation2.get(TokensAnnotation.class)) {
-      if (token.has(NamedEntityTagAnnotation.class)) {
-        String name = token.get(TextAnnotation.class);
-        String type = token.get(NamedEntityTagAnnotation.class);
-        if (!type.equals("O")) {
-          actualEntities.add(name + ": " + type);
-        }
-      }
+    Set<Annotation> annotations = stanfordNLPResult.getAnnotationsForChapter(chapterObjects.get(1));
+    assertThat(annotations, is(not(empty())));
+    assertThat(annotations, is(hasSize(2)));
+
+    for (Annotation annotation : annotations) {
+      assertThat(annotation, is(notNullValue()));
+      assertThat(annotation.getType(), isOneOf("LOCATION", "PERSON"));
     }
-    
-    assertThat(actualEntities, contains("Dave: PERSON", "New: LOCATION", "York: LOCATION",
-        "this: DATE", "summer: DATE"));
   }
-  
+
   @Test
   public void testProgressIsReported() throws Exception {
-    module.execute(resultProvider, progressListener);
-    
     // Check that the 0%-100% range is covered approximately
     // This does not test smoothness as the call times are not considered.
     int steps = 5;
     for (int i = 0; i < steps; i++) {
-      verify(progressListener, atLeastOnce()).observeProgress(
-          doubleThat(closeTo((double) i / steps, (double) 1 / steps)));
+      verify(progressListener, atLeastOnce()).observeProgress(doubleThat(
+          closeTo((double) i / steps, (double) 1 / steps)));
     }
   }
+
 }
