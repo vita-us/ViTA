b9a1b63fa62b88ea4d41a25ee820ab0302d560a5
Vincent Link <mail@linkvt.de>
2015-01-22 12:56:17 +0100

    Remove unused code
    Searched in the whole front-end directory for "alternativeNames". Found
    only this five occurrences and the alternative names are still listed.

--- src/main/front-end/app/js/places/placeListController.js
+++ src/main/front-end/app/js/places/placeListController.js
@@ -7,23 +7,6 @@
   vitaControllers.controller('PlaceListCtrl', ['$scope', 'Document', 'Page', 'Place',
       '$routeParams', function($scope, Document, Page, Place, $routeParams) {
 
-        $scope.alternativeNames = function(place, searchQuery) {
-          for (var i = 0; i < place.attributes.length; i++) {
-            var attribute = place.attributes[i];
-            if (attribute.attributetype == 'name' &&
-              containsQueryCaseInsensitive(attribute.content, searchQuery)) {
-              place.alternativeName = attribute.content;
-            }
-          }
-        };
-
-        var containsQueryCaseInsensitive = function(text, query) {
-          var lowerCaseText = text.toLowerCase();
-          var lowerCaseQuery = query.toLowerCase();
-
-          return lowerCaseText.indexOf(lowerCaseQuery) > -1;
-        };
-
         Place.get({
           documentId: $routeParams.documentId
         }, function(placesWrapper) {
