731f543c7077a5f3eb47a259e9ac9b8cae7a7336
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2015-01-22 03:08:35 +0100

    Add constants for nlp related stuff.

---
+++ src/main/java/de/unistuttgart/vis/vita/analysis/modules/gate/NLPConstants.java
@@ -0,0 +1,18 @@
+/*
+ * NLPConstants.java
+ *
+ */
+
+package de.unistuttgart.vis.vita.analysis.modules.gate;
+
+/**
+ *
+ */
+public class NLPConstants {
+
+  public static final String TYPE_PERSON_STANFORD = "PERSON";
+  public static final String TYPE_LOCATION_STANFORD = "LOCATION";
+
+  public static final String FEATURE_GENDER = "gender";
+
+}
