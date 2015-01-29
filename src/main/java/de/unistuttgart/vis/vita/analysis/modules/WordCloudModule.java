599d8ee2d362d86648a6071950fc671e696fc3dc
Sebastian <franksn@studi.informatik.uni-stuttgart.de>
2015-01-29 15:45:49 +0100

    #250 Refactored WordCloudModule

--- src/main/java/de/unistuttgart/vis/vita/analysis/modules/WordCloudModule.java
+++ src/main/java/de/unistuttgart/vis/vita/analysis/modules/WordCloudModule.java
@@ -26,21 +26,26 @@ import de.unistuttgart.vis.vita.model.wordcloud.WordCloud;
 import de.unistuttgart.vis.vita.model.wordcloud.WordCloudItem;
 
 /**
- * Calculates the document-wide word cloud using lucene
+ * Calculates the document-wide word cloud using lucene. Analysis Parameters can determine to which
+ * value the number of different words in the word cloud is limited and if the stop word list should
+ * be used.
  */
 @AnalysisModule(dependencies = {LuceneResult.class, AnalysisParameters.class})
 public class WordCloudModule extends Module<GlobalWordCloudResult> {
-  private int maxCount;
+  private int maxWordCloudItemsCount;
 
   @Override
   public GlobalWordCloudResult execute(ModuleResultProvider results,
       ProgressListener progressListener) throws IOException {
 
+    // get parameters
     LuceneResult luceneResult = results.getResultFor(LuceneResult.class);
     boolean stopWordListEnabled =
         results.getResultFor(AnalysisParameters.class).getStopWordListEnabled();
-    maxCount = results.getResultFor(AnalysisParameters.class).getWordCloudItemsCount();
+    maxWordCloudItemsCount =
+        results.getResultFor(AnalysisParameters.class).getWordCloudItemsCount();
 
+    // calculate word cloud
     final WordCloud globalWordCloud;
     IndexReader reader = luceneResult.getIndexReader();
     try {
@@ -49,14 +54,24 @@ public class WordCloudModule extends Module<GlobalWordCloudResult> {
       reader.close();
     }
 
+    // return word cloud
     return new GlobalWordCloudResult() {
-        @Override
-        public WordCloud getGlobalWordCloud() {
-          return globalWordCloud;
-        }
-      };
+      @Override
+      public WordCloud getGlobalWordCloud() {
+        return globalWordCloud;
+      }
+    };
   }
 
+  /**
+   * 
+   * @param reader - The Lucene Index Reader of the current document.
+   * @param stopWordListEnabled - true: stop words should be used to filter the Word Cloud. false:
+   *        no filtering.
+   * @return The Global Word Cloud for the document.
+   * @throws IOException - Can be thrown by reader or terms iterator. Can also be thrown if this
+   *         method was unable to get the Stop Word list.
+   */
   private WordCloud getGlobalWordCloud(IndexReader reader, boolean stopWordListEnabled)
       throws IOException {
     Terms terms = SlowCompositeReaderWrapper.wrap(reader).terms(TextRepository.CHAPTER_TEXT_FIELD);
@@ -65,20 +80,33 @@ public class WordCloudModule extends Module<GlobalWordCloudResult> {
       // This means that there are no chapters
       return new WordCloud();
     }
+    
+    Set<String> stopWordList = prepareStopWordsSet(stopWordListEnabled);
+    List<WordCloudItem> items = createWordCloudItemsFromTerms(reader, stopWordList, terms);
+    sortAndFilter(items);
+    
+    return new WordCloud(items);
+  }
 
-    Set<String> stopWordList;
-    if (stopWordListEnabled) {
-      stopWordList = StopWordList.getStopWords();
-    } else {
-      stopWordList = new HashSet<String>();
-    }
+  /**
+   * Uses the Lucene IndexReader and Terms to get Word Counts and create WordCloudItems. Ignores the
+   * stop words.
+   * 
+   * @param reader - The IndexReader.
+   * @param stopWordList - Words on this list will be ignored.
+   * @param terms - The Terms (Words) found by Lucene.
+   * @return The WordCloutItems to create a Word Cloud.
+   * @throws IOException - Thrown by reader or terms iterator.
+   */
+  private List<WordCloudItem> createWordCloudItemsFromTerms(IndexReader reader,
+      Set<String> stopWordList, Terms terms) throws IOException {
+    List<WordCloudItem> items = new ArrayList<WordCloudItem>();
 
     TermsEnum enumerator = terms.iterator(null);
     BytesRef term = enumerator.next();
-    List<WordCloudItem> items = new ArrayList<WordCloudItem>();
+
     while (term != null) {
       String termText = term.utf8ToString();
-
       if (!stopWordList.contains(termText)) {
         long frequency = reader.totalTermFreq(new Term(TextRepository.CHAPTER_TEXT_FIELD, term));
         items.add(new WordCloudItem(termText, (int) frequency));
@@ -86,11 +114,38 @@ public class WordCloudModule extends Module<GlobalWordCloudResult> {
       term = enumerator.next();
     }
 
-    // WordCloudItems sort normally, but we want the higher items at the start, thus reversed
-    Collections.sort(items, Collections.reverseOrder());
-    if (items.size() > maxCount)
-      items = items.subList(0, maxCount);
-
-    return new WordCloud(items);
+    return items;
   }
+
+  /**
+   * Get the stop words if enabled, otherwise an empty set.
+   * 
+   * @param stopWordListEnabled - true: defined stop words should be used to filter the Word Cloud.
+   *        false: only basic filtering, no stop words.
+   * @return all stop words
+   * @throws IOException - Thrown if this methods was unable to get the stop words.
+   */
+  private Set<String> prepareStopWordsSet(boolean stopWordListEnabled) throws IOException {
+    Set<String> stopWordList = new HashSet<String>();
+    if (stopWordListEnabled) {
+      stopWordList = StopWordList.getStopWords();
+    } else {
+      stopWordList = new HashSet<String>();
+    }
+    return stopWordList;
+  }
+  
+  /**
+   * Sorts the WordCloudItems in reversed order (highest first). The most rare items may be removed,
+   * if there are more items than allowed.
+   * 
+   * @param items - The WordCloudItems.
+   */
+  private void sortAndFilter(List<WordCloudItem> items) {
+    Collections.sort(items, Collections.reverseOrder());
+    if (items.size() > maxWordCloudItemsCount) {
+      items = items.subList(0, maxWordCloudItemsCount);
+    }
+  }
+
 }
